a3162c2e7e25cfa50ea8b61843360ebc6435ea05
Audun D. Christoffersen <audunc@met.no>
2009-08-20 08:52:50 +0000

    removed Helvetica and Arial hardcoding
    git-svn-id: https://svn.met.no/diana/trunk@1503 0ccb8871-b436-41a3-ae7d-e3ac59973202

--- src/diSpectrumPlot.cc
+++ src/diSpectrumPlot.cc
@@ -11,7 +11,7 @@
   0313 OSLO
   NORWAY
   email: diana@met.no
-  
+
   This file is part of Diana
 
   Diana is free software; you can redistribute it and/or modify
@@ -23,7 +23,7 @@
   but WITHOUT ANY WARRANTY; without even the implied warranty of
   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
   GNU General Public License for more details.
-  
+
   You should have received a copy of the GNU General Public License
   along with Diana; if not, write to the Free Software
   Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
@@ -721,7 +721,7 @@ bool SpectrumPlot::plot(SpectrumOptions *spopt)
 
       poptions.contourShading= 0;
 
-      fp->setFont("Arial");
+      fp->setFont("SCALEFONT");
       fp->setFontSize(fontSizeLabel);
       fp->getCharSize('0',chxlab,chylab);
       // the real height (x ok)
@@ -891,7 +891,7 @@ bool SpectrumPlot::plot(SpectrumOptions *spopt)
   }
   //...........................................................
 
-  fp->setFont("Helvetica");
+  fp->setFont("BITMAPFONT");
 
   float x,y,dx,dy;
 
