8fc7581b14857f5a349aa3e8d996b82f8b6510a2
Lisbeth Bergholt <lisbethb@met.no>
2009-11-24 20:29:54 +0000

    Typo
    git-svn-id: https://svn.met.no/diana/trunk@1645 0ccb8871-b436-41a3-ae7d-e3ac59973202

--- src/qtSpectrumModelDialog.h
+++ src/qtSpectrumModelDialog.h
@@ -24,7 +24,7 @@
   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
   GNU General Public License for more details.
 
-  You should have received a copy of the GNU General PubliJ_mc License
+  You should have received a copy of the GNU General Public License
   along with Diana; if not, write to the Free Software
   Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
