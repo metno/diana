e209a74987acc0f36160bc8c46de53b4e44e58a7
Audun D. Christoffersen <audunc@met.no>
2010-01-19 14:31:43 +0000

    format style only
    git-svn-id: https://svn.met.no/diana/trunk@1772 0ccb8871-b436-41a3-ae7d-e3ac59973202

--- src/qtStatusGeopos.cc
+++ src/qtStatusGeopos.cc
@@ -1,33 +1,33 @@
 /*
-  Diana - A Free Meteorological Visualisation Tool
+ Diana - A Free Meteorological Visualisation Tool
 
-  $Id$
+ $Id$
 
-  Copyright (C) 2006 met.no
+ Copyright (C) 2006 met.no
 
-  Contact information:
-  Norwegian Meteorological Institute
-  Box 43 Blindern
-  0313 OSLO
-  NORWAY
-  email: diana@met.no
-  
-  This file is part of Diana
+ Contact information:
+ Norwegian Meteorological Institute
+ Box 43 Blindern
+ 0313 OSLO
+ NORWAY
+ email: diana@met.no
 
-  Diana is free software; you can redistribute it and/or modify
-  it under the terms of the GNU General Public License as published by
-  the Free Software Foundation; either version 2 of the License, or
-  (at your option) any later version.
+ This file is part of Diana
 
-  Diana is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-  GNU General Public License for more details.
-  
-  You should have received a copy of the GNU General Public License
-  along with Diana; if not, write to the Free Software
-  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-*/
+ Diana is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ (at your option) any later version.
+
+ Diana is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with Diana; if not, write to the Free Software
+ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
 #include <qtStatusGeopos.h>
 
 #include <QLabel>
@@ -39,45 +39,46 @@
 #include <iomanip>
 #include <math.h>
 
-StatusGeopos::StatusGeopos(QWidget* parent)
-  : QWidget(parent) {
+StatusGeopos::StatusGeopos(QWidget* parent) :
+  QWidget(parent)
+  {
 
-  geographicMode= true;
-  decimalMode= false;
-  
+  geographicMode = true;
+  decimalMode = false;
+
   // Create horisontal layout manager
-  QHBoxLayout* thlayout = new QHBoxLayout( this);
+  QHBoxLayout* thlayout = new QHBoxLayout(this);
   thlayout->setMargin(1);
   thlayout->setSpacing(5);
 
-  sxlabel= new QLabel(tr("Lat:"),this);
+  sxlabel = new QLabel(tr("Lat:"), this);
   sxlabel->setMinimumSize(sxlabel->sizeHint());
-  thlayout->addWidget(sxlabel,0);
+  thlayout->addWidget(sxlabel, 0);
 
-  latlabel= new QLabel(" 00°00'N",this);
-  latlabel->setFrameStyle( QFrame::Panel | QFrame::Sunken );
-//   latlabel->setPalette( QPalette( QColor(0, 54, 125) ) );
+  latlabel = new QLabel(" 00°00'N", this);
+  latlabel->setFrameStyle(QFrame::Panel | QFrame::Sunken);
+  //   latlabel->setPalette( QPalette( QColor(0, 54, 125) ) );
   latlabel->setMinimumSize(latlabel->sizeHint());
-  thlayout->addWidget(latlabel,0);
+  thlayout->addWidget(latlabel, 0);
 
   thlayout->addSpacing(10);
-  
-  sylabel= new QLabel(tr("Lon:"),this);
+
+  sylabel = new QLabel(tr("Lon:"), this);
   sylabel->setMinimumSize(sylabel->sizeHint());
-  thlayout->addWidget(sylabel,0);
-  
-  lonlabel= new QLabel("000°00'W",this);
-  lonlabel->setFrameStyle( QFrame::Panel | QFrame::Sunken );
+  thlayout->addWidget(sylabel, 0);
+
+  lonlabel = new QLabel("000°00'W", this);
+  lonlabel->setFrameStyle(QFrame::Panel | QFrame::Sunken);
   lonlabel->setMinimumSize(lonlabel->sizeHint());
-  thlayout->addWidget(lonlabel,0);
+  thlayout->addWidget(lonlabel, 0);
 
   // Start the geometry management
   thlayout->activate();
-}
+  }
 
-
-void StatusGeopos::changeMode() {
-  geographicMode= !geographicMode;
+void StatusGeopos::changeMode()
+{
+  geographicMode = !geographicMode;
   if (geographicMode) {
     sxlabel->setText(tr("Lat:"));
     sylabel->setText(tr("Lon:"));
@@ -89,12 +90,13 @@ void StatusGeopos::changeMode() {
   lonlabel->clear();
 }
 
-void StatusGeopos::changeLatLonMode() {
-  decimalMode= !decimalMode;
+void StatusGeopos::changeLatLonMode()
+{
+  decimalMode = !decimalMode;
 }
 
-
-void StatusGeopos::setPosition(float lat, float lon){
+void StatusGeopos::setPosition(float lat, float lon)
+{
 
   ostringstream slat, slon;
 
@@ -103,33 +105,33 @@ void StatusGeopos::setPosition(float lat, float lon){
     if (decimalMode) {
 
       slat << setw(6) << setprecision(5) << lat;
-      slon << setw(6) << setprecision(5) <<lon;
-    
+      slon << setw(6) << setprecision(5) << lon;
+
     } else {
 
       int deg, min;
 
-      min= int(fabsf(lat)*60.+0.5);
-      deg= min/60;
-      min= min%60;
-      if (lat>=0.0)
-	slat << setw(2) << deg << "°" << setw(2) << setfill('0') << min << "'N";
+      min = int(fabsf(lat) * 60. + 0.5);
+      deg = min / 60;
+      min = min % 60;
+      if (lat >= 0.0)
+        slat << setw(2) << deg << "°" << setw(2) << setfill('0') << min << "'N";
       else
-	slat << setw(2) << deg << "°" << setw(2) << setfill('0') << min << "'S";
-      
-      min= int(fabsf(lon)*60.+0.5);
-      deg= min/60;
-      min= min%60;
-      if (lon>=0.0)
-	slon << setw(3) << deg << "°" << setw(2) << setfill('0') << min << "'E";
+        slat << setw(2) << deg << "°" << setw(2) << setfill('0') << min << "'S";
+
+      min = int(fabsf(lon) * 60. + 0.5);
+      deg = min / 60;
+      min = min % 60;
+      if (lon >= 0.0)
+        slon << setw(3) << deg << "°" << setw(2) << setfill('0') << min << "'E";
       else
-	slon << setw(3) << deg << "°" << setw(2) << setfill('0') << min << "'W";
+        slon << setw(3) << deg << "°" << setw(2) << setfill('0') << min << "'W";
     }
-    
+
   } else {
-      
-    float x= lat + 1.;  // fortran indexing
-    float y= lon + 1.;
+
+    float x = lat + 1.; // fortran indexing
+    float y = lon + 1.;
     slat << setw(6) << x;
     slon << setw(6) << y;
 
