3db09734bbe98c472592a034d94f2bba9e78fa25
Lisbeth Bergholt <lisbethb@met.no>
2011-01-14 13:17:06 +0000

    removing cerr
    git-svn-id: https://svn.met.no/diana/trunk@2407 0ccb8871-b436-41a3-ae7d-e3ac59973202

--- src/diSat.cc
+++ src/diSat.cc
@@ -547,12 +547,12 @@ void Sat::setArea()
 
   //initializing libmi
 
-    cerr << "setArea 1 " << proj_string <<  endl;
+//    cerr << "setArea 1 " << proj_string <<  endl;
     Projection p;
     p.setProjectionFromAB(Ax, Ay, Bx, By, TrueLat, GridRot, adjustGrid, gridResolutionX, gridResolutionY);
     area.setP(p);
 } else {
-    cerr << "setArea 2 " << proj_string <<  endl;
+//    cerr << "setArea 2 " << proj_string <<  endl;
     Projection p(proj_string, Ax, Ay);
     area.setP(p);
     gridResolutionX = Ax;
