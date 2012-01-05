metno/diana

commit 0377654835f2db321d9ba7b50cc2f018302aa090

Bufr changes and fixes
git-svn-id: https://svn.met.no/diana/trunk@2160 0ccb8871-b436-41a3-ae7d-e3ac59973202

src/diObsPlot.cc
@@ -2999,6 +2999,7 @@ void ObsPlot::plotList(int index)
   }
   if (pFlag.count("911ff")) {
     ypos -= yStep;
+    checkGustTime(dta);
     if ((f_p = dta.fdata.find("911ff")) != dta.fdata.end()) {
       if (ccriteria)
         checkColourCriteria("911ff", f_p->second);
@@ -3019,6 +3020,7 @@ void ObsPlot::plotList(int index)
   }
   if (pFlag.count("fxfx")) {
     ypos -= yStep;
+    checkMaxWindTime(dta);
     if ((f_p = dta.fdata.find("fxfx")) != dta.fdata.end()) {
       if (ccriteria)
         checkColourCriteria("fxfx", f_p->second);
@@ -4325,11 +4327,14 @@ void ObsPlot::plotSynop(int index)
   }
 
   // Maximum wind speed (gusts) - 911ff
-  if (pFlag.count("911ff") && (f_p = dta.fdata.find("911ff")) != fend) {
-    if (ccriteria)
-      checkColourCriteria("911ff", f_p->second);
-    printNumber(ms2knots(f_p->second), iptab[lpos + 38] + 2, iptab[lpos + 39]
-        + 2, "fill_2", true);
+  if (pFlag.count("911ff") ) {
+    checkGustTime(dta);
+    if((f_p = dta.fdata.find("911ff")) != fend) {
+      if (ccriteria)
+        checkColourCriteria("911ff", f_p->second);
+      printNumber(ms2knots(f_p->second), iptab[lpos + 38] + 2, iptab[lpos + 39]
+                                                                     + 2, "fill_2", true);
+    }
   }
 
   // State of the sea - s
@@ -4343,16 +4348,19 @@ void ObsPlot::plotSynop(int index)
   }
 
   // Maximum wind speed
-  if (pFlag.count("fxfx") && (f_p = dta.fdata.find("fxfx")) != fend
-      && !(dta.zone > 1 && dta.zone < 99)) {
-    if (ccriteria)
-      checkColourCriteria("fxfx", f_p->second);
-    if (TxTnFlag)
-      printNumber(ms2knots(f_p->second), iptab[lpos + 6] + 12, iptab[lpos + 7]
-          + 2, "fill_2", true);
-    else
-      printNumber(ms2knots(f_p->second), iptab[lpos + 6] + 12, iptab[lpos + 7]
-          - 14, "fill_2", true);
+  if (pFlag.count("fxfx") ) {
+    checkMaxWindTime(dta);
+    if ( (f_p = dta.fdata.find("fxfx")) != fend
+        && !(dta.zone > 1 && dta.zone < 99)) {
+      if (ccriteria)
+        checkColourCriteria("fxfx", f_p->second);
+      if (TxTnFlag)
+        printNumber(ms2knots(f_p->second), iptab[lpos + 6] + 12, iptab[lpos + 7]
+                                                                       + 2, "fill_2", true);
+      else
+        printNumber(ms2knots(f_p->second), iptab[lpos + 6] + 12, iptab[lpos + 7]
+                                                                       - 14, "fill_2", true);
+    }
   }
 
   //Maritime
@@ -5384,16 +5392,68 @@ void ObsPlot::amountOfClouds(int16 Nh, int16 h, float x, float y)
 void ObsPlot::checkAccumulationTime(ObsData &dta)
 {
 
+  // todo: include this if all data sources reports time info
+//  if (dta.fdata.find("RRR")!= dta.fdata.end()){
+//    dta.fdata.erase(dta.fdata.find("RRR"));
+//  }
+
   int hour = Time.hour();
-  if (hour == 6 && hour == 18 && dta.fdata.count("RRR_12")) {
+  if ((hour == 6  || hour == 18 ) && dta.fdata.count("RRR_12")) {
 
     dta.fdata["RRR"] = dta.fdata["RRR_12"];
 
-  } else if (hour == 0 && hour == 12 && dta.fdata.count("RRR_6")) {
+  } else if ((hour == 0 || hour == 12 ) && dta.fdata.count("RRR_6")) {
     dta.fdata["RRR"] = dta.fdata["RRR_6"];
 
   } else if (dta.fdata.count("RRR_1")) {
     dta.fdata["RRR"] = dta.fdata["RRR_1"];
+
+  }
+}
+
+void ObsPlot::checkGustTime(ObsData &dta)
+{
+
+  // todo: include this if all data sources reports time info
+  //  if (dta.fdata.find("911ff")!= dta.fdata.end()){
+  //    dta.fdata.erase(dta.fdata.find("911ff"));
+  //  }
+  int hour = Time.hour();
+  if ((hour == 0 || hour == 6 || hour == 12 || hour == 18 )
+      && dta.fdata.count("911ff_360")) {
+
+    dta.fdata["911ff"] = dta.fdata["911ff_360"];
+
+  } else if ((hour == 3 || hour == 9 || hour == 15 || hour == 21 )
+      && dta.fdata.count("911ff_180")) {
+
+    dta.fdata["911ff"] = dta.fdata["911ff_180"];
+
+  } else if (dta.fdata.count("911ff_60")) {
+
+    dta.fdata["911ff"] = dta.fdata["911ff_60"];
+
+  }
+}
+
+void ObsPlot::checkMaxWindTime(ObsData &dta)
+{
+
+  // todo: include this if all data sources reports time info
+  //  if (dta.fdata.find("fxfx")!= dta.fdata.end()){
+  //    dta.fdata.erase(dta.fdata.find("fxfx"));
+  //  }
+
+  int hour = Time.hour();
+  if ((hour == 0 || hour == 6 || hour == 12 || hour == 18 )
+      && dta.fdata.count("fxfx_360")) {
+
+    dta.fdata["fxfx"] = dta.fdata["fxfx_360"];
+
+  } else if ((hour == 3 || hour == 9 || hour == 15 || hour == 21 )
+      && dta.fdata.count("fxfx_180")) {
+
+    dta.fdata["fxfx"] = dta.fdata["fxfx_180"];
 
   }
 }

src/diObsBufr.cc
@@ -43,7 +43,7 @@
 #include <diObsBufr.h>
 #include <algorithm>
 
-using namespace std; 
+using namespace std;
 using namespace miutil;
 
 const double bufrMissing = 1.6e+38;
@@ -51,7 +51,7 @@ const double bufrMissing = 1.6e+38;
 bool ObsBufr::init(const miString& bufr_file, const miString& format)
 {
 
-  //   cerr <<"ObsBufr::init:"<<bufr_file<<endl;
+//	     cerr	 <<"ObsBufr::init:"<<bufr_file<<endl;
   obsTime = miTime(); //undef
   //   const int ibflen=4*512000;
   //   int ibuff[512000];
@@ -170,7 +170,7 @@ bool ObsBufr::readStationInfo(const miString& bufr_file,
   namelist = id;
   latitudelist = latitude;
   longitudelist = longitude;
-  return true;			
+  return true;
 }
 
 VprofPlot* ObsBufr::getVprofPlot(const miString& bufr_file,
@@ -304,7 +304,7 @@ bool ObsBufr::get_diana_data(int ktdexl, int *ktdexp, double* values,
 
   // constants for changing to met.no units
   const double pa2hpa = 0.01;
-  const double t0 = 273.2; // 273.15 ?????????
+  const double t0 = 273.15;
 
   int wmoBlock = 0;
   int wmoStation = 0;
@@ -367,6 +367,16 @@ bool ObsBufr::get_diana_data(int ktdexl, int *ktdexp, double* values,
       //     break;
 
       //  1011  SHIP OR MOBILE LAND STATION IDENTIFIER, CCITTIA5 (ascii chars)
+    case 1006:
+    {
+      int index = int(values[j]) / 1000 - 1;
+      for (int k = 0; k < 6; k++) {
+        d.id += cvals[index][k];
+      }
+      landStation = false;
+    }
+    break;
+
     case 1011:
     {
       int index = int(values[j]) / 1000 - 1;
@@ -525,6 +535,7 @@ bool ObsBufr::get_diana_data(int ktdexl, int *ktdexp, double* values,
 
       //   10051  PRESSURE REDUCED TO MEAN SEA LEVEL, Pa->hPa
     case 10051:
+    case 7004:
       if (values[j] < bufrMissing)
         d.fdata["PPPP"] = values[j] * pa2hpa;
       break;
@@ -555,8 +566,7 @@ bool ObsBufr::get_diana_data(int ktdexl, int *ktdexp, double* values,
 
       // 011001 WIND DIRECTION
     case 11001:
-      if (!d.fdata.count("dd") && values[j] < bufrMissing && timePeriodMinute
-          == -10) {
+      if (!d.fdata.count("dd") && values[j] < bufrMissing) {
         d.fdata["dd"] = values[j];
       }
       break;
@@ -568,8 +578,7 @@ bool ObsBufr::get_diana_data(int ktdexl, int *ktdexp, double* values,
       break;
       // 011002 WIND SPEED
     case 11002:
-      if (!d.fdata.count("ff") && values[j] < bufrMissing && timePeriodMinute
-          == -10)
+      if (!d.fdata.count("ff") && values[j] < bufrMissing )
         d.fdata["ff"] = values[j];
       break;
 
@@ -587,16 +596,30 @@ bool ObsBufr::get_diana_data(int ktdexl, int *ktdexp, double* values,
 
       // 011041 MAXIMUM WIND SPEED (GUSTS), m/s
     case 11041:
-      if (values[j] < bufrMissing && timePeriodMinute == -10) {
-        d.fdata["911ff"] = values[j];
+      if (values[j] < bufrMissing) {
+//        d.fdata["911ff"] = values[j];
         d.fdata["fmfm"] = values[j]; //metar
+        if (timePeriodMinute == -10) {
+          d.fdata["911ff_10"] = values[j];
+        } else if (timePeriodMinute == -60) {
+          d.fdata["911ff_60"] = values[j];
+        } else if (timePeriodMinute == -180) {
+          d.fdata["911ff_180"] = values[j];
+        } else if (timePeriodMinute == -360) {
+          d.fdata["911ff_360"] = values[j];
+        }
       }
       break;
 
       // 011042 MAXIMUM WIND SPEED (10 MIN MEAN WIND), m/s
     case 11042:
       if (values[j] < bufrMissing)
-        d.fdata["fxfx"] = values[j];
+//        d.fdata["fxfx"] = values[j];
+      if (timePeriodMinute == -180) {
+        d.fdata["fxfx_189"] = values[j];
+      } else if (timePeriodMinute == -360) {
+        d.fdata["fxfx_360"] = values[j];
+      }
       break;
 
       //   12104  DRY BULB TEMPERATURE AT 2M (16 bits), K->Celsius
@@ -632,21 +655,21 @@ bool ObsBufr::get_diana_data(int ktdexl, int *ktdexp, double* values,
 
       //   12014  MAX TEMPERATURE AT 2M, K->Celsius
     case 12014:
-      if (values[j] < bufrMissing && obsTime.hour() == 18)
+      if (values[j] < bufrMissing && hour == 18)
         d.fdata["TxTn"] = values[j] - t0;
       break;
       //   12111  MAX TEMPERATURE AT HEIGHT AND OVER PERIOD SPECIFIED (16 bits), K->Celsius
       //   12011  MAX TEMPERATURE AT HEIGHT AND OVER PERIOD SPECIFIED (12 bits), K->Celsius
     case 12111:
     case 12011:
-      if (!d.fdata.count("TxTn") && values[j] < bufrMissing && obsTime.hour()
+      if (!d.fdata.count("TxTn") && values[j] < bufrMissing && hour
           == 18 && timePeriodHour == -12 && timeDisplacement == 0)
         d.fdata["TxTn"] = values[j] - t0;
       break;
 
       //   12015  MIN TEMPERATURE AT 2M, K->Celsius
     case 12015:
-      if (values[j] < bufrMissing && obsTime.hour() == 6)
+      if (values[j] < bufrMissing && hour == 6)
         d.fdata["TxTn"] = values[j] - t0;
       break;
 
@@ -654,7 +677,7 @@ bool ObsBufr::get_diana_data(int ktdexl, int *ktdexp, double* values,
       //   12012  MIN TEMPERATURE AT HEIGHT AND OVER PERIOD SPECIFIED (12 bits), K->Celsius
     case 12112:
     case 12012:
-      if (!d.fdata.count("TxTn") && values[j] < bufrMissing && obsTime.hour()
+      if (!d.fdata.count("TxTn") && values[j] < bufrMissing && hour
           == 6 && timePeriodHour == -12 && timeDisplacement == 0)
         d.fdata["TxTn"] = values[j] - t0;
       break;
@@ -703,7 +726,7 @@ bool ObsBufr::get_diana_data(int ktdexl, int *ktdexp, double* values,
     case 13021:
       if (values[j] < bufrMissing) {
 	          d.fdata["RRR_6"] = values[j];
-      }		
+      }
       break;
 
       //13022 Total precipitation past 12 hour
@@ -723,19 +746,19 @@ bool ObsBufr::get_diana_data(int ktdexl, int *ktdexp, double* values,
       // 13011 Total precipitation / total water equivalent of snow
     case 13011:
       if (values[j] < bufrMissing) {
-          d.fdata["RRR_accum"] =-1 * timePeriodHour;
-		            d.fdata["RRR"] = values[j];
-	        if (timePeriodHour == -24 ) {
-	            d.fdata["RRR_24"] = values[j];
-            } else if (timePeriodHour == -12 ) {
-	            d.fdata["RRR_12"] = values[j];
-            } else if (timePeriodHour == -6 ) {
-	            d.fdata["RRR_6"] = values[j];
-            } else if (timePeriodHour == -3 ) {
-	            d.fdata["RRR_3"] = values[j];
-            } else if (timePeriodHour == -1 ) {
-	            d.fdata["RRR_1"] = values[j];
-	            }
+//        d.fdata["RRR_accum"] =-1 * timePeriodHour;
+//        d.fdata["RRR"] = values[j];
+        if (timePeriodHour == -24 ) {
+          d.fdata["RRR_24"] = values[j];
+        } else if (timePeriodHour == -12 ) {
+          d.fdata["RRR_12"] = values[j];
+        } else if (timePeriodHour == -6 ) {
+          d.fdata["RRR_6"] = values[j];
+        } else if (timePeriodHour == -3 ) {
+          d.fdata["RRR_3"] = values[j];
+        } else if (timePeriodHour == -1 ) {
+          d.fdata["RRR_1"] = values[j];
+        }
       }
       break;
 
@@ -1036,14 +1059,14 @@ bool ObsBufr::get_diana_data_level(int ktdexl, int *ktdexp, double* values,
     const char cvals[][80], int len_cvals, int subset, int kelem, ObsData &d,
     int level)
 {
-  //  cerr <<"get_diana_data"<<endl;
+//    cerr <<"get_diana_data"<<endl;
   d.fdata.clear();
   d.id.clear();
   d.zone = 0;
 
   // constants for changing to met.no units
   const double pa2hpa = 0.01;
-  const double t0 = 273.2; // 273.15 ?????????
+  const double t0 = 273.15;
 
   int wmoBlock = 0;
   int wmoStation = 0;
@@ -1226,7 +1249,7 @@ bool ObsBufr::get_data_level(int ktdexl, int *ktdexp, double* values,
 
   // constants for changing to met.no units
   const double pa2hpa = 0.01;
-  const double t0 = 273.2; // 273.15 ????????
+  const double t0 = 273.15;
   const float rad = 3.141592654 / 180.;
   const double ms2knots = 3600.0 / 1852.0;
 
@@ -1314,14 +1337,18 @@ bool ObsBufr::get_data_level(int ktdexl, int *ktdexp, double* values,
         ii = 0;
         break;
 
+
+
         // 1011  SHIP OR MOBILE LAND STATION IDENTIFIER, CCITTIA5 (ascii chars)
+    case 1006:
       case 1011:
-		c=0;
-		station.clear();
-		while (c<5 && int(cvals[j][c])!=0 && int(cvals[j][c])!=32) {
-		  station += cvals[i][c];
-		  c++;
-		}
+
+{
+      station.clear();
+      int iindex = int(values[j]) / 1000 - 1;
+      for (int k = 0; k < 6; k++) {
+        station += cvals[iindex][k];
+      }
 		if( strStation != station ) {
 		  return false;
 		}
@@ -1331,6 +1358,7 @@ bool ObsBufr::get_data_level(int ktdexl, int *ktdexp, double* values,
 		}
 		if(station.exists()){
 			ii=0;
+		}
 		}
         break;
 

src/diObsManager.cc
@@ -592,8 +592,6 @@ bool ObsManager::updateTimesfromFile(miString obsType)
 {
   //Making list of file names and times, opening all files
 
-  //  cerr <<"Finner tider fra fil:"<<endl;
-
   obsType= obsType.downcase();
 
   if (Prod.find(obsType)==Prod.end())
@@ -604,10 +602,10 @@ bool ObsManager::updateTimesfromFile(miString obsType)
 #ifdef ROADOBS
 if (Prod[obsType].obsformat == ofmt_roadobs)
   {
-	// Due to the fact that we have a database insteda of an archive,
-	// we maust fake the behavoir of anarchive
+	// Due to the fact that we have a database instead of an archive,
+	// we must fake the behavoir of anarchive
 	// Assume that all stations report every hour
-	// firt, get the current time.
+	// first, get the current time.
 	miTime now = miTime::nowTime();
 	miClock nowClock = now.clock();
 	miDate nowDate = now.date();
@@ -866,7 +864,7 @@ ObsDialogInfo ObsManager::initDialog()
   psynop.button.push_back(addButton("Wind","Wind (direction and speed)"));
   psynop.button.push_back(addButton("TTT","Temperature (2m)",-50,50));
   psynop.button.push_back(addButton("TdTdTd","Dew point temperature",-50,50));
-  psynop.button.push_back(addButton("PPPP","Pressure ",950,1050));
+  psynop.button.push_back(addButton("PPPP","Pressure ",100,1050));
   psynop.button.push_back(addButton("ppp"," 3 hour pressure change",-10,10));
   psynop.button.push_back(addButton("a","Characteristic of pressure tendency",0,9));
   psynop.button.push_back(addButton("h","height of base of cloud",1,9));
@@ -901,6 +899,7 @@ ObsDialogInfo ObsManager::initDialog()
   obsformat.push_back(ofmt_dribu);
   obsformat.push_back(ofmt_metar);
   obsformat.push_back(ofmt_satob);
+  obsformat.push_back(ofmt_aireps);
   addType(psynop,obsformat);
 
   dialog.plottype.push_back(psynop);
@@ -968,6 +967,7 @@ ObsDialogInfo ObsManager::initDialog()
   obsformat.push_back(ofmt_dribu);
   obsformat.push_back(ofmt_metar);
   obsformat.push_back(ofmt_satob);
+  obsformat.push_back(ofmt_aireps);
   addType(plist,obsformat);
 
   dialog.plottype.push_back(plist);
@@ -978,7 +978,7 @@ ObsDialogInfo ObsManager::initDialog()
   ObsDialogInfo::PlotType ppressure;
   ppressure.name="Pressure";
   ppressure.misc =
-    "allAirepsLevels=true markerboxVisible asFieldButton orientation more_times";
+    "markerboxVisible asFieldButton orientation more_times";
   ppressure.criteriaList = criteriaList["pressure"];
 
   ppressure.pressureLevels = levels;
@@ -998,7 +998,6 @@ ObsDialogInfo ObsManager::initDialog()
   obsformat.clear();
   obsformat.push_back(ofmt_temp);
   obsformat.push_back(ofmt_pilot);
-  obsformat.push_back(ofmt_aireps);
   addType(ppressure,obsformat);
 
   dialog.plottype.push_back(ppressure);
@@ -1029,7 +1028,7 @@ ObsDialogInfo ObsManager::initDialog()
         }
       } else {
         psingle.misc =
-          "allAirepsLevels=true markerboxVisible asFieldButton orientation more_times";
+          "markerboxVisible asFieldButton orientation more_times";
         psingle.criteriaList = criteriaList["pressure"];
 
         psingle.pressureLevels = levels;
@@ -1155,7 +1154,7 @@ ObsDialogInfo ObsManager::initDialog()
   dialog.plottype.push_back(pocea);
 
 
-  //+++++++++Plot type = Tidevann+++++++++++++++
+  //+++++++++Plot type = Tide +++++++++++++++
 
   ObsDialogInfo::PlotType ptide;
   ptide.name="Tide";
@@ -1163,9 +1162,6 @@ ObsDialogInfo ObsManager::initDialog()
   ptide.criteriaList = criteriaList["tide"];
 
   ptide.button.push_back(addButton("Pos","Position",0,0,true));
-  ptide.button.push_back(addButton("Wind","Wind (direction and speed)"));
-  ptide.button.push_back(addButton("dd","wind direction",0,360,true));
-  ptide.button.push_back(addButton("ff","wind speed)",0,100,true));
   ptide.button.push_back(addButton("TE","Tide",-10,10,true));
   ptide.button.push_back(addButton("Id","Identification",0,0,true));
   ptide.button.push_back(addButton("Date","Date(mm-dd)",0,0));
@@ -1633,8 +1629,8 @@ bool ObsManager::parseSetup(SetupParser &sp)
   parameter= "Wind,TTT,TdTdTd,PPPP,ppp,a,h,VV,N,RRR,ww,W1,W2,Nh,Cl,Cm,Ch,vs,ds,TwTwTw,PwaHwa,dw1dw1,Pw1Hw1,TxTn,sss,911ff,s,fxfx,Id,St.no(3),St.no(5),Pos,dd,ff,T_red,Date,Time,Height,Zone,RRR_1,RRR_6,RRR_12,RRR_24";
   defProd["synop"].parameter= parameter.split(",");
   defProd["aireps"].obsformat= ofmt_aireps;
-  defProd["aireps"].timeRangeMin=-180;
-  defProd["aireps"].timeRangeMax= 180;
+  defProd["aireps"].timeRangeMin=-30;
+  defProd["aireps"].timeRangeMax= 30;
   defProd["aireps"].synoptic= false;
   parameter= "Pos,dd,ff,Wind,TTT,TdTdTd,PPPP,Id,Date,Time,HHH";
   defProd["aireps"].parameter= parameter.split(",");
@@ -1651,8 +1647,8 @@ bool ObsManager::parseSetup(SetupParser &sp)
   parameter= "Pos,dd,ff,Wind,TTT,TdTdTd,PPPP,ppp,a,TwTwTw,Id,Date,Time";
   defProd["dribu"].parameter= parameter.split(",");
   defProd["temp"].obsformat= ofmt_temp;
-  defProd["temp"].timeRangeMin=-180;
-  defProd["temp"].timeRangeMax= 180;
+  defProd["temp"].timeRangeMin=-30;
+  defProd["temp"].timeRangeMax= 30;
   defProd["temp"].synoptic= true;
   parameter= "Pos,dd,ff,Wind,TTT,TdTdTd,PPPP,Id,Date,Time,HHH";
   defProd["temp"].parameter= parameter.split(",");
@@ -1665,8 +1661,8 @@ bool ObsManager::parseSetup(SetupParser &sp)
   defProd["tide"].timeRangeMax= 180;
   defProd["tide"].synoptic= true;
   defProd["pilot"].obsformat= ofmt_pilot;
-  defProd["pilot"].timeRangeMin=-180;
-  defProd["pilot"].timeRangeMax= 180;
+  defProd["pilot"].timeRangeMin=-30;
+  defProd["pilot"].timeRangeMax= 30;
   defProd["pilot"].synoptic= true;
   parameter= "Pos,dd,ff,Wind,TTT,TdTdTd,PPPP,Id,Date,Time,HHH";
   defProd["pilot"].parameter= parameter.split(",");

src/diObsPlot.h
@@ -247,6 +247,8 @@ private:
   int vis_direction(float dv);
   void amountOfClouds(int16, int16, float,float);
   void checkAccumulationTime(ObsData &dta);
+  void checkGustTime(ObsData &dta);
+  void checkMaxWindTime(ObsData &dta);
   void plotSynop(int index);
   void plotList(int index);
   void plotAscii(int index);

commit 994da2e3f7a76f5f4462b0da0093ae60a4af1700

Fixing bug introduced in r2160 For the parameters RRR,fxfx and 911ff, accumulation (max value) time must be selected before the criterias are checked.
git-svn-id: https://svn.met.no/diana/trunk@2714 0ccb8871-b436-41a3-ae7d-e3ac59973202

src/diObsPlot.cc
@@ -2390,6 +2390,17 @@ void ObsPlot::plotList(int index)
   glColor4ubv(origcolour.RGBA());
   colour = origcolour;
 
+  //Select parameter with correct accumulation/max value interval
+  if (pFlag.count("911ff")) {
+    checkGustTime(dta);
+  }
+  if (pFlag.count("rrr")) {
+    checkAccumulationTime(dta);
+  }
+  if (pFlag.count("fxfx")) {
+    checkMaxWindTime(dta);
+  }
+
   if (tccriteria)
     checkTotalColourCriteria(index);
 
@@ -2652,7 +2663,6 @@ void ObsPlot::plotList(int index)
   }
   if (pFlag.count("rrr")) {
     ypos -= yStep;
-    checkAccumulationTime(dta);
     if ((f_p = dta.fdata.find("RRR")) != dta.fdata.end()) {
       if (ccriteria)
         checkColourCriteria("RRR", f_p->second);
@@ -2858,7 +2868,6 @@ void ObsPlot::plotList(int index)
   }
   if (pFlag.count("911ff")) {
     ypos -= yStep;
-    checkGustTime(dta);
     if ((f_p = dta.fdata.find("911ff")) != dta.fdata.end()) {
       if (ccriteria)
         checkColourCriteria("911ff", f_p->second);
@@ -2879,7 +2888,6 @@ void ObsPlot::plotList(int index)
   }
   if (pFlag.count("fxfx")) {
     ypos -= yStep;
-    checkMaxWindTime(dta);
     if ((f_p = dta.fdata.find("fxfx")) != dta.fdata.end()) {
       if (ccriteria)
         checkColourCriteria("fxfx", f_p->second);
@@ -3922,6 +3930,17 @@ void ObsPlot::plotSynop(int index)
   bool timeFlag = (pFlag.count("time") && dta.zone == 99);
   bool precip = (dta.fdata.count("ix") && dta.fdata["ix"] == -1);
 
+  //Select parameter with correct accumulation/max value interval
+  if (pFlag.count("911ff")) {
+    checkGustTime(dta);
+  }
+  if (pFlag.count("rrr")) {
+    checkAccumulationTime(dta);
+  }
+  if (pFlag.count("fxfx")) {
+    checkMaxWindTime(dta);
+  }
+
   //reset colour
   glColor4ubv(origcolour.RGBA());
   colour = origcolour;
@@ -4113,7 +4132,6 @@ void ObsPlot::plotSynop(int index)
   //Precipitation - RRR
   if (pFlag.count("rrr") && !(dta.zone == 99 && dta.fdata.count("ds")
       && dta.fdata.count("vs"))) {
-    checkAccumulationTime(dta);
     if ((f_p = dta.fdata.find("RRR")) != fend) {
       if (ccriteria)
         checkColourCriteria("RRR", f_p->second);
@@ -4167,7 +4185,6 @@ void ObsPlot::plotSynop(int index)
 
   // Maximum wind speed (gusts) - 911ff
   if (pFlag.count("911ff") ) {
-    checkGustTime(dta);
     if((f_p = dta.fdata.find("911ff")) != fend) {
       if (ccriteria)
         checkColourCriteria("911ff", f_p->second);
@@ -4188,7 +4205,6 @@ void ObsPlot::plotSynop(int index)
 
   // Maximum wind speed
   if (pFlag.count("fxfx") ) {
-    checkMaxWindTime(dta);
     if ( (f_p = dta.fdata.find("fxfx")) != fend
         && !(dta.zone > 1 && dta.zone < 99)) {
       if (ccriteria)