1685245a3e401220a3ba1dad75a14c718453f340
Lisbeth Bergholt <lisbethb@met.no>
2013-08-23 07:47:17 +0000

    removed whitespace following trailing backslash
    git-svn-id: https://svn.met.no/diana/trunk@3640 0ccb8871-b436-41a3-ae7d-e3ac59973202

--- share/diana/diana.mk
+++ share/diana/diana.mk
@@ -175,7 +175,7 @@ dist_markers_DATA= share/diana/images/markers/x.xpm \
 	share/diana/images/markers/cc_black_obs.xpm \
 	share/diana/images/markers/cc_amber.xpm \
 	share/diana/images/markers/cc_amber_trend.xpm \
-	share/diana/images/markers/cc_amber_obs.xpm \ 
+	share/diana/images/markers/cc_amber_obs.xpm \
 	share/diana/images/markers/cc_default.xpm \
 	share/diana/images/markers/cc_default_trend.xpm \
 	share/diana/images/markers/cc_default_obs.xpm
