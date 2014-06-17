fc59635bccad81dd1b7058366d240ea990e9a881
Lisbeth Bergholt <lisbethb@met.no>
2014-06-17 12:40:34 +0000

    Bugfix: the source file should not be writeable. In order not to mix up the model based on the source file and the model based on the product file, the model based on the source file gets a prefix "SOURCE_"
    git-svn-id: https://svn.met.no/diana/trunk@4305 0ccb8871-b436-41a3-ae7d-e3ac59973202

--- src/diFieldEdit.cc
+++ src/diFieldEdit.cc
@@ -451,7 +451,7 @@ bool FieldEdit::readEditfield(const std::string& filename)
 
 
   //read input file
-  std::string modelName = filename;
+  std::string modelName = "SOURCE_" + filename;
   filenames.clear();
   filenames.push_back(filename);
 
@@ -466,12 +466,6 @@ bool FieldEdit::readEditfield(const std::string& filename)
 
   if ( format[0] == "felt" && !inputFieldConfig.empty() ) {
     config.push_back(inputFieldConfig);
-  }
-
-  option.clear();
-  if ( format[0] == "netcdf" )  {
-    std::string opt = "writeable=true";
-    option.push_back(opt);
   }
 
   fieldPlotManager->addGridCollection(fileType, modelName, filenames,
