metno/diana

commit a14e6cde6e04b883ae16a064f8bc6e9c03c2ade1

Support colour and alpha style props for text items
Summary:
The colour and alpha style properties of a text item may
now be interactively modified. This goes for the text itself,
the background area, and the border.

Test Plan:
Observere that the colour and alpha style properties
are interactively modifiable via the 'Edit Style' context menu for
the text item.

Reviewers: davidb

Reviewed By: davidb

Differential Revision: https://phab.met.no/D55

src/EditItems/drawingstylemanager.cc
@@ -35,6 +35,7 @@
 #include "EditItems/drawingstylemanager.h"
 #include "EditItems/drawingitembase.h"
 #include "EditItems/drawingsymbol.h"
+#include "EditItems/drawingtext.h"
 #include <QApplication>
 #include <QComboBox>
 #include <QPainter>
@@ -212,23 +213,28 @@ DrawingStyleManager::StyleCategory DSP_textcolour::styleCategory() const { retur
 DrawingStyleManager::LockCategory DSP_textcolour::lockCategory() const { return DrawingStyleManager::LockColour; }
 QVariant DSP_textcolour::parse(const QHash<QString, QString> &def) const { return parseColour(textColour(def)); }
 
+QString DSP_textalpha::name() { return "textalpha"; }
+DrawingStyleManager::StyleCategory DSP_textalpha::styleCategory() const { return DrawingStyleManager::General; }
+DrawingStyleManager::LockCategory DSP_textalpha::lockCategory() const { return DrawingStyleManager::LockAlpha; }
+QVariant DSP_textalpha::parse(const QHash<QString, QString> &def) const { return def.value(name(), "255").toInt(); }
+
 QString DSP_fontname::name() { return "fontname"; }
 DrawingStyleManager::StyleCategory DSP_fontname::styleCategory() const { return DrawingStyleManager::General; }
-QVariant DSP_fontname::parse(const QHash<QString, QString> &def) const { return def.value(name(), "SCALEFONT"); }
+QVariant DSP_fontname::parse(const QHash<QString, QString> &def) const { return def.value(name(), QString::fromStdString(PlotOptions::defaultFontName())); }
 
 QString DSP_fontface::name() { return "fontface"; }
 DrawingStyleManager::StyleCategory DSP_fontface::styleCategory() const { return DrawingStyleManager::General; }
-QVariant DSP_fontface::parse(const QHash<QString, QString> &def) const { return def.value(name(), "NORMAL"); }
+QVariant DSP_fontface::parse(const QHash<QString, QString> &def) const { return def.value(name(), QString::fromStdString(PlotOptions::defaultFontFace())); }
 
 QString DSP_fontsize::name() { return "fontsize"; }
 DrawingStyleManager::StyleCategory DSP_fontsize::styleCategory() const { return DrawingStyleManager::General; }
-QVariant DSP_fontsize::parse(const QHash<QString, QString> &def) const { return def.value(name(), "10"); }
+QVariant DSP_fontsize::parse(const QHash<QString, QString> &def) const { return def.value(name(), QString::number(PlotOptions::defaultFontSize())); }
 
 QString DrawingStyleProperty::lineColour(const QHash<QString, QString> &def) { return def.value(DSP_linecolour::name(), "black"); }
 QString DrawingStyleProperty::linePattern(const QHash<QString, QString> &def) { return def.value(DSP_linepattern::name(), "solid"); }
 QString DrawingStyleProperty::fillColour(const QHash<QString, QString> &def) { return def.value(DSP_fillcolour::name(), "128:128:128"); }
 QString DrawingStyleProperty::symbolColour(const QHash<QString, QString> &def) { return def.value(DSP_symbolcolour::name(), "black"); }
-QString DrawingStyleProperty::textColour(const QHash<QString, QString> &def) { return def.value(DSP_linecolour::name(), "black"); }
+QString DrawingStyleProperty::textColour(const QHash<QString, QString> &def) { return def.value(DSP_textcolour::name(), "black"); }
 
 QString DSP_objects::name() { return "objects"; }
 DrawingStyleManager::StyleCategory DSP_objects::styleCategory() const { return DrawingStyleManager::General; }
@@ -300,6 +306,7 @@ DrawingStyleManager::DrawingStyleManager()
   properties_[DrawingItemBase::Text].insert(DSP_fillcolour::name(), new DSP_fillcolour);
   properties_[DrawingItemBase::Text].insert(DSP_fillalpha::name(), new DSP_fillalpha);
   properties_[DrawingItemBase::Text].insert(DSP_textcolour::name(), new DSP_textcolour);
+  properties_[DrawingItemBase::Text].insert(DSP_textalpha::name(), new DSP_textalpha);
   properties_[DrawingItemBase::Text].insert(DSP_fontname::name(), new DSP_fontname);
   properties_[DrawingItemBase::Text].insert(DSP_fontface::name(), new DSP_fontface);
   properties_[DrawingItemBase::Text].insert(DSP_fontsize::name(), new DSP_fontsize);
@@ -392,11 +399,11 @@ void DrawingStyleManager::setComplexTextList(const QStringList &strings)
 
 void DrawingStyleManager::beginLine(DrawingItemBase *item)
 {
-  glPushAttrib(GL_LINE_BIT);
+  glPushAttrib(GL_LINE_BIT|GL_CURRENT_BIT);
 
   const QVariantMap style = getStyle(item);
 
-  const QString lpString = style.value("linepattern").toString();
+  const QString lpString = style.value(DSP_linepattern::name()).toString();
   bool ok = false;
   const ushort linePattern = lpString.toUShort(&ok);
   if (ok) {
@@ -418,12 +425,12 @@ void DrawingStyleManager::endLine(DrawingItemBase *item)
 {
   Q_UNUSED(item)
 
-  glPopAttrib(); // GL_LINE_BIT
+  glPopAttrib();
 }
 
 void DrawingStyleManager::beginFill(DrawingItemBase *item)
 {
-  glPushAttrib(GL_POLYGON_BIT);
+  glPushAttrib(GL_POLYGON_BIT|GL_CURRENT_BIT);
 
   QVariantMap style = getStyle(item);
 
@@ -463,7 +470,7 @@ void DrawingStyleManager::endFill(DrawingItemBase *item)
 {
   Q_UNUSED(item)
 
-  glPopAttrib(); // GL_POLYGON_BIT
+  glPopAttrib();
 }
 
 DrawingStyleManager::StyleCategory DrawingStyleManager::styleCategory(const DrawingItemBase::Category &itemCategory, const QString &name) const
@@ -534,7 +541,8 @@ QVariantMap DrawingStyleManager::getStyle(const DrawingItemBase *item) const
   // If the style is customised then parse the contents of the hash; otherwise
   // just return the style that corresponds to the style name.
   if ((item->category() == DrawingItemBase::PolyLine) ||
-      (item->category() == DrawingItemBase::Symbol))
+      (item->category() == DrawingItemBase::Symbol) ||
+      (item->category() == DrawingItemBase::Text))
     return parse(item->category(), styleProperties);
   else {
     const QString styleName = item->property("style:type").toString();
@@ -547,10 +555,10 @@ QStringList DrawingStyleManager::getComplexTextList() const
   return complexTextList;
 }
 
-void DrawingStyleManager::drawLines(const DrawingItemBase *item, const QList<QPointF> &points, int z) const
+void DrawingStyleManager::drawLines(const DrawingItemBase *item, const QList<QPointF> &points, int z, bool forceClosed) const
 {
   QVariantMap style = getStyle(item);
-  bool closed = style.value(DSP_closed::name()).toBool();
+  bool closed = forceClosed || style.value(DSP_closed::name()).toBool();
 
   if (closed)
     glBegin(GL_LINE_LOOP);
@@ -780,7 +788,7 @@ void DrawingStyleManager::drawDecoration(const QVariantMap &style, const QString
 void DrawingStyleManager::fillLoop(const DrawingItemBase *item, const QList<QPointF> &points) const
 {
   QVariantMap style = getStyle(item);
-  bool closed = style.value("closed").toBool();
+  bool closed = style.value(DSP_closed::name()).toBool();
 
   QList<QPointF> points_;
   if (style.value(DSP_linesmooth::name()).toBool())
@@ -941,33 +949,50 @@ const QList<QPointF> DrawingStyleManager::getDecorationLines(const QList<QPointF
   return new_points;
 }
 
-void DrawingStyleManager::beginText(const DrawingItemBase *item, const PlotOptions &poptions)
+void DrawingStyleManager::drawText(const DrawingItemBase *item_) const
 {
+  const DrawingItem_Text::Text *item = dynamic_cast<const DrawingItem_Text::Text *>(item_);
+  if (!item)
+    qFatal("DrawingStyleManager::drawText(): not a text item");
+
   QVariantMap style = getStyle(item);
-  QColor textColour = style.value("textcolour").value<QColor>();
+
+  const QColor textColour = style.value(DSP_textcolour::name()).value<QColor>();
+  bool alphaOk;
+  const int alpha = style.value(DSP_textalpha::name()).toInt(&alphaOk);
   if (textColour.isValid())
-    glColor4ub(textColour.red(), textColour.green(), textColour.blue(),
-               textColour.alpha());
+    glColor4ub(textColour.red(), textColour.green(), textColour.blue(), alphaOk ? alpha : 255);
   else
-    glColor3f(0.0, 0.0, 0.0);
+    glColor4ub(0, 0, 0, 255);
+
+  setFont(item);
+  const float scale = PlotModule::instance()->getStaticPlot()->getPhysWidth() / PlotModule::instance()->getStaticPlot()->getPlotSize().width();
+
+  const float x = item->getPoints().at(0).x() + item->margin();
+  float y = item->getPoints().at(0).y() - item->margin();
+
+  foreach (QString text, item->text()) {
+    const QSizeF size = item->getStringSize(text);
+    glPushMatrix();
+    glTranslatef(x, y - size.height(), 0);
+    glScalef(scale, scale, 1.0);
+    PlotModule::instance()->getStaticPlot()->getFontPack()->drawStr(text.toStdString().c_str(), 0, 0, 0);
+    glPopMatrix();
+    y -= size.height() * (1.0 + item->spacing());
+  }
 }
 
-void DrawingStyleManager::setFont(const DrawingItemBase *item, const PlotOptions &poptions)
+void DrawingStyleManager::setFont(const DrawingItemBase *item) const
 {
   QVariantMap style = getStyle(item);
 
   // Fill in the default font settings from the plot options object. These
   // will be overridden if equivalent properties are found.
-  QString fontName = style.value("fontname", QString::fromStdString(poptions.fontname)).toString();
-  QString fontFace = style.value("fontface", QString::fromStdString(poptions.fontface)).toString();
-  float fontSize = style.value("fontsize", poptions.fontsize).toFloat();
+  const QString fontName = style.value(DSP_fontname::name(), QString::fromStdString(PlotOptions::defaultFontName())).toString();
+  const QString fontFace = style.value(DSP_fontface::name(), QString::fromStdString(PlotOptions::defaultFontFace())).toString();
+  const float fontSize = style.value(DSP_fontsize::name(), PlotOptions::defaultFontSize()).toFloat();
 
   PlotModule::instance()->getStaticPlot()->getFontPack()->set(fontName.toStdString(), fontFace.toStdString(), fontSize);
-}
-
-void DrawingStyleManager::endText(const DrawingItemBase *item)
-{
-  Q_UNUSED(item)
 }
 
 void DrawingStyleManager::drawSymbol(const DrawingItemBase *item) const
@@ -1038,10 +1063,10 @@ QImage DrawingStyleManager::toImage(const DrawingItemBase::Category &category, c
   }
 
   QVariantMap thisStyle = getStyle(category, name);
-  QStringList objects = thisStyle.value("objects").toStringList();
-  QStringList values = thisStyle.value("values").toStringList();
-  QStringList styles = thisStyle.value("styles").toStringList();
-  QString layout = thisStyle.value("layout").toString();
+  QStringList objects = thisStyle.value(DSP_objects::name()).toStringList();
+  QStringList values = thisStyle.value(DSP_values::name()).toStringList();
+  QStringList styles = thisStyle.value(DSP_styles::name()).toStringList();
+  QString layout = thisStyle.value(DSP_layout::name()).toString();
 
   QList<QImage> images;
   QList<float> positions;

src/EditItems/drawingstylemanager.h
@@ -42,7 +42,6 @@
 #include <QColor>
 
 #include "diFontManager.h"
-#include "diPlotOptions.h"
 #include "EditItems/drawingitembase.h"
 
 class DrawingStyleProperty;
@@ -70,12 +69,12 @@ public:
   void endLine(DrawingItemBase *item);
   void beginFill(DrawingItemBase *item);
   void endFill(DrawingItemBase *item);
-  void beginText(const DrawingItemBase *item, const PlotOptions &poptions);
-  void endText(const DrawingItemBase *item);
 
-  void drawLines(const DrawingItemBase *item, const QList<QPointF> &points, int z = 0) const;
+  void drawText(const DrawingItemBase *) const;
+
+  void drawLines(const DrawingItemBase *item, const QList<QPointF> &points, int z = 0, bool = false) const;
   void fillLoop(const DrawingItemBase *item, const QList<QPointF> &points) const;
-  void setFont(const DrawingItemBase *item, const PlotOptions &poptions);
+  void setFont(const DrawingItemBase *item) const;
 
   void drawSymbol(const DrawingItemBase *) const;
 
@@ -318,6 +317,16 @@ private:
 };
 
 class DSP_textcolour : public DrawingStyleProperty
+{
+public:
+  static QString name();
+private:
+  virtual DrawingStyleManager::StyleCategory styleCategory() const;
+  virtual DrawingStyleManager::LockCategory lockCategory() const;
+  virtual QVariant parse(const QHash<QString, QString> &) const;
+};
+
+class DSP_textalpha : public DrawingStyleProperty
 {
 public:
   static QString name();

src/EditItems/drawingtext.cc
@@ -40,8 +40,11 @@ namespace DrawingItem_Text {
 
 Text::Text()
 {
-  margin_ = 4;
-  spacing_ = 0.5;
+  propertiesRef().insert("style:margin", defaultMargin());
+  propertiesRef().insert("style:spacing", defaultSpacing());
+  propertiesRef().insert("style:fontname", QString::fromStdString(PlotOptions::defaultFontName()));
+  propertiesRef().insert("style:fontface", QString::fromStdString(PlotOptions::defaultFontFace()));
+  propertiesRef().insert("style:fontsize", PlotOptions::defaultFontSize());
 }
 
 Text::~Text()
@@ -50,8 +53,7 @@ Text::~Text()
 
 void Text::draw()
 {
-  QStringList lines_ = text();
-  if (points_.isEmpty() || lines_.isEmpty())
+  if (points_.isEmpty() || text().isEmpty())
     return;
 
   DrawingStyleManager *styleManager = DrawingStyleManager::instance();
@@ -65,30 +67,13 @@ void Text::draw()
   styleManager->fillLoop(this, points);
   styleManager->endFill(this);
 
-  // Draw the outline using the border colour and line pattern defined in
-  // the style.
+  // Draw the outline using the border colour and line pattern defined in the style.
   styleManager->beginLine(this);
-  styleManager->drawLines(this, points);
+  styleManager->drawLines(this, points, 0, true);
   styleManager->endLine(this);
 
-  styleManager->beginText(this, poptions);
-  styleManager->setFont(this, poptions);
-  float scale = PlotModule::instance()->getStaticPlot()->getPhysWidth() / PlotModule::instance()->getStaticPlot()->getPlotSize().width();
-
-  float x = points.at(0).x() + margin_;
-  float y = points.at(0).y() - margin_;
-
-  foreach (QString text, lines_) {
-    QSizeF size = getStringSize(text);
-    glPushMatrix();
-    glTranslatef(x, y - size.height(), 0);
-    glScalef(scale, scale, 1.0);
-    PlotModule::instance()->getStaticPlot()->getFontPack()->drawStr(text.toStdString().c_str(), 0, 0, 0);
-    glPopMatrix();
-    y -= size.height() * (1.0 + spacing_);
-  }
-
-  styleManager->endText(this);
+  // Draw the text itself.
+  styleManager->drawText(this);
 }
 
 QSizeF Text::getStringSize(const QString &text, int index) const
@@ -97,7 +82,7 @@ QSizeF Text::getStringSize(const QString &text, int index) const
     index = text.size();
 
   DrawingStyleManager *styleManager = DrawingStyleManager::instance();
-  styleManager->setFont(this, poptions);
+  styleManager->setFont(this);
 
   float width, height;
   if (!PlotModule::instance()->getStaticPlot()->getFontPack()->getStringSize(text.left(index).toStdString().c_str(), width, height))
@@ -109,10 +94,31 @@ QSizeF Text::getStringSize(const QString &text, int index) const
 
   if (height == 0) {
     PlotModule::instance()->getStaticPlot()->getFontPack()->getStringSize("X", width, height);
-    size.setHeight(qMax(scale * height, poptions.fontsize));
+    size.setHeight(qMax(scale * height, fontSize()));
   }
 
   return size;
+}
+
+float Text::margin() const
+{
+  bool ok = false;
+  const float val = properties().value("style:margin", defaultMargin()).toFloat(&ok);
+  return ok ? val : defaultMargin();
+}
+
+float Text::spacing() const
+{
+  bool ok = false;
+  const float val = properties().value("style:spacing", defaultSpacing()).toFloat(&ok);
+  return ok ? val : defaultSpacing();
+}
+
+float Text::fontSize() const
+{
+  bool ok = false;
+  const float val = properties().value("style:fontsize", PlotOptions::defaultFontSize()).toFloat(&ok);
+  return ok ? val : PlotOptions::defaultFontSize();
 }
 
 DrawingItemBase::Category Text::category() const
@@ -128,9 +134,8 @@ QStringList Text::text() const
 QRectF Text::drawingRect() const
 {
   QRectF bbox = boundingRect();
-  if (property("alignment", Qt::AlignCenter) == Qt::AlignCenter)
-    bbox.translate(-bbox.width()/2, bbox.height()/2);
-
+  //  if (property("alignment", Qt::AlignCenter) == Qt::AlignCenter) ### This must also be considered in DrawingStyleManager::drawText()
+  //    bbox.translate(-bbox.width()/2, bbox.height()/2);
   return bbox;
 }
 
@@ -145,13 +150,13 @@ void Text::updateRect()
     QString text = lines_.at(i);
     QSizeF size = getStringSize(text);
     width = qMax(width, size.width());
-    size.setHeight(qMax(size.height(), qreal(poptions.fontsize)));
+    size.setHeight(qMax(size.height(), qreal(fontSize())));
     y -= size.height();
     if (i < lines_.size() - 1)
-      y -= size.height() * spacing_;
+      y -= size.height() * spacing();
   }
 
-  points_[1] = QPointF(x + width + 2 * margin_, y - 2 * margin_);
+  points_[1] = QPointF(x + width + 2 * margin(), y - 2 * margin());
 }
 
 void Text::setText(const QStringList &lines)
@@ -165,8 +170,6 @@ QDomNode Text::toKML(const QHash<QString, QString> &extraExtData) const
   QHash<QString, QString> extra;
   QStringList lines = text();
   extra["text"] = lines.join("\n");
-  extra["margin"] = QString::number(margin_);
-  extra["spacing"] = QString::number(spacing_);
   return DrawingItemBase::toKML(extra.unite(extraExtData));
 }
 
@@ -174,8 +177,6 @@ void Text::fromKML(const QHash<QString, QString> &extraExtData)
 {
   DrawingManager::instance()->setFromLatLonPoints(*this, getLatLonPoints());
   setText(extraExtData.value("met:text", "").split("\n"));
-  margin_ = extraExtData.value("met:margin", "4").toInt();
-  spacing_ = extraExtData.value("met:spacing", "0.5").toFloat();
 }
 
 } // namespace

src/EditItems/drawingtext.h
@@ -32,11 +32,10 @@
 #ifndef DRAWINGTEXT_H
 #define DRAWINGTEXT_H
 
-#include "diPlot.h"
 #include "drawingitembase.h"
 
 namespace DrawingItem_Text {
-class Text : public Plot, public DrawingItemBase
+class Text : public DrawingItemBase
 {
 public:
   Text();
@@ -55,11 +54,14 @@ public:
   // Returns the category of the item as required by the style manager.
   virtual Category category() const;
 
-protected:
+  float margin() const;
+  float spacing() const;
+  float fontSize() const;
   QSizeF getStringSize(const QString &text, int index = -1) const;
 
-  int margin_;
-  float spacing_;
+private:
+  static float defaultMargin() { return 4; }
+  static float defaultSpacing() { return 0.5; }
 };
 
 } // namespace

src/EditItems/style.cc
@@ -413,6 +413,24 @@ private:
   virtual IndexedEditor *createEditor() { return new IntRangeEditor(0, 255); }
 };
 
+class SPE_textcolour : public StylePropertyEditor
+{
+public:
+  virtual QString name() const { return DSP_textcolour::name(); }
+private:
+  virtual QString labelText() const { return "text colour"; }
+  virtual IndexedEditor *createEditor() { return new ComboBoxEditor(ColourBox(0, true, 0, tr("off").toStdString(),true)); }
+};
+
+class SPE_textalpha : public StylePropertyEditor
+{
+public:
+  virtual QString name() const { return DSP_textalpha::name(); }
+private:
+  virtual QString labelText() const { return "text alpha"; }
+  virtual IndexedEditor *createEditor() { return new IntRangeEditor(0, 255); }
+};
+
 // ... editors for more subtypes
 
 class EditStyleProperty
@@ -550,6 +568,18 @@ private:
   virtual StylePropertyEditor *createSpecialEditor() const { return new SPE_symbolalpha; }
 };
 
+class ESP_textcolour : public EditStyleProperty
+{
+private:
+  virtual StylePropertyEditor *createSpecialEditor() const { return new SPE_textcolour; }
+};
+
+class ESP_textalpha : public EditStyleProperty
+{
+private:
+  virtual StylePropertyEditor *createSpecialEditor() const { return new SPE_textalpha; }
+};
+
 StyleEditor::StyleEditor()
 {
   setWindowTitle(tr("Item Style"));
@@ -585,6 +615,8 @@ StyleEditor::StyleEditor()
   properties_.insert(DSP_decoration2_offset::name(), new ESP_decoration2_offset);
   properties_.insert(DSP_symbolcolour::name(), new ESP_symbolcolour);
   properties_.insert(DSP_symbolalpha::name(), new ESP_symbolalpha);
+  properties_.insert(DSP_textcolour::name(), new ESP_textcolour);
+  properties_.insert(DSP_textalpha::name(), new ESP_textalpha);
 }
 
 StyleEditor *StyleEditor::instance()

src/diPlotOptions.cc
@@ -82,7 +82,7 @@ PlotOptions::PlotOptions():
   plottype(fpt_contour), rotateVectors(1), discontinuous(0), contourShading(0),
   polystyle(poly_fill), arrowstyle(arrow_wind), h_align(align_left), v_align(align_bottom),
   alignX(0), alignY(0),
-  fontname("SCALEFONT"), fontface("NORMAL"), fontsize(10.0), precision(0),
+  fontname(defaultFontName()), fontface(defaultFontFace()), fontsize(defaultFontSize()), precision(0),
   dimension(1), enabled(true), overlay(0), contourShape(0), tableHeader(true),
   antialiasing(false), use_stencil(false), update_stencil(false), plot_under(false), maxDiagonalInMeters(-1.0)
 {

src/diPlotOptions.h
@@ -212,6 +212,10 @@ public:
   static std::vector< std::vector<std::string> >& getPlotTypes(){return plottypes;}
   static std::map< std::string, std::string > getEnabledOptions(){ return enabledOptions;}
 
+  static std::string defaultFontName() { return "SCALEFONT"; }
+  static std::string defaultFontFace() { return "NORMAL"; }
+  static float defaultFontSize() { return 10.0; }
+
 public:
   //! fill in values and "..." in a float vector (error if size==0)
   static std::vector<float> autoExpandFloatVector(const std::string&);

commit 27828ce432090389ff503e91782cee050d80877b

Fixed alpha bug introduced by a14e6cde6e04b883ae16a064f8bc6e9c03c2ade1

src/EditItems/drawingstylemanager.cc
@@ -374,7 +374,7 @@ void DrawingStyleManager::setComplexTextList(const QStringList &strings)
 
 void DrawingStyleManager::beginLine(DrawingItemBase *item)
 {
-  glPushAttrib(GL_LINE_BIT|GL_CURRENT_BIT);
+  glPushAttrib(GL_LINE_BIT);
 
   const QVariantMap style = getStyle(item);
 
@@ -405,7 +405,7 @@ void DrawingStyleManager::endLine(DrawingItemBase *item)
 
 void DrawingStyleManager::beginFill(DrawingItemBase *item)
 {
-  glPushAttrib(GL_POLYGON_BIT|GL_CURRENT_BIT);
+  glPushAttrib(GL_POLYGON_BIT);
 
   QVariantMap style = getStyle(item);
 