493dc3124333c86e6c06b64aa18fdc56d29873ea
Alexander Bürger <alexanderb@met.no>
2015-02-10 15:00:00 +0100

    vcross: layer buttons show palette / arcs and line colour

--- src/vcross_qt/qtVcrossWindow.cc
+++ src/vcross_qt/qtVcrossWindow.cc
@@ -317,23 +317,7 @@ void VcrossWindow::onFieldAdded(int position)
 {
   METLIBS_LOG_SCOPE();
   QBoxLayout* lbl = static_cast<QBoxLayout*>(ui->layerButtons->layout());
-  const int n = lbl->count();
-  METLIBS_LOG_DEBUG(LOGVAL(position) << LOGVAL(n));
-  for (int i=position; i<n; ++i) {
-    QWidgetItem* wi = static_cast<QWidgetItem*>(lbl->itemAt(i));
-    VcrossLayerButton *button = static_cast<VcrossLayerButton*>(wi->widget());
-    button->setPosition(i+1, i==n-1);
-  }
-
-  const std::string model = vcrossm->getModelAt(position),
-      reftime = vcrossm->getReftimeAt(position).isoTime(),
-      field = vcrossm->getFieldAt(position);
-  const QString label = tr("Model: %1 Reftime: %2 Field: %3")
-      .arg(QString::fromStdString(model))
-      .arg(QString::fromStdString(reftime))
-      .arg(QString::fromStdString(field));
-
-  VcrossLayerButton* button = new VcrossLayerButton(label, position, this);
+  VcrossLayerButton* button = new VcrossLayerButton(vcrossm, position, this);
   connect(button, SIGNAL(triggered(int, int)), SLOT(onFieldAction(int, int)));
   lbl->insertWidget(position, button);
 
@@ -353,19 +337,13 @@ void VcrossWindow::onFieldVisibilityChanged(int position)
 void VcrossWindow::onFieldRemoved(int position)
 {
   METLIBS_LOG_SCOPE();
+#if 0
   QBoxLayout* lbl = static_cast<QBoxLayout*>(ui->layerButtons->layout());
-  const int n = lbl->count();
-  METLIBS_LOG_DEBUG(LOGVAL(position) << LOGVAL(n));
-  for (int i=position+1; i<n; ++i) {
-    QWidgetItem* wi = static_cast<QWidgetItem*>(lbl->itemAt(i));
-    VcrossLayerButton *button = static_cast<VcrossLayerButton*>(wi->widget());
-    button->setPosition(i-1, i==n-1);
-  }
-
   QWidgetItem* wi = static_cast<QWidgetItem*>(lbl->takeAt(position));
   QToolButton *button = static_cast<QToolButton*>(wi->widget());
   button->deleteLater();
   delete wi;
+#endif
 
   repaintPlotIfNotInGroup();
 }
