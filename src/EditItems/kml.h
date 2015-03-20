cf15a2e29985ba851297426efae483e9040f797f
David Boddie <david.boddie@met.no>
2015-03-20 14:58:53 +0100

    Ensured that all met: extended data properties in KML files are stored as item properties.
    Summary:
    Iterate over all extended data properties when reading KML files,
    treating certain met: properties specially, and storing the rest.
    
    Test Plan: Jenkins
    
    Reviewers: #diana, lisbethb
    
    Reviewed By: #diana, lisbethb
    
    Differential Revision: https://phab.met.no/D420

--- src/EditItems/kml.h
+++ src/EditItems/kml.h
@@ -171,24 +171,32 @@ static inline QList<QSharedPointer<EditItems::Layer> > createFromDomDocument(
 
     ditem->setProperty("srcFile", srcFileName);
 
-    if (pmExtData.contains("met:layerId"))  {
-      bool ok;
-      const int layerId = pmExtData.value("met:layerId").toInt(&ok);
-      if (!ok) {
-        *error = QString("failed to parse met:layerId as integer: %1").arg(pmExtData.value("met:layerId"));
-        return QList<QSharedPointer<EditItems::Layer> >();
-      }
-      ditem->setProperty("layerId", layerId);
-    }
+    // Keep all the met: properties, treating the layerId and joinId properties specially.
+    QHashIterator<QString, QString> it(pmExtData);
+    while (it.hasNext()) {
 
-    if (pmExtData.contains("met:joinId"))  {
-      bool ok;
-      const int joinId = pmExtData.value("met:joinId").toInt(&ok);
-      if (!ok) {
-        *error = QString("failed to parse met:joinId as integer: %1").arg(pmExtData.value("met:joinId"));
-        return QList<QSharedPointer<EditItems::Layer> >();
-      }
-      ditem->setProperty("joinId", joinId);
+      it.next();
+
+      if (it.key() == "met:layerId") {
+        bool ok;
+        const int layerId = it.value().toInt(&ok);
+        if (!ok) {
+          *error = QString("failed to parse met:layerId as integer: %1").arg(it.value());
+          return QList<QSharedPointer<EditItems::Layer> >();
+        }
+        ditem->setProperty("layerId", layerId);
+
+      } else if (it.key() == "met:joinId") {
+        bool ok;
+        const int joinId = it.value().toInt(&ok);
+        if (!ok) {
+          *error = QString("failed to parse met:joinId as integer: %1").arg(it.value());
+          return QList<QSharedPointer<EditItems::Layer> >();
+        }
+        ditem->setProperty("joinId", joinId);
+
+      } else if (it.key().startsWith("met:"))
+        ditem->setProperty(it.key().mid(4), it.value());
     }
 
     QMap<QString, QDomElement> ancElems;
