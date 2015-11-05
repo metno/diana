metno/diana

commit bb958bc238c5219ee752ecbfca340ab75d1a17a7

contour2: try to respect zorder
 * only in FieldPlot
 * undefined fill + lines in SHADE_BACKGROUND
 * filled contours in SHADE
 * lines and labels in LINES

src/diFieldPlot.cc
@@ -609,19 +609,25 @@ bool FieldPlot::getDataAnnotations(vector<string>& anno)
 void FieldPlot::plot(DiGLPainter* gl, PlotOrder zorder)
 {
   if (zorder == SHADE_BACKGROUND) {
+    // should be below all real fields
+    if (poptions.gridLines > 0)
+      plotGridLines(gl);
+    if (poptions.gridValue > 0)
+      plotNumbers(gl);
+
     plotUndefined(gl);
   } else if (zorder == SHADE) {
-    if (getShadePlot())
-      plotMe(gl);
+    if (plottype == fpt_contour2 || getShadePlot())
+      plotMe(gl, zorder);
   } else if (zorder == LINES) {
-    if (!getShadePlot() )
-      plotMe(gl);
+    if (plottype == fpt_contour2 || !getShadePlot())
+      plotMe(gl, zorder);
   } else if (zorder == OVERLAY) {
-    plotMe(gl);
+    plotMe(gl, zorder);
   }
 }
 
-bool FieldPlot::plotMe(DiGLPainter* gl)
+bool FieldPlot::plotMe(DiGLPainter* gl, PlotOrder zorder)
 {
   METLIBS_LOG_SCOPE();
   METLIBS_LOG_DEBUG(LOGVAL(getModelName()));
@@ -655,12 +661,6 @@ bool FieldPlot::plotMe(DiGLPainter* gl)
   else
     gl->Disable(DiGLPainter::gl_MULTISAMPLE);
 
-  // should be below all real fields
-  if (poptions.gridLines > 0)
-    plotGridLines(gl);
-  if (poptions.gridValue > 0)
-    plotNumbers(gl);
-
   if (poptions.use_stencil || poptions.update_stencil) {
     // Enable the stencil test for masking the field to be plotted or
     // updating the stencil.
@@ -679,7 +679,7 @@ bool FieldPlot::plotMe(DiGLPainter* gl)
   if (plottype == fpt_contour)
     ok = plotContour(gl);
   else if (plottype == fpt_contour2)
-    ok = plotContour2(gl);
+    ok = plotContour2(gl, zorder);
   else if (plottype == fpt_wind)
     ok = plotWind(gl);
   else if (plottype == fpt_wind_temp_fl)
@@ -2257,9 +2257,26 @@ bool FieldPlot::plotContour(DiGLPainter* gl)
 }
 
 //  plot scalar field as contour lines using new contour algorithm
-bool FieldPlot::plotContour2(DiGLPainter* gl)
+bool FieldPlot::plotContour2(DiGLPainter* gl, PlotOrder zorder)
 {
   METLIBS_LOG_SCOPE();
+
+  int paintMode;
+  if (zorder == SHADE_BACKGROUND) {
+    if (poptions.undefMasking <= 0)
+      return true;
+    paintMode = DianaLines::UNDEFINED;
+  } else if (zorder == SHADE) {
+    if (!poptions.contourShading)
+      return true;
+    paintMode = DianaLines::FILL;
+  } else if (zorder == LINES) {
+    paintMode = DianaLines::LINES_LABELS;
+  } else if (zorder == OVERLAY) {
+    paintMode = DianaLines::UNDEFINED | DianaLines::FILL | DianaLines::LINES_LABELS;
+  } else {
+    return true;
+  }
 
   if (not checkFields(1)) {
     METLIBS_LOG_ERROR("no fields or no field data");
@@ -2290,7 +2307,7 @@ bool FieldPlot::plotContour2(DiGLPainter* gl)
   {
     METLIBS_LOG_TIME("contour2");
     if (not poly_contour(nx, ny, ix1, iy1, ix2, iy2, fields[0]->data, x, y, gl,
-        poptions, fieldUndef))
+            poptions, fieldUndef, paintMode))
       METLIBS_LOG_ERROR("contour2 error");
   }
   if (poptions.extremeType != "None" && poptions.extremeType != "Ingen"
@@ -2972,13 +2989,17 @@ bool FieldPlot::plotUndefined(DiGLPainter* gl)
 {
   METLIBS_LOG_SCOPE();
 
-  if (!(poptions.undefMasking > 0 and plottype != fpt_contour2))
+  if (poptions.undefMasking <= 0)
     return false;
 
   if (not isEnabled())
     return false;
   if (not checkFields(1))
     return false;
+  if (plottype == fpt_contour2) {
+    plotContour2(gl, SHADE_BACKGROUND);
+    return true;
+  }
 
   const bool center_on_gridpoint = centerOnGridpoint()
       || plottype == fpt_contour; // old contour does some tricks with undefined values

src/diFieldPlot.h
@@ -103,7 +103,7 @@ private:
   int xAutoStep(float* x, float* y, int& ix1, int ix2, int iy, float sdist);
 
   // plotting methods
-  bool plotMe(DiGLPainter* gl);
+  bool plotMe(DiGLPainter* gl, PlotOrder zorder);
   bool plotWind(DiGLPainter* gl);
   bool plotWindAndValue(DiGLPainter* gl, bool flightlevelChart=false);
   bool plotValue(DiGLPainter* gl);
@@ -115,7 +115,7 @@ private:
       const float* colourfield, float& arrowlength);
 
   bool plotContour(DiGLPainter* gl);
-  bool plotContour2(DiGLPainter* gl);
+  bool plotContour2(DiGLPainter* gl, PlotOrder zorder);
 
   bool plotRaster(DiGLPainter* gl);
   const Colour* colourForValue(float value) const;

src/diPolyContouring.cc
@@ -293,6 +293,7 @@ private:
 
 DianaLines::DianaLines(const PlotOptions& poptions, const DianaLevels& levels)
   : mPlotOptions(poptions), mLevels(levels)
+  , mPaintMode(UNDEFINED | FILL | LINES_LABELS)
 {
 }
 
@@ -302,10 +303,13 @@ DianaLines::~DianaLines()
 
 void DianaLines::paint()
 {
-  paint_polygons();
-  paint_lines();
-  if (mPlotOptions.valueLabel)
-    paint_labels();
+  if ((mPaintMode & (UNDEFINED | FILL)) != 0)
+    paint_polygons();
+  if ((mPaintMode & (UNDEFINED | LINES_LABELS)) != 0) {
+    paint_lines();
+    if (mPlotOptions.valueLabel)
+      paint_labels();
+  }
 }
 
 void DianaLines::paint_polygons()
@@ -321,13 +325,14 @@ void DianaLines::paint_polygons()
   for (level_points_m::const_iterator it = m_polygons.begin(); it != m_polygons.end(); ++it) {
     contouring::level_t li = it->first;
     METLIBS_LOG_TIME(LOGVAL(li));
-    
+
     if (li == DianaLevels::UNDEF_LEVEL) {
-      if (mPlotOptions.undefMasking != 1)
+      if ((mPaintMode & UNDEFINED) == 0 || mPlotOptions.undefMasking != 1)
         continue;
       setFillColour(mPlotOptions.undefColour);
     } else {
-      if ((level_min != DianaLevels::UNDEF_LEVEL and li < level_min)
+      if ((mPaintMode & FILL) == 0
+          or (level_min != DianaLevels::UNDEF_LEVEL and li < level_min)
           or (level_max != DianaLevels::UNDEF_LEVEL and li >= level_max)
           or (mPlotOptions.zeroLine<=0 and li == 0))
       {
@@ -352,10 +357,12 @@ void DianaLines::paint_lines()
 {
   for (level_points_m::const_iterator it = m_lines.begin(); it != m_lines.end(); ++it) {
     if (it->first == DianaLevels::UNDEF_LEVEL) {
-      if (not mPlotOptions.undefMasking)
+      if (!mPlotOptions.undefMasking || (mPaintMode & UNDEFINED) == 0)
         continue;
       setLine(mPlotOptions.undefColour, mPlotOptions.undefLinetype, mPlotOptions.undefLinewidth);
     } else {
+      if ((mPaintMode & LINES_LABELS) == 0)
+        continue;
       setLine(mPlotOptions.linecolour, mPlotOptions.linetype, mPlotOptions.linewidth);
     }
     for (point_vv::const_iterator itP = it->second.begin(); itP != it->second.end(); ++itP)
@@ -375,9 +382,9 @@ void DianaLines::paint_labels()
 
 void DianaLines::add_contour_line(contouring::level_t li, const contouring::points_t& cpoints, bool closed)
 {
-  if (li == DianaLevels::UNDEF_LEVEL and mPlotOptions.undefMasking != 2)
+  if (li == DianaLevels::UNDEF_LEVEL && (mPlotOptions.undefMasking != 2 || (mPaintMode & UNDEFINED) == 0))
     return;
-  if (li != DianaLevels::UNDEF_LEVEL and not mPlotOptions.options_1)
+  if (li != DianaLevels::UNDEF_LEVEL && (!mPlotOptions.options_1 || (mPaintMode & LINES_LABELS) == 0))
     return;
 
   point_v points(cpoints.begin(), cpoints.end());
@@ -388,10 +395,16 @@ void DianaLines::add_contour_line(contouring::level_t li, const contouring::poin
 
 void DianaLines::add_contour_polygon(contouring::level_t level, const contouring::points_t& cpoints)
 {
-  if (level == DianaLevels::UNDEF_LEVEL and mPlotOptions.undefMasking != 1)
+  if ((mPaintMode & FILL) == 0)
     return;
-  if (level != DianaLevels::UNDEF_LEVEL and mPlotOptions.palettecolours.empty() and mPlotOptions.palettecolours_cold.empty())
-    return;
+
+  if (level == DianaLevels::UNDEF_LEVEL) {
+    if (mPlotOptions.undefMasking != 1 || (mPaintMode & UNDEFINED) == 0)
+      return;
+  } else {
+    if ((mPlotOptions.palettecolours.empty() && mPlotOptions.palettecolours_cold.empty()) || (mPaintMode & FILL) == 0)
+      return;
+  }
 
   point_v points(cpoints.begin(), cpoints.end());
   m_polygons[level].push_back(points);
@@ -560,7 +573,7 @@ boost::shared_ptr<DianaLevels> dianaLevelsForPlotOptions(const PlotOptions& popt
 // same parameters as diContouring::contour, most of them ignored
 bool poly_contour(int nx, int ny, int ix0, int iy0, int ix1, int iy1,
     const float z[], const float xz[], const float yz[],
-    DiGLPainter* gl, const PlotOptions& poptions, float fieldUndef)
+    DiGLPainter* gl, const PlotOptions& poptions, float fieldUndef, int paintMode)
 {
   DianaLevels_p levels = dianaLevelsForPlotOptions(poptions, fieldUndef);
 
@@ -569,6 +582,7 @@ bool poly_contour(int nx, int ny, int ix0, int iy0, int ix1, int iy1,
 
   const DianaField df(index, z, *levels, *positions);
   DianaGLLines dl(gl, poptions, *levels);
+  dl.setPaintMode(paintMode);
 
   { METLIBS_LOG_TIME("contouring");
     try {

src/diPolyContouring.h
@@ -150,6 +150,10 @@ public:
 
   virtual void paint();
 
+  enum { UNDEFINED = 1, FILL = 2, LINES_LABELS = 4 };
+  void setPaintMode(int mode)
+    { mPaintMode = mode; }
+
 protected:
   typedef std::vector<contouring::point_t> point_v;
   typedef std::vector<point_v> point_vv;
@@ -171,6 +175,7 @@ protected:
   const DianaLevels& mLevels;
 
 private:
+  int mPaintMode;
   level_points_m m_lines;
   level_points_m m_polygons;
 };
@@ -181,6 +186,7 @@ int find_index(bool repeat, int available, int i);
 
 bool poly_contour(int nx, int ny, int ix0, int iy0, int ix1, int iy1,
     const float z[], const float xz[], const float yz[],
-    DiGLPainter* gl, const PlotOptions& poptions, float fieldUndef);
+    DiGLPainter* gl, const PlotOptions& poptions, float fieldUndef,
+    int paintMode = (DianaLines::UNDEFINED | DianaLines::FILL | DianaLines::LINES_LABELS));
 
 #endif // diPolyContouring_hh

commit f1dec6a4bbd88ce3f003e406c80692a9cc162e2f

bugfix: FieldPlot must check isEnabled() and smooth
 * problems were introduced in bb958bc

src/diFieldPlot.cc
@@ -608,6 +608,33 @@ bool FieldPlot::getDataAnnotations(vector<string>& anno)
 
 void FieldPlot::plot(DiGLPainter* gl, PlotOrder zorder)
 {
+  if (!isEnabled() || fields.size() < 1)
+    return;
+
+  // possibly smooth all "single" fields
+  for (size_t i = 0; i < fields.size(); i++) {
+    if (fields[i] && fields[i]->data) {
+      if (fields[i]->numSmoothed < poptions.fieldSmooth) {
+        int nsmooth = poptions.fieldSmooth - fields[i]->numSmoothed;
+        fields[i]->smooth(nsmooth);
+      } else if (fields[i]->numSmoothed > poptions.fieldSmooth) {
+        METLIBS_LOG_ERROR(
+            "field smoothed too much !!! numSmoothed = " << fields[i]->numSmoothed << ", fieldSmooth=" << poptions.fieldSmooth);
+      }
+    }
+  }
+
+  // avoid background colour
+  poptions.bordercolour = getStaticPlot()->notBackgroundColour(poptions.bordercolour);
+  poptions.linecolour   = getStaticPlot()->notBackgroundColour(poptions.linecolour);
+  for (unsigned int i = 0; i < poptions.colours.size(); i++)
+    poptions.colours[i] = getStaticPlot()->notBackgroundColour(poptions.colours[i]);
+
+  if (poptions.antialiasing)
+    gl->Enable(DiGLPainter::gl_MULTISAMPLE);
+  else
+    gl->Disable(DiGLPainter::gl_MULTISAMPLE);
+
   if (zorder == SHADE_BACKGROUND) {
     // should be below all real fields
     if (poptions.gridLines > 0)
@@ -631,35 +658,6 @@ bool FieldPlot::plotMe(DiGLPainter* gl, PlotOrder zorder)
 {
   METLIBS_LOG_SCOPE();
   METLIBS_LOG_DEBUG(LOGVAL(getModelName()));
-
-  int n = fields.size();
-
-  if (!isEnabled() || n < 1)
-    return false;
-
-  // possibly smooth all "single" fields
-  for (int i = 0; i < n; i++) {
-    if (fields[i] && fields[i]->data) {
-      if (fields[i]->numSmoothed < poptions.fieldSmooth) {
-        int nsmooth = poptions.fieldSmooth - fields[i]->numSmoothed;
-        fields[i]->smooth(nsmooth);
-      } else if (fields[i]->numSmoothed > poptions.fieldSmooth) {
-        METLIBS_LOG_ERROR(
-            "field smoothed too much !!! numSmoothed = " << fields[i]->numSmoothed << ", fieldSmooth=" << poptions.fieldSmooth);
-      }
-    }
-  }
-
-  // avoid background colour
-  poptions.bordercolour = getStaticPlot()->notBackgroundColour(poptions.bordercolour);
-  poptions.linecolour   = getStaticPlot()->notBackgroundColour(poptions.linecolour);
-  for (unsigned int i = 0; i < poptions.colours.size(); i++)
-    poptions.colours[i] = getStaticPlot()->notBackgroundColour(poptions.colours[i]);
-
-  if (poptions.antialiasing)
-    gl->Enable(DiGLPainter::gl_MULTISAMPLE);
-  else
-    gl->Disable(DiGLPainter::gl_MULTISAMPLE);
 
   if (poptions.use_stencil || poptions.update_stencil) {
     // Enable the stencil test for masking the field to be plotted or
@@ -2998,8 +2996,6 @@ bool FieldPlot::plotUndefined(DiGLPainter* gl)
   if (poptions.undefMasking <= 0)
     return false;
 
-  if (not isEnabled())
-    return false;
   if (not checkFields(1))
     return false;
   if (plottype == fpt_contour2) {