metno/diana

commit d750e55882c6bfd56c8425d952daca43a94eaf66

Manage editing mode using the edit manager's editing state rather than the visibility of the editing toolbar.

src/qtMainWindow.cc
@@ -946,7 +946,7 @@ DianaMainWindow::DianaMainWindow(Controller *co, const std::string& dianaTitle)
   connect(editm, SIGNAL(setWorkAreaCursor(const QCursor &)), SLOT(setWorkAreaCursor(const QCursor &)));
   connect(editm, SIGNAL(unsetWorkAreaCursor()), SLOT(unsetWorkAreaCursor()));
   connect(editm, SIGNAL(itemStatesReplaced()), SLOT(updatePlotElements()));
-  connect(drawingDialog, SIGNAL(editingMode(bool)), editDrawingToolBar, SLOT(setVisible(bool)));
+  connect(drawingDialog, SIGNAL(editingMode(bool)), SLOT(setEditDrawingMode(bool)));
 
   textview = new TextView(this);
   textview->setMinimumWidth(300);
@@ -1314,9 +1314,10 @@ void DianaMainWindow::recallPlot(const vector<string>& vstr, bool replace)
 void DianaMainWindow::toggleEditDrawingMode()
 {
   if (editDrawingToolBar->isVisible())
-    editDrawingToolBar->hide();
+    setEditDrawingMode(false);
   else
-    editDrawingToolBar->show();
+    setEditDrawingMode(true);
+
   METLIBS_LOG_DEBUG("DianaMainWindow::toggleEditDrawingMode enabled " << editDrawingToolBar->isVisible());
 }
 
@@ -1326,6 +1327,8 @@ void DianaMainWindow::setEditDrawingMode(bool enabled)
     editDrawingToolBar->show();
   else
     editDrawingToolBar->hide();
+
+  EditItemManager::instance()->setEditing(enabled);
 }
 
 void DianaMainWindow::winResize(int w, int h)

commit dd049667cd818a41a9983e7df4dc5228faf6bf3e

Fixed editing mode handling for vertical cross-sections that was broken in commit d750e55882c6bfd56c8425d952daca43a94eaf66.

src/qtMainWindow.cc
@@ -1876,10 +1876,10 @@ void DianaMainWindow::onVcrossRequestEditManager(bool on, bool timeGraph)
     else
       EditItems::ToolBar::instance()->setCreatePolyLineAction(CROSS_SECTION_TYPE);
 
-    EditItems::ToolBar::instance()->show();
+    setEditDrawingMode(true);
     vcrossEditManagerEnableSignals();
   } else {
-    EditItems::ToolBar::instance()->hide();
+    setEditDrawingMode(false);
   }
 }
 