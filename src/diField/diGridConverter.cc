metno/diana

commit d27717e118af471fae485549e5dfc8d12ce7cb16

FimexIO: stop manipulating proj4 strings

src/diField/diGridConverter.cc
@@ -176,13 +176,13 @@ bool GridConverter::doGetGridPoints(const GridArea& area, const Projection& map_
   for (int iy = 0; iy < ny; iy++) {
     for (int ix = 0; ix < (nx-1); ix++) {
       int i = ix + iy*nx;
-      p0.x[i] = (ix - gdxy)*area.resolutionX;
-      p0.y[i] = (iy - gdxy)*area.resolutionY;
+      p0.x[i] = area.R().x1 + (ix - gdxy)*area.resolutionX;
+      p0.y[i] = area.R().y1 + (iy - gdxy)*area.resolutionY;
     }
     int i = iy*nx + nx - 1;
     // FIXME x[i]=nx-1 converts to x[i]=0 when transforming between geo-projections
-    p0.x[i] = (nx - gdxy - 1.1)*area.resolutionX;
-    p0.y[i] = (iy - gdxy)*area.resolutionY;
+    p0.x[i] = area.R().x1 + (nx - gdxy - 1.1)*area.resolutionX;
+    p0.y[i] = area.R().y1 + (iy - gdxy)*area.resolutionY;
   }
 
   const Projection& pa = area.P();

src/diField/FimexIO.cc
@@ -386,53 +386,8 @@ FimexIO::CDMReaderPtr FimexIO::createReader()
 void FimexIO::inventoryExtractGridProjection(boost::shared_ptr<const MetNoFimex::Projection> projection, gridinventory::Grid& grid,
     CoordinateSystem::ConstAxisPtr xAxis, CoordinateSystem::ConstAxisPtr yAxis)
 {
-  std::string projStr = projection->getProj4String();
-
-  // replace +proj=utm ... with +proj=tmerc... (need proj that accepts +x_0/+y_0)
-  std::string utmstr = "+proj=utm +zone=";
-  size_t utmpos = projStr.find(utmstr,0);
-  if (utmpos != std::string::npos) {
-    const vector<std::string> tokens = miutil::split(projStr, 0, " ");
-    for (size_t ii=0; ii<tokens.size(); ++ii) {
-      const vector<std::string> stokens = miutil::split(tokens[ii], 0, "=");
-      if (stokens.size() == 2 && stokens[0] == "+zone") {
-        int lon_0 = (atof(stokens[1].c_str()) - 1)*6 - 180 + 3;
-        utmstr += stokens[1];
-        ostringstream ost;
-        ost <<"+proj=tmerc +lon_0="<<lon_0<<" +lat_0=0 +k=0.9996 +x_0=500000";
-        boost::replace_first(projStr,utmstr,ost.str());
-        break;
-      }
-    }
-  }
-  
-  //replace +proj=longlat and +proj=latlong with +proj=ob_tran +o_proj=longlat (need proj that accepts +x_0/+y_
-  std::string llstr = "+proj=longlat";
-  size_t llpos = projStr.find(llstr,0);
-  if (llpos == std::string::npos) {
-    llstr = "+proj=latlong";
-    llpos = projStr.find(llstr,0);
-  }
-  if (llpos != std::string::npos) {
-    boost::replace_first(projStr,llstr,"+proj=ob_tran +o_proj=longlat +lon_0=0 +o_lat_p=90");
-  }
-  
-  // Find and remove explicit false easting/northing, add them later
-  const vector<std::string> tokens = miutil::split(projStr, 0, " ");
-  for (size_t ii=0; ii<tokens.size(); ++ii) {
-    const vector<std::string> stokens = miutil::split(tokens[ii], 0, "=");
-    if (stokens.size() == 2 && stokens[0] == "+x_0") {
-      grid.x_0 = -1 *atof(stokens[1].c_str());
-      const size_t spos = projStr.find(tokens[ii], 0);
-      assert(spos != std::string::npos);
-      projStr.erase(spos, tokens[ii].length()+1);
-    } else if (stokens.size() == 2 && stokens[0] == "+y_0") {
-      grid.y_0 = -1 *atof(stokens[1].c_str());
-      const size_t spos = projStr.find(tokens[ii], 0);
-      assert(spos != std::string::npos);
-      projStr.erase(spos, tokens[ii].length()+1);
-    }
-  }
+  grid.x_0 = 0;
+  grid.y_0 = 0;
 
   const float axis_scale = 1.0; // X/Y-axes are scaled by this
   const std::string xyUnit = projection->isDegree() ? "radian" : METER;
@@ -445,7 +400,7 @@ void FimexIO::inventoryExtractGridProjection(boost::shared_ptr<const MetNoFimex:
       boost::shared_array<float> fdata = xdata->asFloat();
       grid.nx = nx;
       grid.x_resolution = (fdata[1] - fdata[0]) * axis_scale;
-      grid.x_0 += fdata[0] * axis_scale;
+      grid.x_0 = fdata[0] * axis_scale;
       METLIBS_LOG_DEBUG(" x_resolution:" << grid.x_resolution << " x_0:" << grid.x_0);
     }
   }
@@ -457,11 +412,11 @@ void FimexIO::inventoryExtractGridProjection(boost::shared_ptr<const MetNoFimex:
       grid.ny = ny;
       if( fdata[0] > fdata[1] ) {
         grid.y_resolution = (fdata[ny-1] - fdata[ny-2]) * axis_scale;
-        grid.y_0 += fdata[ny-1] * axis_scale;
+        grid.y_0 = fdata[ny-1] * axis_scale;
         grid.y_direction_up = false;
       } else {
         grid.y_resolution = (fdata[1] - fdata[0]) * axis_scale;
-        grid.y_0 += fdata[0] * axis_scale;
+        grid.y_0 = fdata[0] * axis_scale;
         grid.y_direction_up = true;
       }
       if ( grid.y_resolution < 0 ) {
@@ -471,11 +426,8 @@ void FimexIO::inventoryExtractGridProjection(boost::shared_ptr<const MetNoFimex:
     }
   }
   
-  // Finalize grid
-  ostringstream ost;
-  ost << projStr << " +x_0=" << -grid.x_0 << " +y_0=" << -grid.y_0;
-  grid.projection += ost.str();
-  
+  grid.projection = projection->getProj4String();
+
   // if defined, use projDefinition from setup
   if (not projDef.empty()) {
     grid.projection = projDef;

src/diField/GridIO.cc
@@ -144,9 +144,8 @@ Field * GridIO::initializeField(const std::string& modelname,
 
   // make the projection and area types
   Projection proj(grid.projection);
-  Rectangle rect(0, 0, (grid.nx - 1) * grid.x_resolution, (grid.ny - 1) * grid.y_resolution);
-  // TODO: which one is correct?
-  // Rectangle rect(grid.x_0, grid.y_0, grid.x_0 + (grid.nx -1) * grid.x_resolution, grid.y_0 + (grid.ny - 1) * grid.y_resolution);
+  const float x0 = grid.x_0, y0 = grid.y_0;
+  const Rectangle rect(x0, y0, x0 + (grid.nx -1) * grid.x_resolution, y0 + (grid.ny - 1) * grid.y_resolution);
 
   Field * field = new Field();
   field->data = new float[grid.nx * grid.ny];

commit 4a4aadae181c03986a87c1d8e8cdc03b241515bb

bugfix Projection::getMapRatios
 * must use x0 and y0
 * these were also ignored in the unit tests, which were confirming that
   the calculation is wrong!
 * regression introduced by commit d27717e

src/diField/diGridConverter.cc
@@ -491,7 +491,7 @@ bool GridConverter::getMapFields(const GridArea& area,
     mf->xmapr = new float[npos];
     mf->ymapr = new float[npos];
     mf->coriolis = new float[npos];
-    if (!area.P().getMapRatios(area.nx, area.ny, area.resolutionX, area.resolutionY,
+    if (!area.P().getMapRatios(area.nx, area.ny, area.R().x1, area.R().y1, area.resolutionX, area.resolutionY,
             mf->xmapr, mf->ymapr, mf->coriolis))
     {
       METLIBS_LOG_ERROR("getMapRatios problem");

src/diField/diProjection.cc
@@ -560,7 +560,7 @@ bool Projection::adjustedLatLonBoundingBox(const Rectangle & maprect,
   return true;
 }
 
-bool Projection::getMapRatios(int nx, int ny, float gridResolutionX, float gridResolutionY,
+bool Projection::getMapRatios(int nx, int ny, float x0, float y0, float gridResolutionX, float gridResolutionY,
     float* xmapr, float* ymapr, float* coriolis) const
 {
   const int npos = nx * ny;
@@ -570,8 +570,8 @@ bool Projection::getMapRatios(int nx, int ny, float gridResolutionX, float gridR
   for (int iy = 0; iy < ny; iy++) {
     for (int ix = 0; ix < nx; ix++) {
       size_t i = ix + iy*nx;
-      x[i] = ix*gridResolutionX;
-      y[i] = iy*gridResolutionY;
+      x[i] = x0 + ix*gridResolutionX;
+      y[i] = y0 + iy*gridResolutionY;
     }
   }
 

src/diField/diProjection.h
@@ -143,7 +143,7 @@ public:
       float & lonmin, float & lonmax, float & latmin, float & latmax) const;
 
   /// Calculate mapratios and coriolis factors
-  bool getMapRatios(int nx, int ny, float gridResolutionX, float gridResolutionY,
+  bool getMapRatios(int nx, int ny, float x0, float y0, float gridResolutionX, float gridResolutionY,
       float* xmapr, float* ymapr, float* coriolis) const;
 
   static bool getLatLonIncrement(float lat, float lon, float& dlat, float& dlon);

test/FieldFunctionsTest.cc
@@ -207,7 +207,7 @@ TEST(FieldFunctionsTest, MapRatios)
 {
   const int nx = 3, ny = 3;
   const GridArea area(Area(Projection("+proj=utm +zone=33 +ellps=WGS84 +datum=WGS84 +units=m +no_defs"),
-          Rectangle(0, 100000, 10000, 110000)), nx, ny, 1000, 1000);
+          Rectangle(0, 0, 10000, 110000)), nx, ny, 1000, 1000);
   GridConverter gc;
 #if 1
   const float* xmapr = 0, *ymapr = 0, *coriolis = 0;

test/GridConverterTest.cc
@@ -9,7 +9,7 @@ TEST(GridConverterTest, GetMapFields)
 {
   const int nx = 3, ny = 3;
   const GridArea area(Area(Projection("+proj=utm +zone=33 +ellps=WGS84 +datum=WGS84 +units=m +no_defs"),
-          Rectangle(0, 100000, 10000, 110000)), nx, ny, 1000, 1000);
+          Rectangle(0, 0, 10000, 110000)), nx, ny, 1000, 1000);
   GridConverter gc;
 #if 1
   const float* xmapr = 0, *ymapr = 0, *coriolis = 0;