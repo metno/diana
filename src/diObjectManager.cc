metno/diana

commit f04cf2c6583862da2df4194858514c21848ee8d3

simplified ObjectManager::getTimes

src/diObjectManager.cc
@@ -164,29 +164,11 @@ vector<std::string> ObjectManager::getObjectNames(bool archive)
 vector<miTime> ObjectManager::getTimes()
 {
   METLIBS_LOG_SCOPE();
-
-  const std::vector<std::string> pinfos(1, objects.getPlotInfo());
-  set<miTime> timeset;
-
-  int nn= pinfos.size();
-  for (int i=0; i<nn; i++){
-    vector<miTime> tv = getObjectTimes(pinfos[i]);
-    for (unsigned int j=0; j<tv.size(); j++){
-      timeset.insert(tv[j]);
-    }
-  }
-
-  vector<miTime> timevec;
-  if (timeset.size()>0) {
-    set<miTime>::iterator p= timeset.begin();
-    for (; p!=timeset.end(); p++) timevec.push_back(*p);
-  }
-
-  return timevec;
+  return getObjectTimes(objects.getPlotInfo());
 }
 
-vector<miTime> ObjectManager::getObjectTimes(const string& pinfo)
 //  * PURPOSE:   return times for list of PlotInfo's
+vector<miTime> ObjectManager::getObjectTimes(const string& pinfo)
 {
   vector<miTime> timevec;
 

commit a420812f2d6fc79882bab0ddc56307c487373724

Bugfix: sort object/product times
 * regression introduced in commit f04cf2c6, i.e. before release 3.42
 * merge ObjectManager::getObjectTimes into ObjectManager::getTimes

src/diObjectManager.cc
@@ -161,24 +161,18 @@ vector<std::string> ObjectManager::getObjectNames(bool archive)
 vector<miTime> ObjectManager::getTimes()
 {
   METLIBS_LOG_SCOPE();
-  return getObjectTimes(objects.getPlotInfo());
-}
-
-//  * PURPOSE:   return times for list of PlotInfo's
-vector<miTime> ObjectManager::getObjectTimes(const miutil::KeyValue_v& pinfo)
-{
   vector<miTime> timevec;
 
-  for (const miutil::KeyValue& kv : pinfo){
-    if (kv.key() == "name"){
-      vector<ObjFileInfo> ofi= getObjectFiles(kv.value(), true);
-      for (unsigned int k=0; k<ofi.size(); k++){
-        timevec.push_back(ofi[k].time);
+  for (const miutil::KeyValue& kv : objects.getPlotInfo()){
+    if (kv.key() == "name") {
+      for (const ObjFileInfo& ofi : getObjectFiles(kv.value(), true)) {
+        timevec.push_back(ofi.time);
       }
       break;
     }
   }
 
+  std::sort(timevec.begin(), timevec.end());
   return timevec;
 }
 

src/diObjectManager.h
@@ -339,7 +339,6 @@ public:
   std::vector <ObjFileInfo> getObjectFiles(const std::string& objectname, bool refresh);
   /// returns list of times
   std::vector<miutil::miTime> getTimes();
-  std::vector<miutil::miTime> getObjectTimes(const miutil::KeyValue_v &pinfo);
 
   ///returns union or intersection of plot times from all pinfos
   void getCapabilitiesTime(std::vector<miutil::miTime>& normalTimes,