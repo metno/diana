d5f02b6743c339a002aee7ce67659be41841ca85
Alexander Bürger <alexander.buerger@met.no>
2017-12-06 08:00:08 +0100

    Bugfix: do not add undef times to time axis
    (cherry picked from commit 8cb7b01210af797e564e87c89f6e653248b862ed)

--- src/diField/GridCollection.cc
+++ src/diField/GridCollection.cc
@@ -388,7 +388,8 @@ std::set<miutil::miTime> GridCollection::getTimes(const std::string& reftime, co
         // double -> miTime
         time_t t = v;
         miutil::miTime tt(t);
-        settime.insert(tt);
+        if (!tt.undef())
+          settime.insert(tt);
       }
     }
   }
