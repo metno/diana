0fd770397ec16655029c8f5dc0c48fcd8848f8bc
Alexander Bürger <alexander.buerger@met.no>
2018-03-22 08:00:08 +0100

    bugfix AreaObjectsCluster::areaObjectsCommand

--- src/diAreaObjectsCluster.cc
+++ src/diAreaObjectsCluster.cc
@@ -1,3 +1,32 @@
+/*
+  Diana - A Free Meteorological Visualisation Tool
+
+  Copyright (C) 2017-2018 met.no
+
+  Contact information:
+  Norwegian Meteorological Institute
+  Box 43 Blindern
+  0313 OSLO
+  NORWAY
+  email: diana@met.no
+
+  This file is part of Diana
+
+  Diana is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+
+  Diana is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with Diana; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
 #include "diAreaObjectsCluster.h"
 
 #include "diPlotModule.h"
@@ -98,6 +127,8 @@ void AreaObjectsCluster::areaObjectsCommand(const std::string& command, const st
           }
         }
       }
+    } else {
+      ++it;
     }
   }
 }
