metno/diana

commit 7f8d6b5aad941f723d95065d83113c1b2553f940

use set<miTime> many places

src/qtTimeSlider.cc
@@ -1,7 +1,7 @@
 /*
   Diana - A Free Meteorological Visualisation Tool
 
-  Copyright (C) 2006-2017 met.no
+  Copyright (C) 2006-2018 met.no
 
   Contact information:
   Norwegian Meteorological Institute
@@ -52,7 +52,7 @@ TimeSlider::TimeSlider(Qt::Orientation ori, QWidget *parent)
 
 void TimeSlider::init()
 {
-  vector<miutil::miTime> tmp;
+  plottimes_t tmp;
   tlist["field"]= tmp;
   tlist["sat"]= tmp;
   tlist["obs"]= tmp;
@@ -89,17 +89,16 @@ void TimeSlider::setInterval(int in)
 
 void TimeSlider::setMinMax(const miutil::miTime& t1, const miutil::miTime& t2)
 {
-  const miutil::miTime& last = orig_times.back();
-  if (t1>=orig_times[0] && t1<=last && t2>=t1 && t2<=last) {
-    start = t1;
-    stop = t2;
-    useminmax= true;
-    // change colours..
-//     QColorGroup cg= pal.active();
-//     cg.setColor(QColorGroup::Foreground,Qt::red);
-//     cg.setColor(QColorGroup::Button,Qt::red);
-    QPalette p(Qt::red);
-    setPalette(p);
+  if (!orig_times.empty()) {
+    const miutil::miTime& first = *orig_times.begin();
+    const miutil::miTime& last = *orig_times.rbegin();
+    if (t1 >= first && t1 <= last && t2 >= t1 && t2 <= last) {
+      start = t1;
+      stop = t2;
+      useminmax = true;
+      QPalette p(Qt::red);
+      setPalette(p);
+    }
   }
 
   updateList();
@@ -188,110 +187,85 @@ bool TimeSlider::nextTime(const int dir, miutil::miTime& time)
   return time != current;
 }
 
-void TimeSlider::insert(const std::string& datatype, const std::vector<miutil::miTime>& vt, bool use)
+bool TimeSlider::useDataType(const std::string& dt, bool ifUsed)
 {
-  tlist[datatype]= vt;
-  usetlist[datatype] = use;
-  updateList();
-  emit newTimes(orig_times);
+  tlist_t::const_iterator it = tlist.find(dt);
+  if (it == tlist.end())
+    return false;
+  if (ifUsed) {
+    usetlist_t::const_iterator ut = usetlist.find(dataType);
+    if (ut == usetlist.end() || !ut->second)
+      return false;
+  }
+  if (it->second.empty())
+    return false;
+  orig_times = it->second;
+  dataTypeUsed = dt;
+  return true;
 }
+
+static const struct DataTypeUse
+{
+  std::string dataType;
+  bool ifUsed;
+} dataTypeOrder[] = {{"field", false}, {"sat", true},       {"obs", false},    {"obj", true},  {"product", false},
+                     {"vprof", false}, {"spectrum", false}, {"vcross", false}, {"sat", false}, {"obj", false}};
 
 void TimeSlider::updateList()
 {
   const int maxticks= 20;
 
-  // FIXME this is very similar to bdiana_capi.cc:selectTime / BdianaMain::getTime
   times.clear();
-  if( tlist[dataType].size()>0 && usetlist[dataType]){
-    times = tlist[dataType];
-    dataTypeUsed = dataType;
+
+  // FIXME this is very similar to bdiana_capi.cc:selectTime / BdianaMain::getTime
+  bool found = false;
+  if (!dataType.empty())
+    found = useDataType(dataType, true);
+  if (!found) {
+    for (const DataTypeUse& dtu : dataTypeOrder) {
+      if ((found = useDataType(dtu.dataType, dtu.ifUsed)))
+        break;
+    }
   }
-  else if( tlist["field"].size()>0 ){
-    times = tlist["field"];
-    dataTypeUsed = "field";
-  }
-  else if( tlist["sat"].size()>0 && usetlist["sat"]){
-    times = tlist["sat"];
-    dataTypeUsed = "sat";
-  }
-  else if( tlist["obs"].size()>0 ){
-    times = tlist["obs"];
-    dataTypeUsed = "obs";
-  }
-  else if ( tlist["obj"].size()>0 && usetlist["obj"]) {
-    times = tlist["obj"];
-    dataTypeUsed = "obj";
-  }
-  else if( tlist["product"].size()>0 ){
-    times = tlist["product"];
-    dataTypeUsed = "product";
-  }
-  else if( tlist["vprof"].size()>0 ){
-    times = tlist["vprof"];
-    dataTypeUsed = "vprof";
-  }
-  else if( tlist["spectrum"].size()>0 ){
-    times = tlist["spectrum"];
-    dataTypeUsed = "spectrum";
-  }
-  else if( tlist["vcross"].size()>0 ){
-    times = tlist["vcross"];
-    dataTypeUsed = "vcross";
-  }
-  else if ( tlist["sat"].size()>0) {
-    times = tlist["sat"];
-    dataTypeUsed = "sat";
-  }
-  else if ( tlist["obj"].size()>0) {
-    times = tlist["obj"];
-    dataTypeUsed = "obj";
-  }
-  else {
+  if (!found) {
     // Just use the first list of times in the collection.
-    map<std::string,vector<miutil::miTime> >::iterator it;
-    for (it = tlist.begin(); it != tlist.end(); ++it) {
-      if (it->second.size() > 0) {
-        times = it->second;
-        dataTypeUsed = it->first;
+    for (const auto& tt : tlist) {
+      if (!tt.second.empty()) {
+        orig_times = tt.second;
+        dataTypeUsed = tt.first;
+        found = true;
         break;
       }
     }
   }
 
-  orig_times = times; // orig_times = all times
-
+  plottimes_t::const_iterator qs, qe;
   if (useminmax) {
-    vector<miutil::miTime>::iterator q;
-    for (q=times.begin(); q!=times.end() && *q<start; q++)
-      ;
-    times.erase(times.begin(), q);
-
-    for (q=times.begin(); q!=times.end() && *q<stop; q++)
-      ;
-    if (q!=times.end())
-      q++;
-    times.erase(q, times.end());
+    qs = std::lower_bound(orig_times.begin(), orig_times.end(), start);
+    qe = std::lower_bound(qs, orig_times.end(), stop);
+    if (qe != orig_times.end())
+      ++qe;
+  } else {
+    qs = orig_times.begin();
+    qe = orig_times.end();
   }
-
 
   // make sure productimes are included
-  const vector<miutil::miTime>& vpt = tlist["product"];
-  if (!vpt.empty()){
-    for (size_t i=0; i<vpt.size(); i++) {
-      vector<miutil::miTime>::iterator q;
-      for (q=times.begin(); q!=times.end() && *q<vpt[i]; q++)
-        ;
-      if ((q==times.end()) || (vpt[i]!=*q))
-        times.insert(q, vpt[i]);
-    }
+  tlist_t::const_iterator pt = tlist.find("product");
+  plottimes_t timeset;
+  if (pt != tlist.end() && !pt->second.empty()) {
+    timeset.insert(qs, qe);
+    timeset.insert(pt->second.begin(), pt->second.end());
+    qs = timeset.begin();
+    qe = timeset.end();
   }
+  times = std::vector<miutil::miTime>(qs, qe);
 
-  int i,n;
-  n= times.size();
+  const int n= times.size();
   if (n>1) {
     // find time-intervals
     int iv, miniv=INT_MAX, maxiv=0;
-    for (i=1; i<n; i++){
+    for (int i=1; i<n; i++){
       iv= miutil::miTime::minDiff(times[i],times[i-1]);
       if (iv<miniv)
         miniv= iv;
@@ -311,8 +285,7 @@ void TimeSlider::updateList()
     while (n/iv>maxticks)
       iv*= 2;
 
-    // qt4 fix: Edited the line below (old line commented out)
-    setTickPosition(TicksBelow); //setTickmarks(TickSetting(Below));
+    setTickPosition(TicksBelow);
     setTickInterval(iv);
     setSingleStep(1);
     setPageStep(iv);
@@ -452,16 +425,24 @@ void TimeSlider::set(const miutil::miTime& t)
   setSliderValue(v);
 }
 
+void TimeSlider::insert(const std::string& datatype, const plottimes_t& vt, bool use)
+{
+  tlist[datatype] = vt;
+  usetlist[datatype] = use;
+  updateList();
+  Q_EMIT newTimes(orig_times);
+}
+
 void TimeSlider::useData(const std::string& datatype)
 {
   dataType = datatype;
   updateList();
-  emit newTimes(orig_times);
+  Q_EMIT newTimes(orig_times);
 }
 
 void TimeSlider::deleteType(const std::string& type)
 {
-  map< std::string, vector<miutil::miTime> >::iterator p = tlist.find(type);
+  tlist_t::iterator p = tlist.find(type);
   if (p != tlist.end())
     tlist.erase(p);
 }

src/CMakeLists.txt
@@ -420,6 +420,7 @@ LIST (APPEND LIBDIANA_HEADERS
   diQuickMenuTypes.h
   diSliderValues.h
   diStationTypes.h
+  diTimeTypes.h
   util/misc_util.h
 )
 LIST (APPEND LIBDIANA_SOURCES

src/EditItems/drawingdialog.cc
@@ -231,9 +231,7 @@ std::string DrawingDialog::name() const
 
 void DrawingDialog::updateTimes()
 {
-  std::vector<miutil::miTime> times;
-  times = drawm_->getTimes();
-  emit emitTimes("DRAWING", times);
+  emitTimes("DRAWING", drawm_->getTimes());
 }
 
 void DrawingDialog::updateDialog()

src/bdiana_main.cc
@@ -1,7 +1,7 @@
 /*
   Diana - A Free Meteorological Visualisation Tool
 
-  Copyright (C) 2017 met.no
+  Copyright (C) 2017-2018 met.no
 
   Contact information:
   Norwegian Meteorological Institute
@@ -98,8 +98,7 @@ QRectF BdianaMain::cutout()
   return static_cast<DianaImageSource*>(imageSource())->annotationsCutout();
 }
 
-typedef std::vector<miutil::miTime> times_t;
-typedef std::map<std::string, times_t> type_times_t;
+typedef std::map<std::string, plottimes_t> type_times_t;
 
 static type_times_t::const_iterator find_times(const type_times_t& times, const std::string& key)
 {
@@ -129,24 +128,23 @@ miutil::miTime BdianaMain::getTime()
   if (it == times.end())
     return miutil::miTime();
 
-  const times_t& fieldtimes = it->second;
+  const plottimes_t& fieldtimes = it->second;
   if (fieldtimes.empty())
     return miutil::miTime::nowTime();
 
   if (use_time == USE_NOWTIME) {
     const miutil::miTime now = miutil::miTime::nowTime();
     // select closest to now without overstepping
-    const size_t n = fieldtimes.size();
-    for (size_t i = 0; i < n; i++) {
-      if (fieldtimes[i] >= now)
-        return (i > 0 ? fieldtimes[i - 1] : fieldtimes[i]);
+    for (plottimes_t::const_iterator it = fieldtimes.begin(); it != fieldtimes.end(); ++it) {
+      if (*it >= now)
+        return *((it != fieldtimes.begin()) ? (--it) : it);
     }
   }
   if (use_time == USE_FIRSTTIME)
-    return fieldtimes.front();
+    return *fieldtimes.begin();
 
   // both USE_LASTTIME and not-found for USE_NOWTIME
-  return fieldtimes.back();
+  return *fieldtimes.rbegin();
 }
 
 void BdianaMain::setTime(const miutil::miTime& time)

src/diAnnotationPlot.cc
@@ -234,8 +234,7 @@ bool AnnotationPlot::prepare(const PlotCommand_cp& pc)
   return true;
 }
 
-void AnnotationPlot::setData(const vector<Annotation>& a,
-    const vector<miTime>& fieldAnalysisTime)
+void AnnotationPlot::setData(const vector<Annotation>& a, const plottimes_t& fieldAnalysisTime)
 {
   if (atype != anno_text)
     annotations = a;

src/diAnnotationPlot.h
@@ -31,7 +31,8 @@
 
 #include "diPlot.h"
 #include "diPlotCommand.h"
-#include <puTools/miTime.h>
+#include "diTimeTypes.h"
+
 #include <vector>
 #include <map>
 
@@ -142,7 +143,7 @@ private:
   float maxwid;
 
   bool useAnaTime;
-  std::vector<miutil::miTime> fieldAnaTime;
+  plottimes_t fieldAnaTime;
   Colour currentColour;
 
   //called from constructor
@@ -182,8 +183,7 @@ public:
   ///decode plot info strings
   bool prepare(const PlotCommand_cp&);
   ///set data annotations
-  void setData(const std::vector<Annotation>& a,
-      const std::vector<miutil::miTime>& fieldAnalysisTime);
+  void setData(const std::vector<Annotation>& a, const plottimes_t& fieldAnalysisTime);
   void setfillcolour(const Colour& c);
   /// mark editable annotationPlot if x,y inside plot
   bool markAnnotationPlot(int, int);

src/diController.cc
@@ -1,7 +1,7 @@
 /*
   Diana - A Free Meteorological Visualisation Tool
 
-  Copyright (C) 2006-2017 met.no
+  Copyright (C) 2006-2018 met.no
 
   Contact information:
   Norwegian Meteorological Institute
@@ -284,7 +284,7 @@ const miutil::miTime& Controller::getPlotTime()
   return plotm->getPlotTime();
 }
 
-void Controller::getPlotTimes(map<string,vector<miutil::miTime> >& times)
+void Controller::getPlotTimes(std::map<std::string, plottimes_t>& times)
 {
   plotm->getPlotTimes(times);
 }
@@ -731,8 +731,7 @@ const vector<SatFileInfo>& Controller::getSatFiles(const std::string& satellite,
 }
 
 //returns union or intersection of plot times from all pinfos
-void Controller::getCapabilitiesTime(set<miTime>& okTimes,
-    const PlotCommand_cpv& pinfos, bool allTimes)
+void Controller::getCapabilitiesTime(plottimes_t& okTimes, const PlotCommand_cpv& pinfos, bool allTimes)
 {
   plotm->getCapabilitiesTime(okTimes,pinfos,allTimes);
 }
@@ -892,7 +891,7 @@ std::map<std::string,std::string> Controller::getFieldGlobalAttributes(const std
   return fieldm->getGlobalAttributes(modelName, refTime);
 }
 
-vector<miTime> Controller::getFieldTime(vector<FieldRequest>& request)
+plottimes_t Controller::getFieldTime(vector<FieldRequest>& request)
 {
   return fieldplotm->getFieldTime(request);
 }
@@ -934,7 +933,7 @@ bool Controller::getQuickMenus(vector<QuickMenuDefs>& qm)
   return LocalSetupParser::getQuickMenus(qm);
 }
 
-vector<miTime> Controller::getObsTimes(const vector<string>& name)
+plottimes_t Controller::getObsTimes(const vector<string>& name)
 {
   return obsm->getTimes(name);
 }

src/diController.h
@@ -43,6 +43,7 @@
 #include "diRectangle.h"
 #include "diSatTypes.h"
 #include "diStationTypes.h"
+#include "diTimeTypes.h"
 
 #include <puTools/miTime.h>
 
@@ -167,11 +168,9 @@ public:
   const miutil::miTime& getPlotTime();
 
   /// return data times (fields,images, observations, objects and editproducts)
-  void getPlotTimes(std::map<std::string, std::vector<miutil::miTime> >& times);
+  void getPlotTimes(std::map<std::string, plottimes_t>& times);
   ///returns union or intersection of plot times from all pinfos
-  void getCapabilitiesTime(std::set<miutil::miTime>& okTimes,
-      const PlotCommand_cpv &pinfos,
-      bool allTimes=true);
+  void getCapabilitiesTime(plottimes_t& okTimes, const PlotCommand_cpv& pinfos, bool allTimes = true);
   /// returns the current product time
   bool getProductTime(miutil::miTime& t);
   /// returns the current product name
@@ -290,7 +289,7 @@ public:
   /// return button names for ObsDialog ... ascii files (when activated)
   void updateObsDialog(ObsDialogInfo::PlotType& pt, const std::string& name);
   /// get observation times for plot types name
-  std::vector<miutil::miTime> getObsTimes(const std::vector<std::string>& name);
+  plottimes_t getObsTimes(const std::vector<std::string>& name);
 
   // Field-dialog methods
   /// return model/file groups and contents to FieldDialog
@@ -310,7 +309,7 @@ public:
   /// return GlobalAttributes for one model
   std::map<std::string,std::string> getFieldGlobalAttributes(const std::string& modelName, const std::string& refTime);
   /// Returns available times for the requested fields.
-  std::vector<miutil::miTime> getFieldTime(std::vector<FieldRequest>& request);
+  plottimes_t getFieldTime(std::vector<FieldRequest>& request);
   ///update list of fieldsources (field files)
   void updateFieldSource(const std::string & modelName);
 

commit e1fb589b1ff5104c7946f0ff294a6c02afe1e188

bugfix time slider
 * allow time list to becom empty
 * bug introduced in commit 7f8d6b5a

src/qtTimeSlider.cc
@@ -215,7 +215,7 @@ void TimeSlider::updateList()
 {
   const int maxticks= 20;
 
-  times.clear();
+  orig_times.clear();
 
   // FIXME this is very similar to bdiana_capi.cc:selectTime / BdianaMain::getTime
   bool found = false;