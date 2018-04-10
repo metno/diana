metno/diana

commit 22d0eb9abbd2c0b5b7519a5fbd2589144fe6ec85

for loops, formatting, diutil::insert_all

src/diAreaObjectsCluster.cc
@@ -1,6 +1,7 @@
 #include "diAreaObjectsCluster.h"
 
 #include "diPlotModule.h"
+#include "util/misc_util.h"
 
 #include <puTools/miStringFunctions.h>
 
@@ -70,43 +71,30 @@ void AreaObjectsCluster::makeAreaObjects(std::string name, std::string areastrin
   }
 
   //check if dataset with this id/name already exist
-  areaobjects_v::iterator it = vareaobjects.begin();
-  while (it != vareaobjects.end() && (id != it->getId() || name != it->getName()))
-    ++it;
-  if (it != vareaobjects.end()) { //add new areas and replace old areas
-    it->makeAreas(name, icon, areastring, id, plot_->getMapArea());
-    return;
-  }
-
-  //make new dataset
-  AreaObjects new_areaobjects;
-  new_areaobjects.makeAreas(name, icon, areastring, id, plot_->getMapArea());
-  vareaobjects.push_back(new_areaobjects);
+  areaobjects_v::iterator it =
+      std::find_if(vareaobjects.begin(), vareaobjects.end(), [&](const AreaObjects& ao) { return id != ao.getId() || name != ao.getName(); });
+  if (it == vareaobjects.end())
+    // not found, add new at end
+    it = vareaobjects.insert(it, AreaObjects());
+  it->makeAreas(name, icon, areastring, id, plot_->getMapArea());
 }
 
 void AreaObjectsCluster::areaObjectsCommand(const std::string& command, const std::string& dataSet,
     const std::vector<std::string>& data, int id)
 {
-  //   METLIBS_LOG_DEBUG("AreaObjectsCluster::areaCommand");
-  //   METLIBS_LOG_DEBUG("id=" << id);
-  //   METLIBS_LOG_DEBUG("command=" << command);
-  //   METLIBS_LOG_DEBUG("data="<<data);
-
-  int n = vareaobjects.size();
-  for (int i = 0; i < n && i > -1; i++) {
-    if ((id == -1 || id == vareaobjects[i].getId())
-        && (dataSet == "all" || dataSet == vareaobjects[i].getName()))
-    {
-      if (command == "delete" && (data.empty() || (data.size() == 1 && data.front() == "all"))) {
-        vareaobjects.erase(vareaobjects.begin() + i);
-        i--;
-        n = vareaobjects.size();
+  const bool is_delete_command = (command == "delete" && (data.empty() || (data.size() == 1 && data.front() == "all")));
+  for (areaobjects_v::iterator it = vareaobjects.begin(); it != vareaobjects.end(); /*nothing*/) {
+    AreaObjects& ao = *it;
+    if ((id == -1 || id == ao.getId()) && (dataSet == "all" || dataSet == ao.getName())) {
+      if (is_delete_command) {
+        it = vareaobjects.erase(it);
       } else {
-        vareaobjects[i].areaCommand(command, data);
+        ++it;
+        ao.areaCommand(command, data);
         //zoom to selected area
-        if (command == "select" && vareaobjects[i].autoZoom()) {
+        if (command == "select" && ao.autoZoom()) {
           if (data.size() == 2 && data[1] == "on") {
-            plot_->setMapAreaFromMap(vareaobjects[i].getBoundBox(data[0])); // TODO try to move this away from here
+            plot_->setMapAreaFromMap(ao.getBoundBox(data[0])); // TODO try to move this away from here
           }
         }
       }
@@ -116,17 +104,14 @@ void AreaObjectsCluster::areaObjectsCommand(const std::string& command, const st
 
 std::vector<selectArea> AreaObjectsCluster::findAreaObjects(int x, int y, bool newArea)
 {
+  std::vector<selectArea> vsA;
   //METLIBS_LOG_DEBUG("AreaObjectsCluster::findAreas"  << x << " " << y);
   float xm = 0, ym = 0;
   plot_->PhysToMap(x, y, xm, ym);
-  std::vector<selectArea> vsA;
-  int n = vareaobjects.size();
-  for (int i = 0; i < n; i++) {
-    if (!vareaobjects[i].isEnabled())
+  for (AreaObjects& ao : vareaobjects) {
+    if (!ao.isEnabled())
       continue;
-    std::vector<selectArea> sub_vsA;
-    sub_vsA = vareaobjects[i].findAreas(xm, ym, newArea);
-    vsA.insert(vsA.end(), sub_vsA.begin(), sub_vsA.end());
+    diutil::insert_all(vsA, ao.findAreas(xm, ym, newArea));
   }
   return vsA;
 }

src/CMakeLists.txt
@@ -343,6 +343,7 @@ METNO_HEADERS(LIBDIANAUI_QTHEADERS "cc" "h" ${LIBDIANAUI_QTSOURCES})
 
 LIST (APPEND LIBDIANA_HEADERS
   diBuild.h
+  util/misc_util.h
 )
 LIST (APPEND LIBDIANA_SOURCES
   ${BUILDINFO_CC}

src/bdiana_capi.cc
@@ -80,6 +80,7 @@
 
 #include "util/charsets.h"
 #include "util/fimex_logging.h"
+#include "util/misc_util.h"
 
 #include <puCtools/sleep.h>
 #include <puTools/miStringFunctions.h>
@@ -2203,8 +2204,7 @@ static int handleFieldFilesCommand(int& k)
 {
   bool found_end = false;
   int kk = k;
-  const std::vector<std::string> pcom = FIND_END_COMMAND(kk, com_field_files_end, &found_end);
-  extra_field_lines.insert(extra_field_lines.end(), pcom.begin(), pcom.end());
+  diutil::insert_all(extra_field_lines, FIND_END_COMMAND(kk, com_field_files_end, &found_end));
 
   if (!found_end) {
     METLIBS_LOG_ERROR("no " << com_field_files_end << " found:" << lines[k] << " Linenumber:" << linenumbers[k]);
@@ -2254,10 +2254,8 @@ static int handleDescribeCommand(int& k)
     std::set<std::string> fieldPatterns;
 
     FieldManager* fieldManager = main_controller->getFieldManager();
-    for (vector<FieldPlot*>::iterator it = fieldPlots.begin(); it != fieldPlots.end(); ++it) {
-      std::string modelName = (*it)->getModelName();
-      std::vector<std::string> fileNames = fieldManager->getFileNames(modelName);
-      fieldPatterns.insert(fileNames.begin(), fileNames.end());
+    for (FieldPlot* fp : fieldPlots) {
+      diutil::insert_all(fieldPatterns, fieldManager->getFileNames(fp->getModelName()));
     }
 
     const SatManager::Prod_t& satProducts = main_controller->getSatelliteManager()->getProductsInfo();

src/diAreaObjects.h
@@ -56,7 +56,7 @@ public:
   std::vector<selectArea> findAreas(float x, float y, bool newArea=false);
 
   /// returns id
-  int getId(){return itsId;}
+  int getId() const { return itsId; }
 
   /// returns name
   const std::string& getName() const

src/diController.cc
@@ -216,15 +216,18 @@ void Controller::getPlotSize(float& x1, float& y1, float& x2, float& y2)
 }
 
 // get plot area (incl. projection)
-const Area& Controller::getMapArea(){
+const Area& Controller::getMapArea()
+{
   return plotm->getMapArea();
 }
 
-void Controller::zoomTo(const Rectangle & r) {
+void Controller::zoomTo(const Rectangle& r)
+{
   plotm->setMapAreaFromMap(r);
 }
 
-void Controller::zoomOut(){
+void Controller::zoomOut()
+{
   plotm->zoomOut();
 }
 
@@ -307,7 +310,8 @@ void Controller::setPlotTime(const miTime& t)
 }
 
 // toggle area conservatism
-void Controller::keepCurrentArea(bool b){
+void Controller::keepCurrentArea(bool b)
+{
   plotm->keepCurrentArea(b);
 }
 

src/diDisplayObjects.cc
@@ -32,9 +32,10 @@
 #include "diDisplayObjects.h"
 #include "diDrawingTypes.h"
 #include "diKVListPlotCommand.h"
+#include "diWeatherArea.h"
 #include "diWeatherFront.h"
 #include "diWeatherSymbol.h"
-#include "diWeatherArea.h"
+#include "util/misc_util.h"
 
 #include <puTools/miStringFunctions.h>
 
@@ -106,7 +107,7 @@ bool DisplayObjects::define(const PlotCommand_cp& pc)
         fixedsymbolsize= kv.toInt();
       } else if (key=="symbolfilter") {
         const std::vector<std::string> vals = miutil::split(value, ",");
-        symbolfilter.insert(symbolfilter.end(), vals.begin(), vals.end());
+        diutil::insert_all(symbolfilter, vals);
       }
     }
   }

src/diEditManager.cc
@@ -30,25 +30,26 @@
 
 #include "diana_config.h"
 
+#include "diAnnotationPlot.h"
 #include "diEditManager.h"
-#include "diPlotModule.h"
-#include "diObjectManager.h"
-#include "diObsPositions.h"
+#include "diFieldEdit.h"
+#include "diFieldPlot.h"
 #include "diFieldPlotCluster.h"
 #include "diFieldPlotManager.h"
+#include "diLabelPlotCommand.h"
+#include "diMapMode.h"
+#include "diObjectManager.h"
+#include "diObsPositions.h"
+#include "diPlotModule.h"
+#include "diUndoFront.h"
+#include "diUtilities.h"
+#include "diWeatherArea.h"
 #include "diWeatherFront.h"
 #include "diWeatherSymbol.h"
-#include "diWeatherArea.h"
-#include "diMapMode.h"
-#include "diFieldPlot.h"
-#include "diUndoFront.h"
-#include "diFieldEdit.h"
-#include "diAnnotationPlot.h"
-#include "diLabelPlotCommand.h"
-#include "diUtilities.h"
 #include "miSetupParser.h"
 #include "util/charsets.h"
 #include "util/math_util.h"
+#include "util/misc_util.h"
 
 #include <puTools/miDirtools.h>
 #include <puTools/miStringFunctions.h>
@@ -200,10 +201,10 @@ bool EditManager::parseSetup()
           ok= false;
 
       } else if (key=="local_save_dir") {
-        ep.local_savedirs.insert(ep.prod_savedirs.end(), values.begin(),values.end());
+        diutil::insert_all(ep.local_savedirs, values);
 
       } else if (key=="prod_save_dir") {
-        ep.prod_savedirs.insert(ep.prod_savedirs.end(), values.begin(),values.end());
+        diutil::insert_all(ep.prod_savedirs, values);
 
       } else if (key=="input_dir") {
         ep.inputdirs.insert(ep.inputdirs.end(),

src/diField/FimexIO.cc
@@ -7,6 +7,8 @@
 
 #include "FimexIO.h"
 
+#include "util/misc_util.h"
+
 #include <puCtools/stat.h>
 #include <puTools/miStringFunctions.h>
 #include <puTools/miDirtools.h>
@@ -813,7 +815,7 @@ bool FimexIO::makeInventory(const std::string& reftime)
       ri.parameters = parameters;
       ri.grids = grids;
       ri.zaxes = zaxes;
-      ri.taxes.insert(taxes.begin(), taxes.end());
+      diutil::insert_all(ri.taxes, taxes);
       ri.extraaxes = extraaxes;
       ri.timestamp = now;
       ri.globalAttributes = globalAttributes;

commit 825d1341f570580e968b92b0e01f91a101e24757

bugfix area objects
 * bug introduced in commit 22d0eb9a

src/diAreaObjectsCluster.cc
@@ -102,7 +102,7 @@ void AreaObjectsCluster::makeAreaObjects(std::string name, std::string areastrin
 
   //check if dataset with this id/name already exist
   areaobjects_v::iterator it =
-      std::find_if(vareaobjects.begin(), vareaobjects.end(), [&](const AreaObjects& ao) { return id != ao.getId() || name != ao.getName(); });
+      std::find_if(vareaobjects.begin(), vareaobjects.end(), [&](const AreaObjects& ao) { return id == ao.getId() && name == ao.getName(); });
   if (it == vareaobjects.end())
     // not found, add new at end
     it = vareaobjects.insert(it, AreaObjects());