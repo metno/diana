metno/diana

commit ebbe9cbc60c79a47ac993ea3966eff926235347e

wms: keep CRS order

src/wmsclient/WebMapWMS.cc
@@ -644,8 +644,12 @@ bool WebMapWMS::parseLayer(QDomElement& eLayer, std::string style, std::string l
     METLIBS_LOG_DEBUG("adding layer '" << sLayerName << "'");
     std::unique_ptr<WebMapWMSLayer> layer(new WebMapWMSLayer(sLayerName));
     layer->setTitle(qs(eLayer.firstChildElement("Title").text()));
-    for (crs_bbox_m::const_iterator it = crs_bboxes.begin(); it != crs_bboxes.end(); ++it)
-      layer->addCRS(it->first, it->second);
+    for (const QString& crs : lCRS) {
+      const std::string sCRS = qs(crs);
+      crs_bbox_m::iterator it = crs_bboxes.find(sCRS);
+      if (it != crs_bboxes.end())
+        layer->addCRS(it->first, it->second);
+    }
     layer->setDefaultStyle(style, legendUrl);
     layer->setDimensions(dimensions);
     mLayers.push_back(layer.release());

commit 1cf8ba85f5070dbef28aef96b7a6c0c47af4a3f2

wms: bugfix child layers
 * bug introduced in commit ebbe9cbc

src/wmsclient/WebMapWMS.cc
@@ -266,10 +266,13 @@ WebMapRequest_x WebMapWMS::createRequest(const std::string& layerIdentifier,
     const Rectangle& viewRect, const Projection& viewProj, double viewScale, int w, int h)
 {
   METLIBS_LOG_SCOPE(LOGVAL(layerIdentifier));
-  WebMapWMSLayer_cx layer = static_cast<WebMapWMSLayer_cx>
-      (findLayerByIdentifier(layerIdentifier));
-  if (!layer || layer->countCRS() == 0) {
-    METLIBS_LOG_DEBUG("no layer, or no CRS");
+  WebMapWMSLayer_cx layer = static_cast<WebMapWMSLayer_cx>(findLayerByIdentifier(layerIdentifier));
+  if (!layer) {
+    METLIBS_LOG_DEBUG("no layer, cannot create request");
+    return 0;
+  }
+  if (layer->countCRS() == 0) {
+    METLIBS_LOG_DEBUG("#CRS=" << layer->countCRS() << ", cannot create request");
     return 0;
   }
 
@@ -471,15 +474,15 @@ bool WebMapWMS::parseReply()
   const crs_bbox_m top_crs_bboxes;
   const WebMapDimension_v top_dimensions;
   QDOM_FOREACH_CHILD(eLayer, eCapability, "Layer") {
-    if (!parseLayer(eLayer, top_style, top_legendUrl, top_crs_bboxes, top_dimensions))
+    if (!parseLayer(eLayer, top_style, top_legendUrl, QStringList(), top_crs_bboxes, top_dimensions))
       return false;
   }
 
   return true;
 }
 
-bool WebMapWMS::parseLayer(QDomElement& eLayer, std::string style, std::string legendUrl,
-    crs_bbox_m crs_bboxes, std::vector<WebMapDimension> dimensions)
+bool WebMapWMS::parseLayer(QDomElement& eLayer, std::string style, std::string legendUrl, QStringList lCRS, crs_bbox_m crs_bboxes,
+                           std::vector<WebMapDimension> dimensions)
 {
   METLIBS_LOG_SCOPE();
   using diutil::qs;
@@ -508,7 +511,6 @@ bool WebMapWMS::parseLayer(QDomElement& eLayer, std::string style, std::string l
   }
 
   // loop over SRS/CRS elements to extract known bbox'es
-  QStringList lCRS;
   QDOM_FOREACH_CHILD(eCRS, eLayer, aCRS) {
     if (mVersion == WMS_111)
       lCRS << eCRS.text().split(" ", QString::SkipEmptyParts);
@@ -658,7 +660,7 @@ bool WebMapWMS::parseLayer(QDomElement& eLayer, std::string style, std::string l
   }
 
   QDOM_FOREACH_CHILD(eChildLayer, eLayer, "Layer") {
-    if (!parseLayer(eChildLayer, style, legendUrl, crs_bboxes, dimensions))
+    if (!parseLayer(eChildLayer, style, legendUrl, lCRS, crs_bboxes, dimensions))
       return false;
   }
 

src/wmsclient/WebMapWMS.h
@@ -200,8 +200,7 @@ private:
   enum WmsVersion { WMS_111, WMS_130 };
 
   bool parseReply();
-  bool parseLayer(QDomElement& eLayer, std::string style,  std::string legendUrl,
-      crs_bbox_m crs_bboxes, WebMapDimension_v dimensions);
+  bool parseLayer(QDomElement& eLayer, std::string style, std::string legendUrl, QStringList lCRS, crs_bbox_m crs_bboxes, WebMapDimension_v dimensions);
   void parseDimensionValues(WebMapDimension& dim, const QString& text, const QString& defaultValue);
 
 private Q_SLOTS: