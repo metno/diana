metno/diana

commit f3764a01c5ad9b313caf6924e75061efecf125d5

code style / simplification
 * sort headers
 * deduplicate code
 * avoid a string copies
 * avoid map lookups
 * formatting
 * ...

src/util/charsets.cc
@@ -189,12 +189,16 @@ GetLineConverter::GetLineConverter(const std::string& comment, const std::string
 
 std::istream& GetLineConverter::operator()(std::istream& in, std::string& line)
 {
-  if (std::getline(in, line)) {
-    if (diutil::CharsetConverter_p c = diutil::findConverterFromCoding(line, cs_internal_, comment_))
-      converter_ = c;
-    converter_->apply(line);
-  }
+  if (!std::getline(in, line))
+    apply(line);
   return in;
+};
+
+void GetLineConverter::apply(std::string& line)
+{
+  if (diutil::CharsetConverter_p c = diutil::findConverterFromCoding(line, cs_internal_, comment_))
+    converter_ = c;
+  converter_->apply(line);
 };
 
 } // namespace diutil

src/diController.cc
@@ -137,22 +137,12 @@ bool Controller::parseSetup()
 {
   METLIBS_LOG_SCOPE();
 
-  // plotm->getStaticPlot()->initFontManager();
-
   //Parse field sections
-  vector<std::string> fieldSubSect = fieldm->subsections();
-  int nsect = fieldSubSect.size();
   vector<std::string> errors;
-  for( int i=0; i<nsect; i++){
-    vector<std::string> lines;
-    if (!SetupParser::getSection(fieldSubSect[i],lines)) {
-      //      METLIBS_LOG_WARN("Missing section "<<fieldSubSect[i]<<" in setupfile.");
-    }
-    vector<std::string> string_lines;
-    for (size_t j=0; j<lines.size(); j++) {
-      string_lines.push_back(lines[j]);
-    }
-    fieldm->parseSetup(string_lines,fieldSubSect[i],errors);
+  vector<std::string> lines;
+  for (const std::string& suse : fieldm->subsections()) {
+    SetupParser::getSection(suse, lines);
+    fieldm->parseSetup(lines, suse, errors);
   }
   //Write error messages
   int nerror = errors.size();
@@ -791,7 +781,7 @@ vector<std::string> Controller::getFieldLevels(const PlotCommand_cp& pinfo)
     return std::vector<std::string>();
 }
 
-set<std::string> Controller::getFieldReferenceTimes(const std::string model)
+set<std::string> Controller::getFieldReferenceTimes(const std::string& model)
 {
   return fieldm->getReferenceTimes(model);
 }

src/diController.h
@@ -304,7 +304,7 @@ public:
   /// return model/file groups and contents to FieldDialog
   std::vector<FieldDialogInfo> initFieldDialog();
   ///return all reference times for the given model
-  std::set<std::string> getFieldReferenceTimes(const std::string model);
+  std::set<std::string> getFieldReferenceTimes(const std::string& model);
   ///return the reference time given by refOffset and refhour or the last reference time for the given model
   std::string getBestFieldReferenceTime(const std::string& model, int refOffset, int refHour);
   ///return referenceTime of first FieldPlot

src/diEditManager.cc
@@ -1148,38 +1148,39 @@ bool EditManager::makeNewFile(int fnum, bool local, QString& message)
   }
 
   //  outputFilename += EdProd.fields[fnum].filename;
-  for (size_t i=0; i<outputFilenames.size();++i) {
-	if ( QFile::exists(outputFilenames[i].c_str()) && !QFile::remove(outputFilenames[i].c_str()) ){
-	  message = "Copy from " + QString(EdProd.templateFilename.c_str()) + " to " + QString(outputFilenames[i].c_str()) + "  failed. (File exsists, but can't be overwritten.)";
-	  return false;
-	}
-	if (!qfile.copy(outputFilenames[i].c_str())){
-      message = "Copy from "  + QString(EdProd.templateFilename.c_str()) + " to " + QString(outputFilenames[i].c_str()) + "  failed";
+  for (const std::string& ofi : outputFilenames) {
+    const QString qofi = QString::fromStdString(ofi);
+    const QString qtfi = QString::fromStdString(EdProd.templateFilename);
+    if (QFile::exists(qofi) && !QFile::remove(qofi)) {
+      message = "Copy from " + qtfi + " to " + qofi + "  failed. (File exsists, but can't be overwritten.)";
       return false;
-	}
+    }
+    if (!qfile.copy(qofi)) {
+      message = "Copy from " + qtfi + " to " + qofi + "  failed";
+      return false;
+    }
 
-	std::string fileType = "fimex";
+    std::string fileType = "fimex";
 
-	std::vector<std::string> filenames;
-	std::string modelName = outputFilenames[i];
-	filenames.push_back(outputFilenames[i]);
+    std::vector<std::string> filenames;
+    std::string modelName = ofi;
+    filenames.push_back(ofi);
 
-	std::vector<std::string> format;
-	format.push_back("netcdf");
+    std::vector<std::string> format;
+    format.push_back("netcdf");
 
-	std::vector<std::string> config;
+    std::vector<std::string> config;
 
-	std::vector<std::string> option;
-	std::string opt = "writeable=true";
-	option.push_back(opt);
+    std::vector<std::string> option;
+    std::string opt = "writeable=true";
+    option.push_back(opt);
 
-	fieldPlotManager->addGridCollection(fileType, modelName, filenames,
-			format,config, option);
+    fieldPlotManager->addGridCollection(fileType, modelName, filenames, format, config, option);
 
-	vector<FieldGroupInfo> fgi;
-	std::string reftime = fieldPlotManager->getBestFieldReferenceTime(modelName,0,-1 );
-	METLIBS_LOG_INFO(LOGVAL(modelName)<<LOGVAL(reftime));
-	fieldPlotManager->getFieldGroups(modelName,reftime,true,fgi);
+    vector<FieldGroupInfo> fgi;
+    std::string reftime = fieldPlotManager->getBestFieldReferenceTime(modelName, 0, -1);
+    METLIBS_LOG_INFO(LOGVAL(modelName) << LOGVAL(reftime));
+    fieldPlotManager->getFieldGroups(modelName, reftime, true, fgi);
   }
   return true;
 

src/diField/diFieldCache.cc
@@ -145,12 +145,12 @@ void FieldCache::set(Field* f)
   FieldCacheKeyset keyset;
   keyset.setKeys(f);
 
-  if(fields.count(keyset))
+  if (fields.count(keyset))
     throw ModifyFieldCacheException("Trying to set an existing Field");
 
-    cleanbyAge();
+  cleanbyAge();
 
-  unsigned long tmpsize=bytesize_+f->bytesize();
+  unsigned long tmpsize = bytesize_ + f->bytesize();
   if (maximumsize_)
     if (tmpsize > maximumsize_) {
       long overflowsize = tmpsize - maximumsize_;
@@ -158,11 +158,11 @@ void FieldCache::set(Field* f)
       METLIBS_LOG_DEBUG("calling cleanOverFlow to make room for " << keyset);
       METLIBS_LOG_DEBUG("State of cache\n"  << *(this));
 
-        cleanOverflow(overflowsize*2);
+      cleanOverflow(overflowsize * 2);
     }
 
   fields[keyset].set(f);
-  bytesize_+=fields[keyset].bytesize();
+  bytesize_ += fields[keyset].bytesize();
 
   METLIBS_LOG_DEBUG("set() " << keyset  << ": new total size " << size());
 }

src/diField/diFieldCacheKeyset.cc
@@ -45,8 +45,8 @@ FieldCacheKeyset::FieldCacheKeyset(const FieldCacheKeyset& fck)
 
 void FieldCacheKeyset::setModel(const std::string& m)
 {
- model_ = m;
- makeKey();
+  model_ = m;
+  makeKey();
 }
 
 void FieldCacheKeyset::setReference (const miutil::miTime& r)
@@ -103,7 +103,7 @@ void FieldCacheKeyset::setKeys(const Field* field)
 
 void FieldCacheKeyset::makeKey()
 {
-  key_=model_+reference_.isoTime()+name_+level_+idnum_+valid_.isoTime();
+  key_ = model_ + reference_.isoTime() + name_ + level_ + idnum_ + valid_.isoTime();
 }
 
 std::ostream& operator<<(std::ostream& out,const FieldCacheKeyset& e )

src/diField/diFieldManager.cc
@@ -34,8 +34,9 @@
 
 #include "diFieldManager.h"
 
-#include "diMetConstants.h"
 #include "GridCollection.h"
+#include "diFieldCache.h"
+#include "diMetConstants.h"
 
 #include "../diUtilities.h"
 #include "util/misc_util.h"
@@ -633,99 +634,80 @@ std::string FieldManager::getBestReferenceTime(const std::string& modelName,
     return "";
 
   //if refime is not a valid time, return string
-  if (!miTime::isValid(*(refTimes.rbegin()))) {
-    return (*(refTimes.rbegin()));
+  const std::string& last = *(refTimes.rbegin());
+  if (!miTime::isValid(last)) {
+    return last;
   }
 
-  miTime refTime(*(refTimes.rbegin()));
+  miTime refTime(last);
 
   if (refHour > -1) {
     miDate date = refTime.date();
     miClock clock(refHour, 0, 0);
     refTime = miTime(date, clock);
   }
-
-  refTime.addDay(refOffset);
-  std::string refString = refTime.isoTime("T");
-
-  set<std::string>::iterator p = refTimes.begin();
-  while (p != refTimes.end() && *p != refString)
-    ++p;
-  if (p != refTimes.end()) {
-    return *p;
+  if (refOffset != 0) {
+    refTime.addDay(refOffset);
   }
 
-  //referencetime not found. If refHour is given and no refoffset, try yesterday
+  std::set<std::string>::const_iterator p = refTimes.find(refTime.isoTime("T"));
+  if (p != refTimes.end())
+    return *p;
+
+  // referencetime not found. If refHour is given and no refoffset, try yesterday
   if (refHour > -1 && refOffset == 0) {
     refTime.addDay(-1);
-    refString = refTime.isoTime("T");
-    p = refTimes.begin();
-    while (p != refTimes.end() && *p != refString)
-      ++p;
-    if (p != refTimes.end()) {
+    p = refTimes.find(refTime.isoTime("T"));
+    if (p != refTimes.end())
       return *p;
-    }
   }
   return "";
 }
 
-bool FieldManager::makeField(Field*& fout, FieldRequest fieldrequest,
-    int cacheOptions)
+bool FieldManager::makeField(Field*& fout, FieldRequest frq, int cacheOptions)
 {
-  METLIBS_LOG_TIME(LOGVAL(fieldrequest.modelName) << LOGVAL(fieldrequest.paramName)
-      << LOGVAL(fieldrequest.zaxis) << LOGVAL(fieldrequest.refTime)
-      << LOGVAL(fieldrequest.ptime) << LOGVAL(fieldrequest.plevel)
-      << LOGVAL(fieldrequest.elevel) << LOGVAL(fieldrequest.unit));
-
+  METLIBS_LOG_TIME(LOGVAL(frq.modelName) << LOGVAL(frq.paramName) << LOGVAL(frq.zaxis) << LOGVAL(frq.refTime) << LOGVAL(frq.ptime) << LOGVAL(frq.plevel)
+                                         << LOGVAL(frq.elevel) << LOGVAL(frq.unit));
 
   //Find best reference time
-  if (fieldrequest.refTime.empty()) {
-    fieldrequest.refTime = getBestReferenceTime(fieldrequest.modelName,
-        fieldrequest.refoffset, fieldrequest.refhour);
+  if (frq.refTime.empty()) {
+    frq.refTime = getBestReferenceTime(frq.modelName, frq.refoffset, frq.refhour);
   }
 
-    // If READ_RESULT, try to read from cache
-    if ((cacheOptions & (READ_RESULT | READ_ALL)) != 0
-        && miTime::isValid(fieldrequest.refTime) && !fieldrequest.ptime.undef()) {
-      FieldCacheKeyset keyset;
-      miTime reftime(fieldrequest.refTime);
-      keyset.setKeys(fieldrequest.modelName, reftime, fieldrequest.paramName,
-          fieldrequest.plevel, fieldrequest.elevel, fieldrequest.ptime);
+  // If READ_RESULT, try to read from cache
+  if ((cacheOptions & (READ_RESULT | READ_ALL)) != 0 && miTime::isValid(frq.refTime) && !frq.ptime.undef()) {
+    FieldCacheKeyset keyset;
+    miTime reftime(frq.refTime);
+    keyset.setKeys(frq.modelName, reftime, frq.paramName, frq.plevel, frq.elevel, frq.ptime);
 
-      METLIBS_LOG_DEBUG("SEARCHING FOR :" << keyset << " in CACHE");
+    METLIBS_LOG_DEBUG("SEARCHING FOR :" << keyset << " in CACHE");
 
-      if (fieldcache->hasField(keyset)) {
-        return fieldcache->get(keyset);
-      }
-
+    if (fieldcache->hasField(keyset)) {
+      return fieldcache->get(keyset);
     }
+  }
 
-  GridCollectionPtr pgc = getGridCollection(fieldrequest.modelName, fieldrequest.refTime,
-      false, fieldrequest.checkSourceChanged);
+  GridCollectionPtr pgc = getGridCollection(frq.modelName, frq.refTime, false, frq.checkSourceChanged);
   if (!pgc) {
-    METLIBS_LOG_WARN("could not find grid collection for model=" << fieldrequest.modelName
-        << " reftime=" << fieldrequest.refTime);
+    METLIBS_LOG_WARN("could not find grid collection for model=" << frq.modelName << " reftime=" << frq.refTime);
     return false;
   }
 
-  fout = pgc->getField(fieldrequest);
+  fout = pgc->getField(frq);
 
   if (fout == 0) {
-    METLIBS_LOG_WARN(
-        "No field read: model: " << fieldrequest.modelName << ", parameter:"
-            << fieldrequest.paramName << ", level:" << fieldrequest.plevel
-            << ", time:" << fieldrequest.ptime << ", referenceTime:"
-            << fieldrequest.refTime);
+    METLIBS_LOG_WARN("No field read: model: " << frq.modelName << ", parameter:" << frq.paramName << ", level:" << frq.plevel << ", time:" << frq.ptime
+                                              << ", referenceTime:" << frq.refTime);
     return false;
   }
 
-  fout->leveltext = fieldrequest.plevel;
-  fout->idnumtext = fieldrequest.elevel;
-  fout->paramName = fieldrequest.paramName;
-  fout->modelName = fieldrequest.modelName;
+  fout->leveltext = frq.plevel;
+  fout->idnumtext = frq.elevel;
+  fout->paramName = frq.paramName;
+  fout->modelName = frq.modelName;
 
-  if (!fieldrequest.palette.empty()) {
-    fout->palette = pgc->getVariable(fieldrequest.refTime, fieldrequest.palette);
+  if (!frq.palette.empty()) {
+    fout->palette = pgc->getVariable(frq.refTime, frq.palette);
   }
 
   if ((cacheOptions & (WRITE_ALL | WRITE_RESULT)) != 0) {

src/diFieldEdit.cc
@@ -661,8 +661,7 @@ bool FieldEdit::notifyEditEvent(const EditEvent& ee)
       float gy= ee.y;
       maparea = getStaticPlot()->getMapArea();
       if (maparea.P()!=editfield->area.P()) {
-        int npos= 1;
-        if (!GridConverter::getPoints(maparea.P(), editfield->area.P(), npos, &gx, &gy)) {
+        if (!GridConverter::getPoints(maparea.P(), editfield->area.P(), 1, &gx, &gy)) {
           METLIBS_LOG_ERROR("EDIT: getPoints error");
           return false;
         }
@@ -682,7 +681,7 @@ bool FieldEdit::notifyEditEvent(const EditEvent& ee)
         ny= editfield->area.ny;
         int nsmooth=0;
         isoline= findIsoLine(gx,gy,fv,nsmooth,nx,ny,workfield->data,discontinuous);
-        if (!isoline.x.size()) {
+        if (isoline.x.empty()) {
           repaint= false;
         } else {
           drawIsoline= true;

commit 87a8674129638cbe23624083ef36b9847d28b773

bugfix GetLineConverter
 * regression from f3764a01c5ad9b313caf6924e75061efecf125d5
 * added test case

src/util/charsets.cc
@@ -189,7 +189,7 @@ GetLineConverter::GetLineConverter(const std::string& comment, const std::string
 
 std::istream& GetLineConverter::operator()(std::istream& in, std::string& line)
 {
-  if (!std::getline(in, line))
+  if (std::getline(in, line))
     apply(line);
   return in;
 };

test/TestUtilities.cc
@@ -243,7 +243,7 @@ TEST(TestUtilities, Utf8ToLatin1)
   EXPECT_EQ("\xC5r\xF8", c->convert("Årø"));
 }
 
-TEST(TestUtilities, GetLineConverter)
+TEST(TestUtilities, GetLineConverterUTF8)
 {
   std::istringstream stream(
         "# -*- coding: utf-8 -*-\n"
@@ -257,6 +257,22 @@ TEST(TestUtilities, GetLineConverter)
     found = l;
   }
   EXPECT_EQ("blueberry = blåbær", found);
+}
+
+TEST(TestUtilities, GetLineConverterLatin1)
+{
+  std::istringstream stream(
+        // no coding specified, expecting iso8859-1
+        "blue = bl\xE5\n"
+        );
+  diutil::GetLineConverter convertline("#");
+  std::string l, found;
+  while (convertline(stream, l)) {
+    if (l.empty() || l[0]=='#')
+      continue;
+    found = l;
+  }
+  EXPECT_EQ("blue = bl\xC3\xA5", found); // this is utf-8
 }
 
 TEST(TestUtilities, GreatCircleDistance)