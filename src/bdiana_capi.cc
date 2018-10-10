e43af8d15a3ef2928989029370a28b79e488941d
Alexander Bürger <alexander.buerger@met.no>
2018-10-10 08:00:08 +0200

    revert "bugfix": bdiana time choice
     * the problem was elsewhere
     * the reverted change was actually solving a problem with specifying
       TIME=... on the command line
    
    (cherry picked from commit 56476494f0af92f2b097f9f84bd9899962576e75)
    (cherry picked from commit b1a6e74676e5c1a302c9479e7ee5860b849800e1)

--- src/bdiana_capi.cc
+++ src/bdiana_capi.cc
@@ -870,7 +870,7 @@ static void printUsage(bool showexample)
 
 void Bdiana::set_ptime(BdianaSource& src)
 {
-  if (ptime.undef()) {
+  if (fixedtime.undef()) {
     fixedtime = src.getTime();
     if (verbose)
       METLIBS_LOG_INFO("using default time:" << format_time(fixedtime));
