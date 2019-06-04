metno/diana

commit 11823927bd0c5ead27c5f5b5b523c2602745e51a

bdiana: improve handling of missing times
 - log WARN message and avoid plotting but continue

src/bdiana_source.cc
@@ -72,6 +72,15 @@ miutil::miTime BdianaSource::getTime()
   return *times.rbegin();
 }
 
+bool BdianaSource::selectTime(const miutil::miTime& time)
+{
+  setTime(time);
+  if (time.undef())
+    return false;
+  const plottimes_t times = getTimes();
+  return (times.find(time) != times.end());
+}
+
 void BdianaSource::setTimeChoice(TimeChoice tc)
 {
   use_time_ = tc;

src/bdiana_source.h
@@ -52,6 +52,7 @@ public:
   virtual miutil::miTime getTime();
 
   virtual void setTime(const miutil::miTime& time) = 0;
+  virtual bool selectTime(const miutil::miTime& time);
 
   TimeChoice getTimeChoice() const { return use_time_; }
   virtual void setTimeChoice(TimeChoice tc);

commit 8499b849231f095e301f0b9afc411967fb614ca9

Bugfix - bdiana - accepting plots with no time
THe bug was introduced in 11823927bd0c5ead27c5f5b5b523c2602745e51a

src/bdiana_source.cc
@@ -78,7 +78,7 @@ bool BdianaSource::selectTime(const miutil::miTime& time)
   if (time.undef())
     return false;
   const plottimes_t times = getTimes();
-  return (times.find(time) != times.end());
+  return (times.size() == 0 || times.find(time) != times.end());
 }
 
 void BdianaSource::setTimeChoice(TimeChoice tc)