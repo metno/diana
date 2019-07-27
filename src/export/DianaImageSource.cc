metno/diana

commit ac319e8f0c7ba8c6e85b33554bc1e2d884a4410c

bugfix diana series export: set first timestep

src/export/DianaImageSource.cc
@@ -52,13 +52,14 @@ void DianaImageSource::setTimes(const std::vector<miutil::miTime>& times)
 
 void DianaImageSource::prepare(bool printing, bool single)
 {
+  PaintableImageSource::prepare(printing, single);
   oldTime_ = controller()->getPlotTime();
   if (single || times_.empty()) {
     times_.clear();
     times_.push_back(oldTime_);
   }
   timestep_ = 0;
-  PaintableImageSource::prepare(printing, single);
+  setTimeFromStep();
 }
 
 int DianaImageSource::count()
@@ -97,18 +98,27 @@ void DianaImageSource::paintGL(DiPaintGLPainter* gl)
 bool DianaImageSource::next()
 {
   timestep_ += 1;
+  return setTimeFromStep();
+}
+
+bool DianaImageSource::setTimeFromStep()
+{
   if (timestep_ >= times_.size())
     return false;
-  controller()->setPlotTime(times_[timestep_]);
-  controller()->updatePlots();
+  setPlotTime(times_[timestep_]);
   return true;
+}
+
+void DianaImageSource::setPlotTime(const miutil::miTime& time)
+{
+  controller()->setPlotTime(time);
+  controller()->updatePlots();
 }
 
 void DianaImageSource::finish()
 {
   PaintableImageSource::finish();
-  controller()->setPlotTime(oldTime_);
-  controller()->updatePlots();
+  setPlotTime(oldTime_);
 }
 
 Controller* DianaImageSource::controller()

src/export/DianaImageSource.h
@@ -62,6 +62,8 @@ public:
 
 private:
   Controller* controller();
+  bool setTimeFromStep();
+  void setPlotTime(const miutil::miTime& time);
 
 private:
   bool annotationsOnly_;

src/export/qtExportImageDialog.cc
@@ -641,7 +641,7 @@ void ExportImageDialog::enableStartButton()
   bool enable = false;
   if (exporting > 0) {
     ui->buttonExport->setText(tr("Exporting..."));
-  } else {
+  } else if (imageSource && imageSource->count() > 0) {
     ui->buttonExport->setText(tr("Export"));
     const Product product = static_cast<Product>(ui->comboProduct->currentIndex());
     const int saveTo = ui->comboSaveTo->currentIndex();

commit 12af8850b5a23e75a37c017f0e3984aef3993f9f

improve bugfix diana series export
 * only set first timestep if time actually changes, avoid reading data twice
 * regression in ac319e8f

src/export/DianaImageSource.cc
@@ -1,7 +1,7 @@
 /*
   Diana - A Free Meteorological Visualisation Tool
 
-  Copyright (C) 2017 met.no
+  Copyright (C) 2017-2019 met.no
 
   Contact information:
   Norwegian Meteorological Institute
@@ -111,8 +111,10 @@ bool DianaImageSource::setTimeFromStep()
 
 void DianaImageSource::setPlotTime(const miutil::miTime& time)
 {
-  controller()->setPlotTime(time);
-  controller()->updatePlots();
+  if (time != controller()->getPlotTime()) {
+    controller()->setPlotTime(time);
+    controller()->updatePlots();
+  }
 }
 
 void DianaImageSource::finish()