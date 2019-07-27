metno/diana

commit 7b32576848d70334fdc7eb033db44781b8a78ecc

moved everything static from Plot to StaticPlot reformatting PlotModule  - moved utility functions from diPlotModule.cc to diUtilities.h added diutil::glob and use it instead of puCglob EditManager and ObjectManager no longer friends of PlotModule moved sat plots from PlotModule::vsp to SatManager merged 3*xyclip implementations into one, moved to diUtilities.cc
git-svn-id: https://svn.met.no/diana/trunk@4366 0ccb8871-b436-41a3-ae7d-e3ac59973202

src/bdiana_capi.cc
@@ -71,10 +71,11 @@
 #include <diPrintOptions.h>
 #include <diFontManager.h>
 #include <diImageIO.h>
-#include <puCtools/puCglob.h>
-#include <puCtools/glob_cache.h>
+#include "diUtilities.h"
 #include <puTools/miSetupParser.h>
 #include <puTools/mi_boost_compatibility.hh>
+#include <miLogger/logger.h>
+#include <miLogger/LogHandler.h>
 
 
 #ifdef USE_VCROSS_V2
@@ -2585,16 +2586,9 @@ static int parseAndProcess(istream &is)
       vector<std::string> newlines;
       std::string waitline = com_wait_for_commands;
 
-      glob_t globBuf;
-      int number_of_files = 0;
-      while (number_of_files == 0) {
-        glob(pattern.c_str(), 0, 0, &globBuf);
-        number_of_files = globBuf.gl_pathc;
-        if (number_of_files == 0) {
-          globfree_cache(&globBuf);
-          pu_sleep(1);
-        }
-      }
+      diutil::string_v matches;
+      while ((matches = diutil::glob(pattern)).empty())
+        pu_sleep(1);
 
       nowtime = miTime::nowTime();
       prev_iclock = clock();
@@ -2603,8 +2597,8 @@ static int parseAndProcess(istream &is)
       vector<std::string> filenames;
 
       //loop over files
-      for (int ij = 0; ij < number_of_files; ij++) {
-        std::string filename = globBuf.gl_pathv[ij];
+      for (diutil::string_v::const_iterator it = matches.begin(); it != matches.end(); ++it) {
+        const std::string& filename = *it;
         METLIBS_LOG_INFO("==== Reading file:" << filename);
         filenames.push_back(filename);
         ifstream file(filename.c_str());
@@ -2621,7 +2615,6 @@ static int parseAndProcess(istream &is)
           }
         }
       }
-      globfree_cache(&globBuf);
       // remove processed files
       for (unsigned int ik = 0; ik < filenames.size(); ik++) {
         ostringstream ost;
@@ -2720,23 +2713,27 @@ static int parseAndProcess(istream &is)
             fieldPatterns.insert(fileNames.begin(), fileNames.end());
           }
 
-          map<string, map<string,SatManager::subProdInfo> > satProducts = main_controller->getSatelliteManager()->getProductsInfo();
+          const SatManager::Prod_t& satProducts = main_controller->getSatelliteManager()->getProductsInfo();
           set<std::string> satPatterns;
           set<std::string> satFiles;
 
-          vector<SatPlot*> satellitePlots = main_controller->getSatellitePlots();
-          for (vector<SatPlot*>::iterator it = satellitePlots.begin(); it != satellitePlots.end(); ++it) {
-              Sat* sat = (*it)->satdata;
-              if (satProducts.find(sat->satellite) != satProducts.end() && satProducts[sat->satellite].find(sat->filetype) != satProducts[sat->satellite].end()) {
-                  SatManager::subProdInfo satInfo = satProducts[sat->satellite][sat->filetype];
-                  for (vector<SatFileInfo>::iterator itsf = satInfo.file.begin(); itsf != satInfo.file.end(); ++itsf) {
-                    satFiles.insert(itsf->name);
-                    if (itsf->name == sat->actualfile) {
-                        for (vector<string>::iterator itp = satInfo.pattern.begin(); itp != satInfo.pattern.end(); ++itp)
-                            satPatterns.insert(*itp);
-                    }
+          const vector<SatPlot*>& satellitePlots = main_controller->getSatellitePlots();
+          for (vector<SatPlot*>::const_iterator it = satellitePlots.begin(); it != satellitePlots.end(); ++it) {
+            Sat* sat = (*it)->satdata;
+            const SatManager::Prod_t::const_iterator itp = satProducts.find(sat->satellite);
+            if (itp != satProducts.end()) {
+              const SatManager::SubProd_t::const_iterator itsp = itp->second.find(sat->satellite);
+              if (itsp != itp->second.end()) {
+                const SatManager::subProdInfo& satInfo = itsp->second;
+                for (vector<SatFileInfo>::const_iterator itsf = satInfo.file.begin(); itsf != satInfo.file.end(); ++itsf) {
+                  satFiles.insert(itsf->name);
+                  if (itsf->name == sat->actualfile) {
+                    for (vector<string>::const_iterator itp = satInfo.pattern.begin(); itp != satInfo.pattern.end(); ++itp)
+                      satPatterns.insert(*itp);
                   }
+                }
               }
+            }
           }
 
           map<string,ObsManager::ProdInfo> obsProducts = main_controller->getObservationManager()->getProductsInfo();
@@ -3391,26 +3388,15 @@ static void doWork()
     return;
   }
 
-  string pattern = command_path;
-  glob_t globBuf;
-  int number_of_files = 0;
-
-  glob(pattern.c_str(), 0, 0, &globBuf);
-  number_of_files = globBuf.gl_pathc;
-
-  if (number_of_files == 0) {
-          COMMON_LOG::getInstance("common").warnStream() << "WARNING, scan for commands returned nothing";
-    globfree_cache(&globBuf);
+  const diutil::string_v matches = diutil::glob(command_path);
+  if (matches.empty()) {
+    COMMON_LOG::getInstance("common").warnStream() << "WARNING, scan for commands returned nothing";
     return;
   }
 
   // loop over files
-  for (int i = 0; i < number_of_files; i++) {
-    string filename = globBuf.gl_pathv[i];
-    dispatchWork(filename);
-  }
-
-  globfree_cache(&globBuf);
+  for (diutil::string_v::const_iterator it = matches.begin(); it != matches.end(); ++it)
+    dispatchWork(*it);
 }
 
 

src/diController.cc
@@ -107,14 +107,14 @@ void Controller::setColourIndices(std::vector<Colour::ColourInfo>& vc){
 
 void  Controller::restartFontManager()
 {
-  Plot::restartFontManager();
+  StaticPlot::restartFontManager();
 }
 
 bool Controller::parseSetup()
 {
   METLIBS_LOG_SCOPE();
 
-  Plot::initFontManager();
+  StaticPlot::initFontManager();
 
   //Parse field sections
   vector<std::string> fieldSubSect = fieldm->subsections();
@@ -147,12 +147,9 @@ bool Controller::parseSetup()
   if (!editm->parseSetup()) return false;
   if (!stam->parseSetup()) return false;
 
-  map<string,Manager*>::iterator it = plotm->managers.begin();
-
-  while (it != plotm->managers.end()) {
+  for (PlotModule::managers_t::iterator it = plotm->managers.begin(); it != plotm->managers.end(); ++it) {
     if (!it->second->parseSetup())
       return false;
-    ++it;
   }
 
   MapManager mapm;
@@ -166,23 +163,19 @@ bool Controller::parseSetup()
 
 void Controller::plotCommands(const vector<string>& inp){
 #ifdef DEBUGPRINT
+  METLIBS_LOG_SCOPE();
   for (int q = 0; q < inp.size(); q++)
-  METLIBS_LOG_DEBUG("++ Controller::plotCommands:" << inp[q]);
+    METLIBS_LOG_DEBUG("inp['" << q << "]='" << inp[q] << "'");
 #endif
   plotm->preparePlots(inp);
-#ifdef DEBUGPRINT
-  METLIBS_LOG_DEBUG("++ Returning from Controller::plotCommands ++");
-#endif
 }
 
-void Controller::plot(bool under, bool over){
+void Controller::plot(bool under, bool over)
+{
 #ifdef DEBUGPRINT
-  METLIBS_LOG_DEBUG("++ Controller::plot() ++");
+  METLIBS_LOG_SCOPE();
 #endif
   plotm->plot(under, over);
-#ifdef DEBUGPRINT
-  METLIBS_LOG_DEBUG("++ Returning from Controller::plot() ++");
-#endif
 }
 
 vector<AnnotationPlot*> Controller::getAnnotations()
@@ -407,12 +400,12 @@ void Controller::obsStepChanged(int step){
 // get name++ of current channels (with calibration)
 vector<string> Controller::getCalibChannels()
 {
-  return plotm->getCalibChannels();
+  return satm->getCalibChannels();
 }
 
 // show values in grid position x,y
 vector<SatValues> Controller::showValues(float x, float y){
-  return plotm->showValues(x,y);
+  return satm->showValues(x,y);
 }
 
 // show or hide satelitte classificiation table
@@ -426,7 +419,7 @@ vector<SatValues> Controller::showValues(float x, float y){
 
 vector<string> Controller::getSatnames()
 {
-  return plotm->getSatnames();
+  return satm->getSatnames();
 }
 
 void Controller::showAnnotations(bool on){
@@ -533,15 +526,13 @@ void Controller::sendMouseEvent(QMouseEvent* me, EventResult& res)
     // Send the event to the other managers to see if one of them will handle it.
     bool handled = false;
     if (!(me->modifiers() & Qt::ShiftModifier)) {
-      map<string,Manager*>::iterator it = plotm->managers.begin();
-      while (it != plotm->managers.end()) {
+      for (PlotModule::managers_t::iterator it = plotm->managers.begin(); it != plotm->managers.end(); ++it) {
         if (it->second->isEditing()) {
           it->second->sendMouseEvent(me, res);
           if (me->isAccepted())
             handled = true;
           break;
         }
-        ++it;
       }
     }
     if (!handled) {
@@ -592,15 +583,13 @@ void Controller::sendKeyboardEvent(QKeyEvent* ke, EventResult& res)
 
   // A more general way to override normal keypress behaviour is to query
   // the managers to find any that are in editing mode.
-  map<string,Manager*>::iterator it = plotm->managers.begin();
-  while (it != plotm->managers.end()) {
+  for (PlotModule::managers_t::iterator it = plotm->managers.begin(); it != plotm->managers.end(); ++it) {
     if (it->second->isEditing()) {
       res.savebackground = true;
       it->second->sendKeyboardEvent(ke, res);
       if (it->second->hasFocus())
         return;
     }
-    ++it;
   }
 
   // Access to normal keypress behaviour is obtained by holding down the Shift key
@@ -755,13 +744,13 @@ void Controller::SatRefresh(const std::string& satellite, const std::string& fil
 bool Controller::satFileListChanged(){
   // returns information about whether list of satellite files have changed
   //hence dialog and timeSlider times should change as well
-  return satm->fileListChanged;
+  return satm->isFileListChanged();
 }
 
 void Controller::satFileListUpdated(){
   //called when the dialog and timeSlider updated with info from satellite
   //file list
-  satm->fileListChanged = false;
+  satm->setFileListChanged(false);
 }
 
 bool Controller::obsTimeListChanged(){
@@ -778,8 +767,9 @@ void Controller::obsTimeListUpdated(){
 
 
 void Controller::setSatAuto(bool autoFile,const std::string& satellite,
-                            const std::string& file){
-  plotm->setSatAuto(autoFile,satellite,file);
+                            const std::string& file)
+{
+  satm->setSatAuto(autoFile,satellite,file);
 }
 
 
@@ -1050,7 +1040,7 @@ map<string,InfoFile> Controller::getInfoFiles()
 }
 
 
-vector<PlotElement>& Controller::getPlotElements()
+vector<PlotElement> Controller::getPlotElements()
 {
   return plotm->getPlotElements();
 }
@@ -1079,9 +1069,9 @@ bool Controller::useScrollwheelZoom() {
 }
 
 // Miscellaneous get methods
-vector<SatPlot*> Controller::getSatellitePlots() const
+const vector<SatPlot*>& Controller::getSatellitePlots() const
 {
-  return plotm->getSatellitePlots();
+  return satm->getSatellitePlots();
 }
 
 vector<FieldPlot*> Controller::getFieldPlots() const
@@ -1101,9 +1091,9 @@ void Controller::addManager(const std::string &name, Manager *man)
 
 Manager *Controller::getManager(const std::string &name)
 {
-  map<string,Manager*>::iterator it = plotm->managers.find(name);
+  PlotModule::managers_t::iterator it = plotm->managers.find(name);
   if (it != plotm->managers.end())
-    return plotm->managers[name];
+    return it->second;
   else
     return 0;
 }

src/diController.h
@@ -371,7 +371,7 @@ public:
 
   std::map<std::string,InfoFile> getInfoFiles();
 
-  std::vector<PlotElement>& getPlotElements();
+  std::vector<PlotElement> getPlotElements();
   void enablePlotElement(const PlotElement& pe);
 
 /********************* reading and writing log file *******************/
@@ -384,7 +384,7 @@ public:
   bool useScrollwheelZoom();
 
   // Miscellaneous get methods
-  std::vector<SatPlot*> getSatellitePlots() const;   // Returns a vector of defined satellite plots.
+  const std::vector<SatPlot*>& getSatellitePlots() const;   // Returns a vector of defined satellite plots.
   std::vector<FieldPlot*> getFieldPlots() const;      // Returns a vector of defined field plots.
   std::vector<ObsPlot*> getObsPlots() const;         // Returns a vector of defined observation plots.
 };

src/diSatManager.h
@@ -31,7 +31,7 @@
 #ifndef diSatManager_h
 #define diSatManager_h
 
-#include <diPlot.h>
+#include <diAnnotationPlot.h>
 #include <diSat.h>
 #include <diSatPlot.h>
 #include <diCommonTypes.h>
@@ -93,43 +93,40 @@ public:
     bool mosaic; //ok to make mosaic
   };
 
-private:
-  std::map<std::string, std::map<std::string,subProdInfo> > Prod;
-  SatDialogInfo Dialog;
-  std::map<std::string,std::string> channelmap; // ex: name:1+2+3 -> channelmap[name]=1+2+3
+  typedef std::map<std::string, subProdInfo> SubProd_t;
+  typedef std::map<std::string, SubProd_t> Prod_t;
 
-  // needed for getFiles error return
-  const std::vector<SatFileInfo> emptyfile;
+private:
+  Prod_t Prod;
+  SatDialogInfo Dialog;
+
+  typedef std::map<std::string,std::string> channelmap_t;
+  channelmap_t channelmap; // ex: name:1+2+3 -> channelmap[name]=1+2+3
+
   bool useArchive; //read archive files too.
 
 /************************************************************************/
 
-//current sat image and plot
-  Sat *satdata;
-  SatPlot * sp;
-
   int updateFreq;   //Max time between filelist updates in seconds
   miutil::miTime ztime;     //zero time = 00:00:00 UTC Jan 1 1970
-  int timeDiff;
 
-  // Copy members
-  void memberCopy(const SatManager& rhs);
-  void getMosaicfiles();
-  void addMosaicfiles();
+  void getMosaicfiles(Sat* satdata);
+  void addMosaicfiles(Sat* satdata);
   std::vector<SatFileInfo> mosaicfiles;
 
-  void cutImage(unsigned char*, float, int&, int&);
-  void setRGB();
-  void setPalette(SatFileInfo &);
+  void cutImage(Sat* satdata, unsigned char*, float, int&, int&);
+  void setRGB(Sat* satdata);
+  void setPalette(Sat* satdata, SatFileInfo &);
   void listFiles(subProdInfo &subp);
   bool readHeader(SatFileInfo &, std::vector<std::string> &);
 
   bool _isafile(const std::string name);
   unsigned long _modtime(const std::string fname);
   int _filestat(const std::string fname, pu_struct_stat& filestat);
-  bool parseChannels(SatFileInfo &info);
-  bool readSatFile();
+  bool parseChannels(Sat* satdata, SatFileInfo &info);
+  bool readSatFile(Sat* satdata);
 
+  bool init(const std::vector<std::string>&);
   void init_rgbindex(Sat& sd);
   void init_rgbindex_Meteosat(Sat& sd);
 
@@ -141,13 +138,49 @@ private:
   };
   ColourStretchInfo colourStretchInfo;
 
+  typedef std::vector<SatPlot*> SatPlot_xv;
+  SatPlot_xv vsp;   // vector of satellite plots
+
+  bool fileListChanged;
+
+  bool setData(SatPlot *satp);
+  void cutImageRGBA(Sat* satdata, unsigned char *image, float cut, int *index);
+  int getFileName(Sat* satdata, std::string &);
+  int getFileName(Sat* satdata, const miutil::miTime&);
+
 public:
-  // Constructors
   SatManager();
 
-  bool init(std::vector<SatPlot*>&, const std::vector<std::string>&);
-  bool setData(SatPlot *);
-  std::vector<miutil::miTime> getSatTimes(const std::vector<std::string>&, bool updateFileList=false, bool openFiles=false);
+  /// handles images plot info strings
+  void prepareSat(const std::vector<std::string>& inp);
+
+  void addPlotElements(std::vector<PlotElement>& pel);
+  void enablePlotElement(const PlotElement& pe);
+  void addSatAnnotations(std::vector<AnnotationPlot::Annotation>& annotations);
+  void getSatAnnotations(std::vector<std::string>& anno);
+  void plot();
+  void clear();
+  bool getGridResolution(float& rx, float& ry);
+
+  bool setData();
+  bool getSatArea(Area& a) const
+    { if (vsp.empty()) return false; a = vsp.front()->getSatArea(); return true; }
+
+  bool isFileListChanged() const
+    { return fileListChanged; }
+  void setFileListChanged(bool flc)
+    { fileListChanged = flc; }
+
+  std::vector<miutil::miTime> getSatTimes(bool updateFileList=false, bool openFiles=false);
+
+  /// get name++ of current channels (with calibration)
+  std::vector<std::string> getCalibChannels();
+  ///show pixel values in status bar
+  std::vector<SatValues> showValues(float x, float y);
+  ///get satellite name from all SatPlots
+  std::vector <std::string> getSatnames();
+  ///satellite follows main plot time
+  void setSatAuto(bool, const std::string&, const std::string&);
 
   ///returns union or intersection of plot times from all pinfos
   void getCapabilitiesTime(std::vector<miutil::miTime>& progTimes,
@@ -166,30 +199,25 @@ public:
 				      int index=-1);
   bool isMosaic(const std::string &satellite, const std::string & file);
 
-  void cutImageRGBA(unsigned char *image, float cut, int *index);
-  int getFileName(std::string &);
-  int getFileName(const miutil::miTime&);
-
-  SatDialogInfo initDialog(void){ return Dialog;}
+  SatDialogInfo initDialog()
+    { return Dialog; }
   bool parseSetup();
 
   //  Sat * findSatdata(const std::string & filename);//search vsatdata
   void updateFiles();
-  void setSatAuto(bool,const std::string &, const std::string &);
 
-  void archiveMode( bool on ){useArchive=on; updateFiles();}
+  void archiveMode(bool on)
+    { useArchive = on; updateFiles(); }
 
   std::vector <std::string> vUffdaClass;
   std::vector <std::string> vUffdaClassTip;
   std::string uffdaMailAddress;
   bool uffdaEnabled;
 
-  bool fileListChanged;
+  const Prod_t& getProductsInfo() const;
 
-  // Radar echo mesure
-  std::map<float,float> radarecho;
-
-  std::map<std::string, std::map<std::string,subProdInfo> > getProductsInfo() const;
+  const std::vector<SatPlot*>& getSatellitePlots() const
+    { return vsp; }
 };
 
 #endif

commit cdf01b70cba4f4ece5790c618b6e678de6eb3444

remove broken bdiana SAT describe
 * broken by commit 7b325768 (2014-07-29): these changes make bdiana
   search for subtype_name with the image_name key

src/bdiana_capi.cc
@@ -46,10 +46,7 @@
 #include <diController.h>
 #include <diFieldPlot.h>
 #include <diObsManager.h>
-#include <diObsPlot.h>
 #include <diPlotModule.h>
-#include <diSatManager.h>
-#include <diSatPlot.h>
 
 #include "diLocalSetupParser.h"
 #include "diPlotCommandFactory.h"
@@ -1421,55 +1418,7 @@ command_result Bdiana::handleDescribeCommand(int& k)
   //Find ENDDESCRIBE
   std::vector<std::string> pcom = FIND_END_COMMAND(k, com_describe_end);
 
-  if (not main.MAKE_CONTROLLER())
-    return cmd_abort;
-
-  main.commands(pcom);
-
-  if (!set_ptime(main))
-    return cmd_fail;
-
-  if (verbose)
-    METLIBS_LOG_INFO("- updatePlots");
-
-  if (main.controller->updatePlots() || !failOnMissingData) {
-
-    if (verbose)
-      METLIBS_LOG_INFO("- opening file '" << outputfilename << "'");
-
-    // open filestream
-    ofstream file(outputfilename.c_str());
-    if (!file) {
-      METLIBS_LOG_ERROR("ERROR OPEN (WRITE) '" << outputfilename << "'");
-      return cmd_abort;
-    }
-
-    const SatManager::Prod_t& satProducts = main.controller->getSatelliteManager()->getProductsInfo();
-    set<std::string> satPatterns;
-    set<std::string> satFiles;
-
-    for (SatPlot* sp : main.controller->getSatellitePlots()) {
-      Sat* sat = sp->satdata;
-      const SatManager::Prod_t::const_iterator itp = satProducts.find(sat->satellite);
-      if (itp != satProducts.end()) {
-        const SatManager::SubProd_t::const_iterator itsp = itp->second.find(sat->satellite);
-        if (itsp != itp->second.end()) {
-          const SatManager::subProdInfo& satInfo = itsp->second;
-          for (const SatFileInfo& sfi : satInfo.file) {
-            satFiles.insert(sfi.name);
-            if (sfi.name == sat->actualfile)
-              satPatterns.insert(satInfo.pattern.begin(), satInfo.pattern.end());
-          }
-        }
-      }
-    }
-
-    file << "FILES" << endl;
-    for (const std::string& p : satFiles)
-      file << p << endl;
-    for (const std::string& p : satPatterns)
-      file << p << endl;
-  }
+  METLIBS_LOG_WARN("- describe is disabled");
   return cmd_abort;
 }
 

src/diController.cc
@@ -725,11 +725,6 @@ SatPlotCluster* Controller::getSatPlotCluster() const
   return plotm->satplots();
 }
 
-vector<SatPlot*> Controller::getSatellitePlots() const
-{
-  return plotm->satplots()->getSatellitePlots();
-}
-
 std::vector<ObsPlot*> Controller::getObsPlots() const
 {
   return plotm->obsplots()->getObsPlots();

src/diController.h
@@ -280,7 +280,6 @@ public:
 
   // Miscellaneous get methods
   SatPlotCluster* getSatPlotCluster() const;
-  std::vector<SatPlot*> getSatellitePlots() const;   // Returns a vector of defined satellite plots.
   std::vector<ObsPlot*> getObsPlots() const;         // Returns a vector of defined observation plots.
 
 Q_SIGNALS:

src/diSatManager.cc
@@ -1144,8 +1144,3 @@ void SatManager::init_rgbindex_Meteosat(Sat& sd)
     inFile.close();
   }
 }
-
-const std::map<std::string, std::map<std::string,SatManager::subProdInfo> >& SatManager::getProductsInfo() const
-{
-  return Prod;
-}

src/diSatManager.h
@@ -132,8 +132,6 @@ public:
   bool parseSetup();
 
   void archiveMode(bool on) { useArchive = on; }
-
-  const Prod_t& getProductsInfo() const;
 };
 
 #endif

src/diSatPlotCluster.cc
@@ -258,11 +258,3 @@ plottimes_t SatPlotCluster::getSatTimes()
 
   return timeset;
 }
-
-std::vector<SatPlot*> SatPlotCluster::getSatellitePlots() const
-{
-  std::vector<SatPlot*> satplots;
-  satplots.reserve(plots_.size());
-  std::transform(plots_.begin(), plots_.end(), std::back_inserter(satplots), [](Plot* p) { return static_cast<SatPlot*>(p); });
-  return satplots;
-}

src/diSatPlotCluster.h
@@ -57,8 +57,6 @@ public:
 
   void getDataAnnotations(std::vector<std::string>& anno);
 
-  std::vector<SatPlot*> getSatellitePlots() const;
-
   bool getGridResolution(float& rx, float& ry) const;
 
   bool getSatArea(Area& a) const;