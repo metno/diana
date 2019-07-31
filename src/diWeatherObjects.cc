metno/diana

commit 70fd7648340da9afeb728ae8607a996e765b941c

add Plot::changeProjection and changeTime

src/diWeatherObjects.cc
@@ -26,12 +26,9 @@
   along with Diana; if not, write to the Free Software
   Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  */
-//#define DEBUGPRINT
-
-#include "diana_config.h"
 
 #include "diWeatherObjects.h"
-#include "diDrawingTypes.h"
+
 #include "diWeatherFront.h"
 #include "diWeatherSymbol.h"
 #include "diWeatherArea.h"
@@ -113,6 +110,9 @@ void WeatherObjects::plot(DiGLPainter* gl, PlotOrder porder)
   if (!enabled || (porder != PO_LINES && porder != PO_OVERLAY))
     return;
 
+  if (mapArea.P() != itsArea.P())
+    switchProjection(mapArea);
+
   const objectType order[] = {wArea, wFront, wSymbol, wText, Border, RegionName, ShapeXXX};
   for (int ord : order) {
     for (ObjectPlot* op : objects) {
@@ -124,7 +124,12 @@ void WeatherObjects::plot(DiGLPainter* gl, PlotOrder porder)
 
 /*********************************************/
 
-bool WeatherObjects::changeProjection(const Area& newArea)
+void WeatherObjects::changeProjection(const Area& newArea, const Rectangle& /*plotSize*/)
+{
+  mapArea = newArea;
+}
+
+bool WeatherObjects::switchProjection(const Area& newArea)
 {
   METLIBS_LOG_SCOPE("Change projection from " << itsArea <<" to " << newArea);
 
@@ -201,23 +206,20 @@ void WeatherObjects::updateObjects()
 
 /*********************************************/
 
-bool WeatherObjects::readEditDrawFile(const std::string& fn, const Area& newArea)
+bool WeatherObjects::readEditDrawFile(const std::string& fn)
 {
   METLIBS_LOG_SCOPE("filename" << fn);
 
   //if *.shp read shapefile
   if (diutil::endswith(fn, ".shp")) {
-    if (newArea.P()!=itsArea.P()){
-      changeProjection(newArea);
-    }
-    changeProjection(geoArea);
+    // shape file expected to be in geographical projection; convert all other objects to this projection
+    // such that all objects have the same projection after adding the shape file
+    switchProjection(geoArea);
 
     METLIBS_LOG_INFO("This is a shapefile");
     ShapeObject * shape = new ShapeObject();
     addObject(shape);
     shape->read(fn);
-
-    changeProjection(newArea);
 
     return true;
   }
@@ -274,11 +276,10 @@ bool WeatherObjects::readEditDrawFile(const std::string& fn, const Area& newArea
     }
   }
   file.close();
-  return readEditDrawString(fileString, newArea);
+  return readEditDrawString(fileString);
 }
 
-bool WeatherObjects::readEditDrawString(const std::string& inputString,
-    const Area& newArea, bool replace)
+bool WeatherObjects::readEditDrawString(const std::string& inputString, bool replace)
 {
   METLIBS_LOG_SCOPE(LOGVAL(inputString));
 
@@ -287,10 +288,7 @@ bool WeatherObjects::readEditDrawString(const std::string& inputString,
   // also in DisplayObjects::define and in EditManager::startEdit
 
   // first convert existing objects to geographic coordinates
-  if (newArea.P()!=itsArea.P()){
-    changeProjection(newArea);
-  }
-  changeProjection(geoArea);
+  switchProjection(geoArea);
 
   //split inputString into one string for each object
   const std::vector<std::string> objectStrings = miutil::split(inputString, 0, "!");
@@ -338,7 +336,6 @@ bool WeatherObjects::readEditDrawString(const std::string& inputString,
     }
   }
 
-  changeProjection(newArea);
   return true;
 }
 
@@ -350,7 +347,7 @@ std::string WeatherObjects::writeEditDrawString(const miTime& t)
     return std::string();
 
   const Area oldarea = itsArea;
-  changeProjection(geoArea);
+  switchProjection(geoArea);
 
   ostringstream ostr;
   ostr << "Date=" << miutil::stringFromTime(t, true) << ';' << endl << endl;
@@ -358,7 +355,7 @@ std::string WeatherObjects::writeEditDrawString(const miTime& t)
   for (vector <ObjectPlot*>::iterator p = objects.begin(); p!=objects.end(); ++p)
     ostr << (*p)->writeObjectString();
 
-  changeProjection(oldarea);
+  switchProjection(oldarea);
 
   for (PlotCommand_cp pc : itsLabels)
     ostr << pc->toString() << "\n";
@@ -425,7 +422,7 @@ const PlotCommand_cpv& WeatherObjects::getEditLabels()
  *  Methods for reading and writing areaBorders  *
  *************************************************/
 
-bool WeatherObjects::readAreaBorders(const std::string fn, const Area& newArea)
+bool WeatherObjects::readAreaBorders(const string& fn)
 {
   METLIBS_LOG_SCOPE("filename = " << fn);
 
@@ -442,7 +439,7 @@ bool WeatherObjects::readAreaBorders(const std::string fn, const Area& newArea)
 
   file.close();
 
-  return readEditDrawString(miutil::from_latin1_to_utf8(fileString),newArea);
+  return readEditDrawString(miutil::from_latin1_to_utf8(fileString));
 }
 
 
@@ -459,7 +456,7 @@ bool WeatherObjects::writeAreaBorders(const std::string& fn)
   }
 
   const Area oldarea = itsArea;
-  changeProjection(geoArea);
+  switchProjection(geoArea);
 
   diutil::CharsetConverter_p converter = diutil::findConverter(diutil::CHARSET_INTERNAL(), diutil::ISO_8859_1);
 
@@ -471,7 +468,7 @@ bool WeatherObjects::writeAreaBorders(const std::string& fn)
 
   file.close();
 
-  changeProjection(oldarea);
+  switchProjection(oldarea);
   return true;
 }
 

src/diAreaObjects.cc
@@ -1,7 +1,7 @@
 /*
   Diana - A Free Meteorological Visualisation Tool
 
-  Copyright (C) 2006-2018 met.no
+  Copyright (C) 2006-2019 met.no
 
   Contact information:
   Norwegian Meteorological Institute
@@ -51,8 +51,7 @@ AreaObjects::~AreaObjects()
 {
 }
 
-void AreaObjects::makeAreas(const std::string& name, const std::string& icon,
-    const std::string& areastring, int id,const Area& area)
+void AreaObjects::makeAreas(const std::string& name, const std::string& icon, const std::string& areastring, int id)
 {
   METLIBS_LOG_SCOPE();
   METLIBS_LOG_DEBUG(LOGVAL(name));
@@ -63,7 +62,7 @@ void AreaObjects::makeAreas(const std::string& name, const std::string& icon,
   itsName=name;
   iconName=icon;
 
-  if (readEditDrawString(areastring,area,true))
+  if (readEditDrawString(areastring, true))
     METLIBS_LOG_DEBUG("AreaObjects " << itsName << " id=" << itsId << " read OK!");
   else
     METLIBS_LOG_DEBUG("Areaobjects not read OK");

src/diAreaObjects.h
@@ -1,7 +1,7 @@
 /*
   Diana - A Free Meteorological Visualisation Tool
 
-  Copyright (C) 2006-2018 met.no
+  Copyright (C) 2006-2019 met.no
 
   Contact information:
   Norwegian Meteorological Institute
@@ -50,7 +50,7 @@ public:
   ~AreaObjects();
 
   /// assign name,icon and id, and read areastring to make WeatherArea objects
-  void makeAreas(const std::string& name, const std::string& icon, const std::string& areastring, int id, const Area&);
+  void makeAreas(const std::string& name, const std::string& icon, const std::string& areastring, int id);
   /// apply a command
   bool areaCommand(const std::string& command, const std::vector<std::string>& data);
 

src/diAreaObjectsCluster.cc
@@ -1,7 +1,7 @@
 /*
   Diana - A Free Meteorological Visualisation Tool
 
-  Copyright (C) 2017-2018 met.no
+  Copyright (C) 2017-2019 met.no
 
   Contact information:
   Norwegian Meteorological Institute
@@ -38,8 +38,7 @@
 #define MILOGGER_CATEGORY "diana.AreaObjectsCluster"
 #include <miLogger/miLogging.h>
 
-AreaObjectsCluster::AreaObjectsCluster(PlotModule* plot)
-  : plot_(plot)
+AreaObjectsCluster::AreaObjectsCluster()
 {
 }
 
@@ -49,10 +48,14 @@ AreaObjectsCluster::~AreaObjectsCluster()
 
 void AreaObjectsCluster::plot(DiGLPainter* gl, PlotOrder zorder)
 {
-  for (size_t i = 0; i < vareaobjects.size(); i++) {
-    vareaobjects[i].changeProjection(plot_->getMapArea()); // TODO move this somewhere else
-    vareaobjects[i].plot(gl, zorder);
-  }
+  for (AreaObjects& ao : vareaobjects)
+    ao.plot(gl, zorder);
+}
+
+void AreaObjectsCluster::changeProjection(const Area& mapArea, const Rectangle& plotSize)
+{
+  for (AreaObjects& ao : vareaobjects)
+    ao.changeProjection(mapArea, plotSize);
 }
 
 void AreaObjectsCluster::addPlotElements(std::vector<PlotElement>& pel)
@@ -107,7 +110,7 @@ void AreaObjectsCluster::makeAreaObjects(std::string name, std::string areastrin
   if (it == vareaobjects.end())
     // not found, add new at end
     it = vareaobjects.insert(it, AreaObjects());
-  it->makeAreas(name, icon, areastring, id, plot_->getMapArea());
+  it->makeAreas(name, icon, areastring, id);
 }
 
 void AreaObjectsCluster::areaObjectsCommand(const std::string& command, const std::string& dataSet,
@@ -128,4 +131,3 @@ void AreaObjectsCluster::areaObjectsCommand(const std::string& command, const st
     }
   }
 }
-

src/diAreaObjectsCluster.h
@@ -1,7 +1,7 @@
 /*
   Diana - A Free Meteorological Visualisation Tool
 
-  Copyright (C) 2017-2018 met.no
+  Copyright (C) 2017-2019 met.no
 
   Contact information:
   Norwegian Meteorological Institute
@@ -31,18 +31,16 @@
 #define AREAOBJECTSCLUSTER_H
 
 #include "diAreaObjects.h"
-#include "diPlot.h"
 
-class PlotModule;
 class PlotElement;
-class StaticPlot;
 
 class AreaObjectsCluster
 {
 public:
-  AreaObjectsCluster(PlotModule* pm);
+  AreaObjectsCluster();
   ~AreaObjectsCluster();
 
+  void changeProjection(const Area& mapArea, const Rectangle& plotSize);
   void plot(DiGLPainter* gl, PlotOrder zorder);
 
   void addPlotElements(std::vector<PlotElement>& pel);
@@ -57,8 +55,6 @@ public:
       const std::vector<std::string>& data, int id);
 
 private:
-  PlotModule* plot_;
-
   typedef std::vector<AreaObjects> areaobjects_v;
   areaobjects_v vareaobjects;  //QED areas
 };

src/diDrawingManager.cc
@@ -1,7 +1,7 @@
 /*
   Diana - A Free Meteorological Visualisation Tool
 
-  Copyright (C) 2013-2018 met.no
+  Copyright (C) 2013-2019 met.no
 
   Contact information:
   Norwegian Meteorological Institute
@@ -445,7 +445,7 @@ plottimes_t DrawingManager::getTimes() const
  * Prepares the manager for display of, and interaction with, items that
  * correspond to the given \a time.
 */
-bool DrawingManager::prepare(const miutil::miTime &time)
+bool DrawingManager::changeTime(const miutil::miTime& time)
 {
   bool found = false;
 
@@ -496,12 +496,11 @@ bool DrawingManager::prepare(const miutil::miTime &time)
   return found;
 }
 
-bool DrawingManager::changeProjection(const Area& newArea)
+bool DrawingManager::changeProjection(const Area& /*mapArea*/, const Rectangle& plotSize)
 {
   // Record the new plot rectangle and area.
   // Update the edit rectangle so that objects are positioned consistently.
-  const Rectangle& r = PLOTM->getPlotSize();
-  setEditRect(r);
+  setEditRect(plotSize);
   return true;
 }
 

src/diDrawingManager.h
@@ -1,7 +1,7 @@
 /*
   Diana - A Free Meteorological Visualisation Tool
 
-  Copyright (C) 2013-2018 met.no
+  Copyright (C) 2013-2019 met.no
 
   Contact information:
   Norwegian Meteorological Institute
@@ -67,9 +67,9 @@ public:
   // parse DRAWING section of setup file (defines Drawing products)
   virtual bool parseSetup() override;
 
-  virtual bool changeProjection(const Area& newArea) override;
+  virtual bool changeProjection(const Area& mapArea, const Rectangle& plotSize) override;
   virtual QString loadDrawing(const QString &name, const QString &fileName);
-  virtual bool prepare(const miutil::miTime &time) override;
+  virtual bool changeTime(const miutil::miTime& time) override;
   virtual void setCanvas(DiCanvas* canvas) override;
   virtual void plot(DiGLPainter* gl, bool under, bool over) override;
   virtual bool processInput(const PlotCommand_cpv& inp) override;

src/diEditItemManager.cc
@@ -1,7 +1,7 @@
 /*
   Diana - A Free Meteorological Visualisation Tool
 
-  Copyright (C) 2013-2018 met.no
+  Copyright (C) 2013-2019 met.no
 
   Contact information:
   Norwegian Meteorological Institute
@@ -949,7 +949,7 @@ void EditItemManager::updateTimes()
 {
   // Update the visibility of items based on the current plot time.
   const miutil::miTime& time = PLOTM->getPlotTime();
-  prepare(time);
+  changeTime(time);
 }
 
 void EditItemManager::updateActionsAndTimes()

commit c8cdc1943c01e6c29406c689b9bec078b768f596

bugfix: initialize WeatherObjects::itsArea
 * regression in commit 70fd7648

src/diWeatherObjects.cc
@@ -65,6 +65,7 @@ WeatherObjects::WeatherObjects()
 
   // correct spec. when making Projection for long/lat coordinates
   geoArea.setP(Projection::geographic());
+  itsArea = geoArea;
 
   //use all objects if nothing else specified
   for (const std::string& otn : ObjectTypeNames)