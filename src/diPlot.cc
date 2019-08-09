metno/diana

commit a8781d5eec4891bd196a58d2929ba83a0942d3b8

add StationPlotCluster
 * contains previous PlotModule::prepareStations
 * contains previous StationManager::stationPlots

src/diPlot.cc
@@ -93,3 +93,8 @@ void Plot::getAnnotation(std::string& s, Colour& c) const
   c = Colour("black");
   s = getPlotName();
 }
+
+const std::string& Plot::getPlotName() const
+{
+  return plotname;
+}

src/diPlot.h
@@ -85,8 +85,7 @@ public:
     { plotname= name; }
 
   /// return name of this plot object
-  const std::string& getPlotName() const
-    { return plotname; }
+  virtual const std::string& getPlotName() const;
 
   virtual void getAnnotation(std::string &str, Colour &col) const;
 

src/diPlotCluster.cc
@@ -113,9 +113,15 @@ void PlotCluster::addPlotElements(std::vector<PlotElement>& pel)
   for (size_t j = 0; j < plots_.size(); j++) {
     const std::string& nm = plots_[j]->getPlotName();
     if (!nm.empty()) {
+#if 1
+      const std::string& icon = key;
+#else
+      const std::string& icon_ = sp->getIcon();
+      const std::string& icon = icon_.empty() ? key : icon_;
+#endif
       std::string str = nm + "# " + miutil::from_number(int(j));
       bool enabled = plots_[j]->isEnabled();
-      pel.push_back(PlotElement(key, str, key, enabled));
+      pel.push_back(PlotElement(key, str, icon, enabled));
     }
   }
 }

src/diStationPlot.cc
@@ -442,9 +442,23 @@ void StationPlot::unselect()
   index = -1;
 }
 
-bool StationPlot::isVisible()
+bool StationPlot::isVisible() const
 {
   return visible;
+}
+
+void StationPlot::setVisible(bool on)
+{
+  if (on)
+    show();
+  else
+    hide();
+}
+
+const std::string& StationPlot::getPlotName() const
+{
+  static const std::string EMPTY;
+  return isVisible() ? Plot::getPlotName() : EMPTY;
 }
 
 void StationPlot::defineCoordinates()

src/diStationPlot.h
@@ -172,7 +172,11 @@ public:
   /// unselects all stations
   void unselect();
   /// returns true if stationplot visible
-  bool isVisible();
+  bool isVisible() const;
+  void setVisible(bool on);
+
+  const std::string& getPlotName() const override;
+
   /// change stationplot projection
   void changeProjection(const Area& mapArea, const Rectangle& plotSize) override;
 

src/CMakeLists.txt
@@ -175,6 +175,7 @@ SET (LIBDIANA_SOURCES
   diStaticPlot.cc
   diStationManager.cc
   diStationPlot.cc
+  diStationPlotCluster.cc
   diStationPlotCommand.cc
   diStringPlotCommand.cc
   diTrajectoryGenerator.cc

src/diController.cc
@@ -1,7 +1,7 @@
 /*
   Diana - A Free Meteorological Visualisation Tool
 
-  Copyright (C) 2006-2018 met.no
+  Copyright (C) 2006-2019 met.no
 
   Contact information:
   Norwegian Meteorological Institute
@@ -52,6 +52,7 @@
 #include "diSatPlotCluster.h"
 #include "diStationManager.h"
 #include "diStationPlot.h"
+#include "diStationPlotCluster.h"
 #include "miSetupParser.h"
 #include "wmsclient/WebMapManager.h"
 
@@ -595,9 +596,14 @@ plottimes_t Controller::getObsTimes(const vector<string>& name, bool update)
 
 //********** plotting and selecting stations on the map***************
 
+QString Controller::getStationsText(int x, int y)
+{
+  return plotm->stationplots()->getStationsText(x, y);
+}
+
 void Controller::putStations(StationPlot* stationPlot)
 {
-  stam->putStations(stationPlot);
+  plotm->stationplots()->putStations(stationPlot);
   plotm->setAnnotations();
 }
 
@@ -607,17 +613,17 @@ void Controller::makeStationPlot(const string& commondesc,
     int from,
     const  vector<string>& data)
 {
-  stam->makeStationPlot(commondesc,common,description,from,data);
+  plotm->stationplots()->makeStationPlot(commondesc, common, description, from, data);
 }
 
 std::string Controller::findStation(int x, int y, const std::string& name, int id)
 {
-  return stam->findStation(x,y,name,id);
+  return plotm->stationplots()->findStation(x, y, name, id);
 }
 
 std::vector<std::string> Controller::findStations(int x, int y, const std::string& name, int id)
 {
-  return stam->findStations(x,y,name,id);
+  return plotm->stationplots()->findStations(x, y, name, id);
 }
 
 void Controller::findStations(int x, int y, bool add,
@@ -625,7 +631,7 @@ void Controller::findStations(int x, int y, bool add,
     vector<int>& id,
     vector<std::string>& station)
 {
-  stam->findStations(x,y,add,name,id,station);
+  plotm->stationplots()->findStations(x, y, add, name, id, station);
 }
 
 void Controller::stationCommand(const string& command,
@@ -633,7 +639,7 @@ void Controller::stationCommand(const string& command,
     const string& name, int id,
     const string& misc)
 {
-  stam->stationCommand(command,data,name,id,misc);
+  plotm->stationplots()->stationCommand(command, data, name, id, misc);
 
   if (command == "annotation")
     plotm->setAnnotations();
@@ -642,8 +648,7 @@ void Controller::stationCommand(const string& command,
 void Controller::stationCommand(const string& command,
     const string& name, int id)
 {
-  stam->stationCommand(command,name,id);
-
+  plotm->stationplots()->stationCommand(command, name, id);
   plotm->setAnnotations();
 }
 
@@ -723,6 +728,11 @@ void Controller::readLog(const vector<string>& vstr,
 SatPlotCluster* Controller::getSatPlotCluster() const
 {
   return plotm->satplots();
+}
+
+StationPlotCluster* Controller::getStationPlotCluster() const
+{
+  return plotm->stationplots();
 }
 
 std::vector<ObsPlot*> Controller::getObsPlots() const

src/diController.h
@@ -1,7 +1,7 @@
 /*
   Diana - A Free Meteorological Visualisation Tool
 
-  Copyright (C) 2006-2018 met.no
+  Copyright (C) 2006-2019 met.no
 
   Contact information:
   Norwegian Meteorological Institute
@@ -63,6 +63,7 @@ class SatManager;
 class SatPlot;
 class SatPlotCluster;
 class StationManager;
+class StationPlotCluster;
 class StationPlot;
 
 struct LocationData;
@@ -239,6 +240,7 @@ public:
   std::set<std::string> getComplexList();
 
   //stations
+  QString getStationsText(int x, int y);
   void putStations(StationPlot*);
   void makeStationPlot(const std::string& commondesc, const std::string& common,
       const std::string& description, int from,
@@ -280,6 +282,7 @@ public:
 
   // Miscellaneous get methods
   SatPlotCluster* getSatPlotCluster() const;
+  StationPlotCluster* getStationPlotCluster() const;
   std::vector<ObsPlot*> getObsPlots() const;         // Returns a vector of defined observation plots.
 
 Q_SIGNALS:

commit c839755aa51cda2c706459a0265a11f7490b4158

bugfix StationPlot icon
 * regression in commit a8781d5e

src/diPlot.cc
@@ -87,3 +87,9 @@ const std::string& Plot::getPlotName() const
 {
   return plotname;
 }
+
+const std::string& Plot::getIconName() const
+{
+  static const std::string EMPTY;
+  return EMPTY;
+}

src/diPlot.h
@@ -82,6 +82,9 @@ public:
   /// return name of this plot object
   virtual const std::string& getPlotName() const;
 
+  /// return name of this plot object
+  virtual const std::string& getIconName() const;
+
 private:
   bool enabled;               // plot enabled
   std::string plotname;       // name of plot

src/diPlotCluster.cc
@@ -118,17 +118,13 @@ void PlotCluster::addPlotElements(std::vector<PlotElement>& pel)
 {
   const std::string& key = keyPlotElement();
   for (size_t j = 0; j < plots_.size(); j++) {
-    const std::string& nm = plots_[j]->getPlotName();
+    const Plot* p = plots_[j];
+    const std::string& nm = p->getPlotName();
     if (!nm.empty()) {
-#if 1
-      const std::string& icon = key;
-#else
-      const std::string& icon_ = sp->getIcon();
+      const std::string& icon_ = p->getIconName();
       const std::string& icon = icon_.empty() ? key : icon_;
-#endif
       std::string str = nm + "# " + miutil::from_number(int(j));
-      bool enabled = plots_[j]->isEnabled();
-      pel.push_back(PlotElement(key, str, icon, enabled));
+      pel.push_back(PlotElement(key, str, icon, p->isEnabled()));
     }
   }
 }

src/diStationPlot.cc
@@ -466,6 +466,11 @@ const std::string& StationPlot::getPlotName() const
   return isVisible() ? Plot::getPlotName() : EMPTY;
 }
 
+const std::string& StationPlot::getIconName() const
+{
+  return iconName;
+}
+
 void StationPlot::defineCoordinates()
 {
   //should be called from constructor and when new stations have

src/diStationPlot.h
@@ -167,6 +167,7 @@ public:
   std::string getEnabledStateKey() const override;
 
   const std::string& getPlotName() const override;
+  const std::string& getIconName() const override;
 
   /// hide stations
   void hide();
@@ -227,8 +228,6 @@ public:
   void setUseStationName(bool normal, bool selected);
   void setIcon(const std::string& icon)
     { iconName = icon; }
-  const std::string& getIcon() const
-    { return iconName; }
   bool stationCommand(const std::string& Command,
       const std::vector<std::string>& data,
       const std::string& misc="");