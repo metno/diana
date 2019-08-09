metno/diana

commit a8781d5eec4891bd196a58d2929ba83a0942d3b8

add StationPlotCluster
 * contains previous PlotModule::prepareStations
 * contains previous StationManager::stationPlots

src/diStationPlotCluster.cc
@@ -1,0 +1,320 @@
+/*
+  Diana - A Free Meteorological Visualisation Tool
+
+  Copyright (C) 2019 met.no
+
+  Contact information:
+  Norwegian Meteorological Institute
+  Box 43 Blindern
+  0313 OSLO
+  NORWAY
+  email: diana@met.no
+
+  This file is part of Diana
+
+  Diana is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+
+  Diana is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with Diana; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+#include "diStationPlotCluster.h"
+
+#include "diStationManager.h"
+#include "diStationPlot.h"
+#include "diStationPlotCommand.h"
+#include "util/misc_util.h"
+
+#include <puTools/miStringFunctions.h>
+#include <puTools/miTime.h>
+
+#define MILOGGER_CATEGORY "diana.StationPlotCluster"
+#include <miLogger/miLogging.h>
+
+using namespace miutil;
+using namespace std;
+
+static const std::string STATION = "STATION";
+
+StationPlotCluster::StationPlotCluster(StationManager* stam)
+    : PlotCluster(STATION)
+    , stam_(stam)
+{
+}
+
+StationPlotCluster::~StationPlotCluster() {}
+
+StationPlotCluster::Plot_xv::iterator StationPlotCluster::findStationPlot(const std::string& name, int id, Plot_xv::iterator begin)
+{
+  for (; begin != plots_.end(); ++begin) {
+    StationPlot* sp = static_cast<StationPlot*>(*begin);
+    if (sp->getId() == id && sp->getName() == name)
+      break;
+  }
+  return begin;
+}
+
+/**
+ * Updates the vector of \a plots with the data provided in \a inp.
+ */
+void StationPlotCluster::processInputPE(const PlotCommand_cpv& inp)
+{
+  METLIBS_LOG_SCOPE();
+  // Hide all the station plots to begin with so that we can later
+  // show and select/unselect them.
+
+  for (StationPlot* sp : diutil::static_content_cast<StationPlot*>(plots_))
+    sp->setVisible(!sp->isVisible());
+
+  for (PlotCommand_cp pc : inp) {
+    StationPlotCommand_cp c = std::dynamic_pointer_cast<const StationPlotCommand>(pc);
+    if (!c)
+      continue;
+
+    Plot_xv::iterator it = findStationPlot(c->name, -1, plots_.begin());
+    StationPlot* plot = 0;
+
+    if (it == plots_.end()) {
+      // No existing plot exists. If the select part of the plot command
+      // contains the word "hidden" then we ignore this set of stations.
+
+      if (c->select != "hidden") {
+        // Load the stations.
+        plot = stam_->importStations(c->name, c->url);
+        if (plot)
+          putStations(plot);
+      }
+
+    } else {
+      // An existing plot containing a loaded list of stations.
+      plot = static_cast<StationPlot*>(*it);
+    }
+
+    if (plot) {
+      if (c->select == "hidden") {
+        plot->hide();
+        stam_->initDialog().chosen[c->url] = false;
+      } else {
+        plot->show();
+        stam_->initDialog().chosen[c->url] = true;
+      }
+
+      if (c->select == "selected") {
+        stam_->initDialog().selected = c->name;
+        for (Station* s : plot->getStations())
+          s->isSelected = true;
+
+      } else
+        plot->unselect();
+    }
+  }
+}
+
+//********** plotting and selecting stations on the map***************
+
+void StationPlotCluster::putStations(StationPlot* stationPlot)
+{
+  METLIBS_LOG_SCOPE();
+
+  Plot_xv::iterator p = findStationPlot(stationPlot->getName(), stationPlot->getId(), plots_.begin());
+  if (p != plots_.end()) {
+    StationPlot* old = static_cast<StationPlot*>(*p);
+    if (!old->isVisible())
+      stationPlot->hide();
+    stationPlot->setEnabled(old->isEnabled());
+    delete old;
+    *p = stationPlot;
+  } else {
+    add(stationPlot);
+  }
+}
+
+void StationPlotCluster::makeStationPlot(const std::string& commondesc, const std::string& common, const std::string& description, int from,
+                                         const vector<std::string>& data)
+{
+  putStations(new StationPlot(commondesc, common, description, from, data));
+}
+
+Station* StationPlotCluster::findStation(int x, int y)
+{
+  for (StationPlot* sp : diutil::static_content_cast<StationPlot*>(plots_)) {
+    Station* station = sp->stationAt(x, y);
+    if (station)
+      return station;
+  }
+  return 0;
+}
+
+vector<Station*> StationPlotCluster::findStations(int x, int y)
+{
+  vector<Station*> stations;
+  for (StationPlot* sp : diutil::static_content_cast<StationPlot*>(plots_)) {
+    if (sp->isVisible()) {
+      diutil::insert_all(stations, sp->stationsAt(x, y, 5));
+    }
+  }
+  return stations;
+}
+
+string StationPlotCluster::findStation(int x, int y, const std::string& name, int id)
+{
+  for (StationPlot* sp : diutil::static_content_cast<StationPlot*>(plots_)) {
+    if ((id == -1 || id == sp->getId()) && (name == sp->getName())) {
+      vector<std::string> st = sp->findStation(x, y);
+      if (st.size() > 0)
+        return st[0];
+    }
+  }
+  return std::string();
+}
+
+vector<string> StationPlotCluster::findStations(int x, int y, const std::string& name, int id)
+{
+  for (StationPlot* sp : diutil::static_content_cast<StationPlot*>(plots_)) {
+    if ((id == -1 || id == sp->getId()) && (name == sp->getName())) {
+      vector<std::string> st = sp->findStations(x, y);
+      if (st.size() > 0)
+        return st;
+    }
+  }
+  return vector<std::string>();
+}
+
+void StationPlotCluster::findStations(int x, int y, bool add, std::vector<std::string>& name, std::vector<int>& id, std::vector<std::string>& station)
+{
+  for (StationPlot* sp : diutil::static_content_cast<StationPlot*>(plots_)) {
+    const int ii = sp->getId();
+    if (ii > -1) {
+      const vector<std::string> st = sp->findStation(x, y, add);
+      for (size_t j = 0; j < st.size(); j++) {
+        name.push_back(sp->getName());
+        id.push_back(ii);
+        station.push_back(st[j]);
+      }
+    }
+  }
+}
+
+void StationPlotCluster::stationCommand(const std::string& command, const vector<std::string>& data, const std::string& name, int id, const std::string& misc)
+{
+  for (StationPlot* sp : diutil::static_content_cast<StationPlot*>(plots_)) {
+    if ((id == -1 || id == sp->getId()) && (name == sp->getName() || name.empty())) {
+      sp->stationCommand(command, data, misc);
+      break;
+    }
+  }
+}
+
+void StationPlotCluster::stationCommand(const std::string& command, const std::string& name, int id)
+{
+  if (command == "delete") {
+    // name == "all" && id == -1 => coserver disconnect => delete all plots with id >= 0
+    // name == "" && id >= 0 => client disconnect => delete all plots with getId() == id
+    for (Plot_xv::iterator it = plots_.begin(); it != plots_.end(); /*nothing*/) {
+      StationPlot* sp = static_cast<StationPlot*>(*it);
+      if ((name == "all" && sp->getId() != -1) || (id == sp->getId() && (name == sp->getName() || name.empty()))) {
+        delete *it;
+        it = plots_.erase(it);
+      } else {
+        it++;
+      }
+    }
+
+  } else {
+    for (StationPlot* sp : diutil::static_content_cast<StationPlot*>(plots_)) {
+      if ((id == -1 || id == sp->getId()) && (name == sp->getName() || name.empty()))
+        sp->stationCommand(command);
+    }
+  }
+}
+
+QString StationPlotCluster::getStationsText(int x, int y)
+{
+  QString stationsText;
+  vector<Station*> allStations = findStations(x, y);
+  vector<Station*> stations;
+  for (unsigned int i = 0; i < allStations.size(); ++i) {
+    if (allStations[i]->status != Station::noStatus)
+      stations.push_back(allStations[i]);
+  }
+
+  if (stations.size() > 0) {
+
+    // Count the number of times each station name appears in the list.
+    // This is used later to decide whether or not to show the "auto" or
+    // "vis" text.
+    map<std::string, unsigned int> stationNames;
+    for (unsigned int i = 0; i < stations.size(); ++i) {
+      unsigned int number = stationNames.count(stations[i]->name);
+      stationNames[stations[i]->name] = number + 1; // FIXME this is always == 2
+    }
+
+    stationsText = "<table>";
+    for (unsigned int i = 0; i < stations.size(); ++i) {
+      if (!stations[i]->isVisible)
+        continue;
+
+      stationsText += "<tr>";
+      stationsText += "<td>";
+      switch (stations[i]->status) {
+      case Station::failed:
+        stationsText += "<span style=\"background: red; color: red\">X</span>";
+        break;
+      case Station::underRepair:
+        stationsText += "<span style=\"background: yellow; color: yellow\">X</span>";
+        break;
+      case Station::working:
+        stationsText += "<span style=\"background: lightgreen; color: lightgreen\">X</span>";
+        break;
+      default:;
+      }
+      stationsText += "</td>";
+
+      stationsText += "<td>";
+      stationsText += QString("<a href=\"%1\">%2</a>").arg(QString::fromStdString(stations[i]->url)).arg(QString::fromStdString(stations[i]->name));
+      if (stationNames[stations[i]->name] > 1) {
+        if (stations[i]->type == Station::automatic)
+          stationsText += "&nbsp;auto";
+        else
+          stationsText += "&nbsp;vis";
+      }
+      stationsText += "</td>";
+
+      stationsText += "<td>";
+      const float lat = stations[i]->lat(), lon = stations[i]->lon();
+      if (lat >= 0)
+        stationsText += QString("%1&nbsp;N,&nbsp;").arg(lat);
+      else
+        stationsText += QString("%1&nbsp;S,&nbsp;").arg(-lat);
+      if (lon >= 0)
+        stationsText += QString("%1&nbsp;E").arg(lon);
+      else
+        stationsText += QString("%1&nbsp;W").arg(-lon);
+      stationsText += "</td>";
+
+      stationsText += "<td>";
+      stationsText += QString("%1&nbsp;m").arg(stations[i]->height);
+      stationsText += "</td>";
+
+      stationsText += "</tr>";
+    }
+    stationsText += "</table>";
+  }
+
+  return stationsText;
+}
+
+std::vector<StationPlot*> StationPlotCluster::getStationPlots() const
+{
+  const auto sps = diutil::static_content_cast<StationPlot*>(plots_);
+  return std::vector<StationPlot*>(sps.begin(), sps.end());
+}

src/CMakeLists.txt
@@ -175,6 +175,7 @@ SET (LIBDIANA_SOURCES
   diStaticPlot.cc
   diStationManager.cc
   diStationPlot.cc
+  diStationPlotCluster.cc
   diStationPlotCommand.cc
   diStringPlotCommand.cc
   diTrajectoryGenerator.cc

src/diController.cc
@@ -1,7 +1,7 @@
 /*
   Diana - A Free Meteorological Visualisation Tool
 
-  Copyright (C) 2006-2018 met.no
+  Copyright (C) 2006-2019 met.no
 
   Contact information:
   Norwegian Meteorological Institute
@@ -52,6 +52,7 @@
 #include "diSatPlotCluster.h"
 #include "diStationManager.h"
 #include "diStationPlot.h"
+#include "diStationPlotCluster.h"
 #include "miSetupParser.h"
 #include "wmsclient/WebMapManager.h"
 
@@ -595,9 +596,14 @@ plottimes_t Controller::getObsTimes(const vector<string>& name, bool update)
 
 //********** plotting and selecting stations on the map***************
 
+QString Controller::getStationsText(int x, int y)
+{
+  return plotm->stationplots()->getStationsText(x, y);
+}
+
 void Controller::putStations(StationPlot* stationPlot)
 {
-  stam->putStations(stationPlot);
+  plotm->stationplots()->putStations(stationPlot);
   plotm->setAnnotations();
 }
 
@@ -607,17 +613,17 @@ void Controller::makeStationPlot(const string& commondesc,
     int from,
     const  vector<string>& data)
 {
-  stam->makeStationPlot(commondesc,common,description,from,data);
+  plotm->stationplots()->makeStationPlot(commondesc, common, description, from, data);
 }
 
 std::string Controller::findStation(int x, int y, const std::string& name, int id)
 {
-  return stam->findStation(x,y,name,id);
+  return plotm->stationplots()->findStation(x, y, name, id);
 }
 
 std::vector<std::string> Controller::findStations(int x, int y, const std::string& name, int id)
 {
-  return stam->findStations(x,y,name,id);
+  return plotm->stationplots()->findStations(x, y, name, id);
 }
 
 void Controller::findStations(int x, int y, bool add,
@@ -625,7 +631,7 @@ void Controller::findStations(int x, int y, bool add,
     vector<int>& id,
     vector<std::string>& station)
 {
-  stam->findStations(x,y,add,name,id,station);
+  plotm->stationplots()->findStations(x, y, add, name, id, station);
 }
 
 void Controller::stationCommand(const string& command,
@@ -633,7 +639,7 @@ void Controller::stationCommand(const string& command,
     const string& name, int id,
     const string& misc)
 {
-  stam->stationCommand(command,data,name,id,misc);
+  plotm->stationplots()->stationCommand(command, data, name, id, misc);
 
   if (command == "annotation")
     plotm->setAnnotations();
@@ -642,8 +648,7 @@ void Controller::stationCommand(const string& command,
 void Controller::stationCommand(const string& command,
     const string& name, int id)
 {
-  stam->stationCommand(command,name,id);
-
+  plotm->stationplots()->stationCommand(command, name, id);
   plotm->setAnnotations();
 }
 
@@ -723,6 +728,11 @@ void Controller::readLog(const vector<string>& vstr,
 SatPlotCluster* Controller::getSatPlotCluster() const
 {
   return plotm->satplots();
+}
+
+StationPlotCluster* Controller::getStationPlotCluster() const
+{
+  return plotm->stationplots();
 }
 
 std::vector<ObsPlot*> Controller::getObsPlots() const

src/diController.h
@@ -1,7 +1,7 @@
 /*
   Diana - A Free Meteorological Visualisation Tool
 
-  Copyright (C) 2006-2018 met.no
+  Copyright (C) 2006-2019 met.no
 
   Contact information:
   Norwegian Meteorological Institute
@@ -63,6 +63,7 @@ class SatManager;
 class SatPlot;
 class SatPlotCluster;
 class StationManager;
+class StationPlotCluster;
 class StationPlot;
 
 struct LocationData;
@@ -239,6 +240,7 @@ public:
   std::set<std::string> getComplexList();
 
   //stations
+  QString getStationsText(int x, int y);
   void putStations(StationPlot*);
   void makeStationPlot(const std::string& commondesc, const std::string& common,
       const std::string& description, int from,
@@ -280,6 +282,7 @@ public:
 
   // Miscellaneous get methods
   SatPlotCluster* getSatPlotCluster() const;
+  StationPlotCluster* getStationPlotCluster() const;
   std::vector<ObsPlot*> getObsPlots() const;         // Returns a vector of defined observation plots.
 
 Q_SIGNALS:

src/diPlot.cc
@@ -93,3 +93,8 @@ void Plot::getAnnotation(std::string& s, Colour& c) const
   c = Colour("black");
   s = getPlotName();
 }
+
+const std::string& Plot::getPlotName() const
+{
+  return plotname;
+}

src/diPlot.h
@@ -85,8 +85,7 @@ public:
     { plotname= name; }
 
   /// return name of this plot object
-  const std::string& getPlotName() const
-    { return plotname; }
+  virtual const std::string& getPlotName() const;
 
   virtual void getAnnotation(std::string &str, Colour &col) const;
 

src/diPlotCluster.cc
@@ -113,9 +113,15 @@ void PlotCluster::addPlotElements(std::vector<PlotElement>& pel)
   for (size_t j = 0; j < plots_.size(); j++) {
     const std::string& nm = plots_[j]->getPlotName();
     if (!nm.empty()) {
+#if 1
+      const std::string& icon = key;
+#else
+      const std::string& icon_ = sp->getIcon();
+      const std::string& icon = icon_.empty() ? key : icon_;
+#endif
       std::string str = nm + "# " + miutil::from_number(int(j));
       bool enabled = plots_[j]->isEnabled();
-      pel.push_back(PlotElement(key, str, key, enabled));
+      pel.push_back(PlotElement(key, str, icon, enabled));
     }
   }
 }

src/diPlotModule.cc
@@ -49,6 +49,7 @@
 #include "diStaticPlot.h"
 #include "diStationManager.h"
 #include "diStationPlot.h"
+#include "diStationPlotCluster.h"
 #include "diTrajectoryGenerator.h"
 #include "diTrajectoryPlot.h"
 #include "diUtilities.h"
@@ -116,6 +117,7 @@ void PlotModule::setCanvas(DiCanvas* canvas)
   mCanvas = canvas;
   mapplots_->setCanvas(canvas);
   obsplots_->setCanvas(mCanvas);
+  stationplots_->setCanvas(mCanvas);
   fieldplots_->setCanvas(mCanvas);
   for (Manager* m : boost::adaptors::values(managers))
     m->setCanvas(canvas);
@@ -134,7 +136,7 @@ void PlotModule::preparePlots(const PlotCommand_cpv& vpi)
   ordered.insert(mapplots_->plotCommandKey());
   ordered.insert("AREA");
   ordered.insert(satplots()->plotCommandKey());
-  ordered.insert("STATION");
+  ordered.insert(stationplots_->plotCommandKey());
   ordered.insert("OBJECTS");
   ordered.insert("TRAJECTORY");
   ordered.insert("LABEL");
@@ -162,7 +164,7 @@ void PlotModule::preparePlots(const PlotCommand_cpv& vpi)
   fieldplots_->processInput(ordered_pi[fieldplots()->plotCommandKey()]);
   obsplots_->processInput(ordered_pi[obsplots()->plotCommandKey()]);
   satplots_->processInput(ordered_pi[satplots()->plotCommandKey()]);
-  prepareStations(ordered_pi["STATION"]);
+  stationplots_->processInput(ordered_pi[stationplots_->plotCommandKey()]);
   objm->prepareObjects(ordered_pi["OBJECTS"]);
   prepareTrajectory(ordered_pi["TRAJECTORY"]);
   prepareAnnotation(ordered_pi["LABEL"]);
@@ -230,15 +232,6 @@ void PlotModule::prepareArea(const PlotCommand_cpv& inp)
   staticPlot_->setRequestedarea(requestedarea);
 }
 
-void PlotModule::prepareStations(const PlotCommand_cpv& inp)
-{
-  METLIBS_LOG_SCOPE();
-
-  if (!stam->init(inp)) {
-    METLIBS_LOG_DEBUG("init returned false");
-  }
-}
-
 void PlotModule::prepareAnnotation(const PlotCommand_cpv& inp)
 {
   METLIBS_LOG_SCOPE();
@@ -264,6 +257,7 @@ vector<PlotElement> PlotModule::getPlotElements()
   obsplots_->addPlotElements(pel);
   satplots_->addPlotElements(pel);
   objm->addPlotElements(pel);
+  stationplots_->addPlotElements(pel);
 
   // get trajectory names
   for (size_t j = 0; j < vtp.size(); j++) {
@@ -272,24 +266,6 @@ vector<PlotElement> PlotModule::getPlotElements()
       str += "# " + miutil::from_number(int(j));
       bool enabled = vtp[j]->isEnabled();
       pel.push_back(PlotElement("TRAJECTORY", str, "TRAJECTORY", enabled));
-    }
-  }
-
-  // get stationPlot names; vector is built on each call to stam->plots()
-  const std::vector<StationPlot*>& stam_plots = stam->plots();
-  for (size_t j = 0; j < stam_plots.size(); j++) {
-    StationPlot* sp = stam_plots[j];
-    if (!sp->isVisible())
-      continue;
-    std::string str = sp->getPlotName();
-    if (not str.empty()) {
-      str += "# " + miutil::from_number(int(j));
-      bool enabled = sp->isEnabled();
-      std::string icon = sp->getIcon();
-      if (icon.empty())
-        icon = "STATION";
-      std::string ptype = "STATION";
-      pel.push_back(PlotElement(ptype, str, icon, enabled));
     }
   }
 
@@ -324,16 +300,6 @@ void PlotModule::enablePlotElement(const PlotElement& pe)
         break;
       }
     }
-  } else if (pe.type == "STATION") {
-    const std::vector<StationPlot*>& stam_plots = stam->plots();
-    for (size_t i = 0; i < stam_plots.size(); i++) {
-      StationPlot* sp = stam_plots[i];
-      std::string str = sp->getPlotName() + "# " + miutil::from_number(int(i));
-      if (str == pe.str) {
-        plot = sp;
-        break;
-      }
-    }
   } else if (pe.type == "LOCATION") {
     for (unsigned int i = 0; i < locationPlots.size(); i++) {
       std::string str = locationPlots[i]->getPlotName() + "# " + miutil::from_number(int(i));
@@ -356,6 +322,8 @@ void PlotModule::enablePlotElement(const PlotElement& pe)
     change = obsplots_->enablePlotElement(pe);
   } else if (pe.type == satplots_->keyPlotElement()) {
     change = satplots_->enablePlotElement(pe);
+  } else if (pe.type == stationplots_->keyPlotElement()) {
+    change = stationplots_->enablePlotElement(pe);
   } else if (pe.type == "OBJECTS") {
     change = objm->enablePlotElement(pe);
   } else if (areaobjects_ && pe.type == areaobjects_->keyPlotElement()) {
@@ -407,6 +375,7 @@ void PlotModule::setAnnotations()
   }
 
   obsplots_->addAnnotations(annotations);
+  stationplots_->addAnnotations(annotations);
 
   // get trajectory annotations
   for (TrajectoryPlot* tp : vtp) {
@@ -416,14 +385,6 @@ void PlotModule::setAnnotations()
     // empty string if data plot is off
     if (!ann.str.empty())
       annotations.push_back(ann);
-  }
-
-  // get stationPlot annotations
-  for (StationPlot* sp : stam->plots()) {
-    if (!sp->isEnabled())
-      continue;
-    sp->getAnnotation(ann.str, ann.col);
-    annotations.push_back(ann);
   }
 
   // get locationPlot annotations
@@ -680,10 +641,7 @@ void PlotModule::plotUnder(DiGLPainter* gl)
   if (areaobjects_.get())
     areaobjects_->plot(gl, PO_LINES);
 
-  // plot station plots
-  const std::vector<StationPlot*>& stam_plots = stam->plots();
-  for (size_t j = 0; j < stam_plots.size(); j++)
-    stam_plots[j]->plot(gl, PO_LINES);
+  stationplots_->plot(gl, PO_LINES);
 
   // plot inactive edit fields/objects under observations
   editm->plot(gl, PO_LINES);
@@ -794,7 +752,7 @@ void PlotModule::cleanup()
   mapplots_->cleanup();
   fieldplots_->cleanup();
   satplots_->cleanup();
-  stam->cleanup();
+  stationplots_->cleanup();
   obsplots_->cleanup();
 
   objm->clearObjects();
@@ -828,8 +786,7 @@ void PlotModule::notifyChangeProjection()
   objm->changeProjection(ma, ps);
   if (areaobjects_)
     areaobjects_->changeProjection(ma, ps);
-  for (StationPlot* stp : stam->plots())
-    stp->changeProjection(ma, ps);
+  stationplots_->changeProjection(ma, ps);
   for (TrajectoryPlot* tp : vtp)
     tp->changeProjection(ma, ps);
   for (MeasurementsPlot* mp : vMeasurementsPlot)
@@ -999,6 +956,7 @@ void PlotModule::setManagers(FieldPlotManager* fpm, ObsManager* om, SatManager* 
   mapplots_.reset(new MapPlotCluster());
   fieldplots_.reset(new FieldPlotCluster(fieldplotm));
   satplots_.reset(new SatPlotCluster(satm));
+  stationplots_.reset(new StationPlotCluster(stam));
 }
 
 Manager *PlotModule::getManager(const std::string &name)

commit 9cd40296539cfe3e54bf80739b9ad384bd85b81d

bugfix StationPlot visibility
 * regression in commit a8781d5e

src/diStationPlotCluster.cc
@@ -73,7 +73,7 @@ void StationPlotCluster::processInputPE(const PlotCommand_cpv& inp)
   // show and select/unselect them.
 
   for (StationPlot* sp : diutil::static_content_cast<StationPlot*>(plots_))
-    sp->setVisible(!sp->isVisible());
+    sp->unselect();
 
   for (PlotCommand_cp pc : inp) {
     StationPlotCommand_cp c = std::dynamic_pointer_cast<const StationPlotCommand>(pc);