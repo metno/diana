metno/diana

commit 0dcbfa97f4c0b12081af032f48f8e334768f3cba

ObjectsPlotCommand
 * not sure if ObjectManager::prepareObjects still sets objects.enable
   in the same way as before

src/qtObjectDialog.cc
@@ -30,7 +30,6 @@
 #include "qtObjectDialog.h"
 
 #include "diController.h"
-#include "diKVListPlotCommand.h"
 #include "diObjectManager.h"
 #include "qtEditComment.h"
 #include "qtToggleButton.h"
@@ -438,48 +437,42 @@ PlotCommand_cpv ObjectDialog::getOKString()
   PlotCommand_cpv vstr;
 
   if (selectedFileList->count()) {
+    ObjectsPlotCommand_p cmd = std::make_shared<ObjectsPlotCommand>();
+
     miutil::KeyValue_v kvs;
     int index = namebox->currentRow();
     int timefileListIndex = timefileList->currentRow();
     if (index>-1 && index<int(objectnames.size())){
       //item has been selected in dialog
-      add(kvs, "name", objectnames[index]);
+      cmd->objectname = objectnames[index];
 
       if (timefileListIndex>-1 && timefileListIndex<int(files.size())) {
         const ObjFileInfo& file = files[timefileListIndex];
         if (timeButton->isChecked()){
           const miutil::miTime& time=file.time;
           if (!time.undef())
-            add(kvs, "time", miutil::stringFromTime(time, true));
+            cmd->time = time;
         } else if (fileButton->isChecked()) {
           if (!file.name.empty())
-            add(kvs, "file", file.name);
+            cmd->file = file.name;
         }
       }
     }
 
-    std::vector<std::string> types;
     if (cbs0->isChecked())
-      types.push_back("front");
+      cmd->objecttypes.push_back("front");
     if (cbs1->isChecked())
-      types.push_back("symbol");
+      cmd->objecttypes.push_back("symbol");
     if (cbs2->isChecked())
-      types.push_back("area");
+      cmd->objecttypes.push_back("area");
     if (cbs3->isChecked())
-      types.push_back("anno");
-    add(kvs, "types", types);
+      cmd->objecttypes.push_back("anno");
 
-    add(kvs, "timediff", m_totalminutes);
+    cmd->timeDiff = m_totalminutes;
     if (alpha->isChecked())
-      add(kvs, "alpha", m_alphanr);
+      cmd->alpha = m_alphanr;
 
-    KVListPlotCommand_p cmd = std::make_shared<KVListPlotCommand>("OBJECTS");
-    cmd->add(kvs);
-    cmd->add(plotVariables.external);
     vstr.push_back(cmd);
-
-    // clear external variables
-    plotVariables.external.clear();
   }
 
   return vstr;
@@ -494,15 +487,9 @@ void ObjectDialog::putOKString(const PlotCommand_cpv& vstr)
   if (vstr.empty())
     return;
 
-  // loop through all PlotInfo's
-  for (PlotCommand_cp pc : vstr) {
-    KVListPlotCommand_cp c = std::dynamic_pointer_cast<const KVListPlotCommand>(pc);
-    if (!c)
-      continue;
-    //(if there are several plotInfos, only the last one will be used
-    plotVariables = decodeString(c->all());
-  }
-
+  // loop through all PlotInfo's (if there are several plotInfos, only the last one will be used)
+  if (ObjectsPlotCommand_cp c = std::dynamic_pointer_cast<const ObjectsPlotCommand>(vstr.back()))
+    plotVariables = decodeString(c);
 
   //update dialog
   bool found=false;
@@ -578,40 +565,18 @@ void ObjectDialog::putOKString(const PlotCommand_cpv& vstr)
 
 
 // static
-ObjectDialog::PlotVariables ObjectDialog::decodeString(const miutil::KeyValue_v& tokens)
+ObjectDialog::PlotVariables ObjectDialog::decodeString(ObjectsPlotCommand_cp cmd)
 {
   METLIBS_LOG_SCOPE();
 
   PlotVariables okVar;
-  okVar.totalminutes=-1;
-  okVar.alphanr=1.0;
-
-  //loop over OKstrings
-  for (const miutil::KeyValue& kv : tokens) {
-    if (kv.hasValue()) {
-      const std::string& key = kv.key();
-      const std::string& value = kv.value();
-      if (key == "name") {
-        okVar.objectname = diutil::quote_removed(value);
-      } else if (key == "types") {
-        okVar.useobject = WeatherObjects::decodeTypeString(value);
-      } else if (key == "time") {
-        okVar.time=value;
-      } else if (key == "file") {
-        okVar.file=value;
-      } else if (key == "timediff") {
-        okVar.totalminutes = kv.toInt();
-      } else if (key == "alpha" || key == "alfa") {
-        okVar.alphanr = kv.toFloat();
-      } else {
-        // unknown key: add to external string
-        okVar.external.push_back(kv);
-      }
-    } else {
-      // no value: add to external string
-      okVar.external.push_back(kv);
-    }
-  }
+  okVar.objectname = cmd->objectname;
+  okVar.useobject = WeatherObjects::decodeTypeString(cmd->objecttypes);
+  okVar.file = cmd->file;
+  if (!cmd->time.undef())
+    okVar.time = miutil::stringFromTime(cmd->time, true);
+  okVar.totalminutes = cmd->timeDiff;
+  okVar.alphanr = cmd->alpha / 255.0;
   return okVar;
 }
 

src/qtObjectDialog.h
@@ -32,7 +32,7 @@
 #include "qtDataDialog.h"
 
 #include "diObjTypes.h"
-#include "util/diKeyValue.h"
+#include "diObjectsPlotCommand.h"
 
 #include <vector>
 
@@ -106,7 +106,7 @@ private:
   void updateSelectedFileList();
 
   // decode part of OK string
-  static PlotVariables decodeString(const miutil::KeyValue_v& tokens);
+  static PlotVariables decodeString(ObjectsPlotCommand_cp cmd);
   // make string from plotVariables
   std::string makeOKString(PlotVariables& okVar);
 

src/CMakeLists.txt
@@ -126,6 +126,7 @@ SET (LIBDIANA_SOURCES
   diMeasurementsPlot.cc
   diObjectManager.cc
   diObjectPlot.cc
+  diObjectsPlotCommand.cc
   diObjectPoint.cc
   diObsAscii.cc
   diObsData.cc

src/diDisplayObjects.cc
@@ -31,7 +31,7 @@
 
 #include "diDisplayObjects.h"
 #include "diDrawingTypes.h"
-#include "diKVListPlotCommand.h"
+#include "diObjectsPlotCommand.h"
 #include "diWeatherArea.h"
 #include "diWeatherFront.h"
 #include "diWeatherSymbol.h"
@@ -74,45 +74,30 @@ bool DisplayObjects::define(const PlotCommand_cp& pc)
 {
   METLIBS_LOG_SCOPE();
 
-  KVListPlotCommand_cp cmd = std::dynamic_pointer_cast<const KVListPlotCommand>(pc);
-  if (!cmd)
-    return 0;
-
-  init();
-  pin = cmd->all();
-
-  for (const miutil::KeyValue& kv : cmd->all()){
-    if (kv.key() == "types") {
-      setSelectedObjectTypes(kv.value());
-    } else {
-      const std::string& key = kv.key();
-      const std::string& value = kv.value();
-      METLIBS_LOG_DEBUG(LOGVAL(key) << LOGVAL(value));
-      if (key=="file") {
-        int l= value.length();
-        int f= value.rfind('.') + 1;
-        std::string tstr= value.substr(f,l-f);
-        itsTime = miutil::timeFromString(tstr);
-        autoFile= false;
-      } else if (key=="name") {
-        objectname = value;
-      } else if (key=="time") {
-        itsTime = miutil::timeFromString(value);
-        autoFile= false;
-      } else if (key == "timediff") {
-        timeDiff = kv.toInt();
-      } else if (key=="alpha" || key=="alfa") {
-        alpha = int(kv.toDouble()*255);
-      } else if (key=="frontlinewidth") {
-        newfrontlinewidth = kv.toInt();
-      } else if (key=="fixedsymbolsize") {
-        fixedsymbolsize= kv.toInt();
-      } else if (key=="symbolfilter") {
-        const std::vector<std::string> vals = miutil::split(value, ",");
-        diutil::insert_all(symbolfilter, vals);
-      }
-    }
+  ObjectsPlotCommand_cp cmd = std::dynamic_pointer_cast<const ObjectsPlotCommand>(pc);
+  if (!cmd) {
+    init();
+    return false;
   }
+
+  objectname = cmd->objectname;
+  if (!cmd->objecttypes.empty())
+    setSelectedObjectTypes(cmd->objecttypes);
+  if (!cmd->file.empty()) {
+    const int l = cmd->file.length();
+    const int f = cmd->file.rfind('.') + 1;
+    itsTime = miutil::timeFromString(cmd->file.substr(f, l - f));
+    autoFile = false;
+  }
+  if (!cmd->time.undef()) {
+    itsTime = cmd->time;
+    autoFile = false;
+  }
+  timeDiff = cmd->timeDiff;
+  alpha = cmd->alpha;
+  newfrontlinewidth = cmd->newfrontlinewidth;
+  fixedsymbolsize = cmd->fixedsymbolsize;
+  symbolfilter = cmd->symbolfilter;
 
   defined= true;
   return true;
@@ -139,7 +124,7 @@ bool DisplayObjects::prepareObjects()
   //set alpha value for objects as requested in objectdialog
   //and set state to passive
   for (ObjectPlot* pobject : objects) {
-    pobject->setPlotInfo(pin);
+    pobject->setPlotInfo(miutil::KeyValue_v());
     pobject->setColorAlpha(alpha);
     pobject->setState(ObjectPlot::passive);
     if (newfrontlinewidth)

src/diDisplayObjects.h
@@ -68,10 +68,6 @@ public:
   const std::string& getObjectName() const
     { return objectname; }
 
-  /// returns plotInfo string
-  const miutil::KeyValue_v& getPlotInfo() const
-    { return pin; }
-
   bool isDefined() const
     { return defined; }
 
@@ -97,9 +93,6 @@ private:
   std::string objectname;
   bool defined;
 
-  std::map<std::string,bool> useobject;
-
-  miutil::KeyValue_v pin;
   int alpha;
   int newfrontlinewidth;
   int fixedsymbolsize;

src/diObjectManager.cc
@@ -153,13 +153,9 @@ plottimes_t ObjectManager::getTimes()
   METLIBS_LOG_SCOPE();
   plottimes_t times;
 
-  for (const miutil::KeyValue& kv : objects.getPlotInfo()){
-    if (kv.key() == "name") {
-      for (const ObjFileInfo& ofi : getObjectFiles(kv.value(), true)) {
-        times.insert(ofi.time);
-      }
-      break;
-    }
+  if (!objects.getObjectName().empty()) {
+    for (const ObjFileInfo& ofi : getObjectFiles(objects.getObjectName(), true))
+      times.insert(ofi.time);
   }
 
   return times;
@@ -231,7 +227,7 @@ void ObjectManager::prepareObjects(const PlotCommand_cpv& inp)
 {
   METLIBS_LOG_SCOPE();
 
-  const miutil::KeyValue_v old_plotinfo = objects.getPlotInfo(); // make a copy
+  const bool old_defined = objects.isDefined();
   const bool old_enabled = objects.isEnabled();
 
   objects.init();
@@ -239,8 +235,7 @@ void ObjectManager::prepareObjects(const PlotCommand_cpv& inp)
   for (PlotCommand_cp pc : inp)
     objects.define(pc);
 
-  const miutil::KeyValue_v& new_plotinfo = objects.getPlotInfo();
-  objects.enable(old_enabled || (new_plotinfo != old_plotinfo));
+  objects.enable(!old_defined || old_enabled);
 }
 
 

src/diObjectsPlotCommand.cc
@@ -1,0 +1,112 @@
+/*
+  Diana - A Free Meteorological Visualisation Tool
+
+  Copyright (C) 2019 met.no
+
+  Contact information:
+  Norwegian Meteorological Institute
+  Box 43 Blindern
+  0313 OSLO
+  NORWAY
+  email: diana@met.no
+
+  This file is part of Diana
+
+  Diana is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+
+  Diana is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with Diana; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+#include "diObjectsPlotCommand.h"
+
+#include "util/misc_util.h"
+#include "util/time_util.h"
+
+#include <puTools/miStringFunctions.h>
+
+#include <sstream>
+
+static const std::string OBJECTS = "OBJECTS";
+
+ObjectsPlotCommand::ObjectsPlotCommand()
+    : timeDiff(0)
+    , alpha(255)
+    , newfrontlinewidth(0)
+    , fixedsymbolsize(0)
+{
+}
+
+miutil::KeyValue_v ObjectsPlotCommand::toKV() const
+{
+  miutil::KeyValue_v kvs;
+  add(kvs, "name", objectname);
+  add(kvs, "types", objecttypes);
+  if (!symbolfilter.empty())
+    add(kvs, "symbolfilter", symbolfilter);
+  if (!file.empty())
+    add(kvs, "file", file);
+  if (!time.undef())
+    add(kvs, "time", miutil::stringFromTime(time, true));
+  if (timeDiff > 0)
+    add(kvs, "timediff", timeDiff);
+  if (alpha != 255)
+    add(kvs, "alpha", alpha / 255.0f);
+  if (newfrontlinewidth != 0)
+    add(kvs, "frontlinewidth", newfrontlinewidth);
+  if (fixedsymbolsize != 0)
+    add(kvs, "fixedsymbolsize", fixedsymbolsize);
+
+  return kvs;
+}
+
+const std::string& ObjectsPlotCommand::commandKey() const
+{
+  return OBJECTS;
+}
+
+std::string ObjectsPlotCommand::toString() const
+{
+  std::ostringstream s;
+  s << OBJECTS << ' ' << toKV();
+  return s.str();
+}
+
+// static
+ObjectsPlotCommand_cp ObjectsPlotCommand::fromString(const std::string& line)
+{
+  ObjectsPlotCommand_p cmd = std::make_shared<ObjectsPlotCommand>();
+  for (const miutil::KeyValue& kv : miutil::splitKeyValue(line)) {
+    const std::string& key = kv.key();
+    const std::string& value = kv.value();
+    if (kv.key() == "types") {
+      cmd->objecttypes = miutil::split(value, ",");
+    } else if (key == "file") {
+      cmd->file = value;
+    } else if (key == "name") {
+      cmd->objectname = value;
+    } else if (key == "time") {
+      cmd->time = miutil::timeFromString(value);
+    } else if (key == "timediff") {
+      cmd->timeDiff = kv.toInt();
+    } else if (key == "alpha" || key == "alfa") {
+      cmd->alpha = int(kv.toFloat() * 255);
+    } else if (key == "frontlinewidth") {
+      cmd->newfrontlinewidth = kv.toInt();
+    } else if (key == "fixedsymbolsize") {
+      cmd->fixedsymbolsize = kv.toInt();
+    } else if (key == "symbolfilter") {
+      diutil::insert_all(cmd->symbolfilter, miutil::split(value, ","));
+    }
+  }
+  return cmd;
+}

src/diObjectsPlotCommand.h
@@ -1,0 +1,65 @@
+/*
+  Diana - A Free Meteorological Visualisation Tool
+
+  Copyright (C) 2019 met.no
+
+  Contact information:
+  Norwegian Meteorological Institute
+  Box 43 Blindern
+  0313 OSLO
+  NORWAY
+  email: diana@met.no
+
+  This file is part of Diana
+
+  Diana is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+
+  Diana is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with Diana; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+#ifndef DIOBJECTSPLOTCOMMAND_H
+#define DIOBJECTSPLOTCOMMAND_H
+
+#include "diPlotCommand.h"
+
+#include "util/diKeyValue.h"
+
+#include <puTools/miTime.h>
+
+class ObjectsPlotCommand : public PlotCommand
+{
+public:
+  ObjectsPlotCommand();
+
+  const std::string& commandKey() const override;
+  std::string toString() const override;
+
+  miutil::KeyValue_v toKV() const;
+
+  std::string objectname;
+  std::vector<std::string> objecttypes;
+  std::string file;
+  miutil::miTime time;
+  int timeDiff;
+  int alpha; // 0..255
+  int newfrontlinewidth;
+  int fixedsymbolsize;
+  std::vector<std::string> symbolfilter;
+
+  static std::shared_ptr<const ObjectsPlotCommand> fromString(const std::string& line);
+};
+
+typedef std::shared_ptr<ObjectsPlotCommand> ObjectsPlotCommand_p;
+typedef std::shared_ptr<const ObjectsPlotCommand> ObjectsPlotCommand_cp;
+
+#endif // DIOBJECTSPLOTCOMMAND_H

commit ee05f356fcfdc77d1a1cb80147412b4d82eb2416

bugfix ObjectsPlotCommand
 * regression in commit 0dcbfa97

src/qtObjectDialog.cc
@@ -470,7 +470,7 @@ PlotCommand_cpv ObjectDialog::getOKString()
 
     cmd->timeDiff = m_totalminutes;
     if (alpha->isChecked())
-      cmd->alpha = m_alphanr;
+      cmd->alpha = int(m_alphanr * 255.0f);
 
     vstr.push_back(cmd);
   }

src/qtObjectDialog.h
@@ -118,7 +118,7 @@ private:
   float m_scalediff;
 
   float m_alphascale;
-  float m_alphanr;
+  float m_alphanr; // 0..1
 
   bool useArchive;
   std::vector<std::string> objectnames;