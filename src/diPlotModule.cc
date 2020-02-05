metno/diana

commit c307b72ea2cc73d76da983a443ae5db16b7e1415

split PlotModule::updatePlots

src/diPlotModule.cc
@@ -187,6 +187,8 @@ void PlotModule::preparePlots(const PlotCommand_cpv& vpi)
   }
 
   defineMapArea();
+  // changeProjection()?
+  // changeTime()?
 }
 
 void PlotModule::prepareArea(const PlotCommand_cpv& inp)
@@ -361,48 +363,40 @@ void PlotModule::setAnnotations()
   diutil::insert_all(vap, objectplots_->getExtraAnnotations());
 }
 
-bool PlotModule::updatePlots()
+void PlotModule::changeTime(const miutil::miTime& mapTime)
 {
   METLIBS_LOG_SCOPE();
-  const miTime& t = staticPlot_->getTime();
+  staticPlot_->setTime(mapTime);
 
-  bool nodata = mapplots_->empty(); // false when data are found
+  satplots_->changeTime(mapTime);
+  fieldplots_->changeTime(mapTime);
 
-  if (fieldplots_->update()) {
-    nodata = false;
-    // level for vertical level observations "as field"
-    staticPlot_->setVerticalLevel(fieldplots_->getVerticalLevel());
-  }
+  // level for vertical level observations "as field"
+  staticPlot_->setVerticalLevel(fieldplots_->getVerticalLevel());
 
-  // prepare data for satellite plots
-  if (satplots_->setData())
-    nodata = false;
-  else
-    METLIBS_LOG_DEBUG("SatManager returned false from setData");
-
-  if (obsplots_->update(false, t))
-    nodata = false;
-
-  // prepare met-objects
-  objectplots_->changeTime(t);
-  if (!objectplots_->empty())
-    nodata = false;
-
-  // prepare item stored in miscellaneous managers
+  obsplots_->changeTime(mapTime);
+  objectplots_->changeTime(mapTime);
   for (Manager* m : boost::adaptors::values(managers)) {
-    // If the preparation fails then return false to indicate an error.
-    if (m->isEnabled() && !m->changeTime(t))
-      nodata = false;
+    if (m->isEnabled())
+      m->changeTime(mapTime);
   }
 
   setAnnotations();
+}
 
-  return !nodata;
+bool PlotModule::hasData()
+{
+  if (mapplots_->hasData() || fieldplots_->hasData() || satplots_->hasData() || obsplots_->hasData() || objectplots_->hasData())
+    return true;
+  for (Manager* m : boost::adaptors::values(managers)) {
+    if (m->isEnabled() && m->hasData())
+      return true;
+  }
+  return false;
 }
 
 bool PlotModule::defineMapAreaFromData(Area& newMapArea, bool& allowKeepCurrentArea)
 {
-  satplots_->setData();
   if (satplots_->getSatArea(newMapArea)) {
     // set area equal to first EXISTING sat-area
     allowKeepCurrentArea = true;
@@ -415,7 +409,6 @@ bool PlotModule::defineMapAreaFromData(Area& newMapArea, bool& allowKeepCurrentA
     return true;
   }
 
-  fieldplots_->update();
   if (fieldplots_->getRealFieldArea(newMapArea)) {
     allowKeepCurrentArea = true;
     return true;
@@ -959,12 +952,6 @@ void PlotModule::getCapabilitiesTime(plottimes_t& ctimes, const PlotCommand_cpv&
   }
 }
 
-// set plottime
-void PlotModule::setPlotTime(const miTime& t)
-{
-  staticPlot_->setTime(t);
-}
-
 void PlotModule::updateObs()
 {
   // Update ObsPlots if data files have changed
@@ -1134,7 +1121,7 @@ void PlotModule::deleteAllEditAnnotations()
 bool PlotModule::startTrajectoryComputation()
 {
   METLIBS_LOG_SCOPE();
-  if (trajectoryplots_->empty())
+  if (trajectoryplots_->hasData())
     return false;
 
   TrajectoryPlot* tp = trajectoryplots_->getPlot(0);
@@ -1162,7 +1149,7 @@ void PlotModule::stopTrajectoryComputation()
 // write trajectory positions to file
 bool PlotModule::printTrajectoryPositions(const std::string& filename)
 {
-  if (!trajectoryplots_->empty())
+  if (trajectoryplots_->hasData())
     return trajectoryplots_->getPlot(0)->printTrajectoryPositions(filename);
 
   return false;

src/bdiana_capi.cc
@@ -1146,7 +1146,7 @@ command_result Bdiana::handlePlotCommand(int& k)
 
     // necessary to set time before plotCommands()..?
     miTime thetime = miTime::nowTime();
-    main.controller->setPlotTime(thetime);
+    main.controller->changeTime(thetime);
     main.commands(pcom);
 
     if (!set_ptime(main))
@@ -1154,7 +1154,7 @@ command_result Bdiana::handlePlotCommand(int& k)
 
     if (verbose)
       METLIBS_LOG_INFO("- updatePlots");
-    if (!main.controller->updatePlots() && failOnMissingData) {
+    if (!main.controller->hasData() && failOnMissingData) {
       METLIBS_LOG_WARN("Failed to update plots.");
       return cmd_abort;
     }

src/bdiana_main.cc
@@ -136,5 +136,5 @@ plottimes_t BdianaMain::getTimes()
 
 void BdianaMain::setTime(const miutil::miTime& time)
 {
-  controller->setPlotTime(time);
+  controller->changeTime(time);
 }

src/diAreaObjectsCluster.cc
@@ -100,7 +100,7 @@ void AreaObjectsCluster::areaObjectsCommand(const std::string& command, const st
   }
 }
 
-bool AreaObjectsCluster::empty() const
+bool AreaObjectsCluster::hasData()
 {
-  return vareaobjects.empty();
+  return !vareaobjects.empty();
 }

src/diAreaObjectsCluster.h
@@ -44,7 +44,7 @@ public:
 
   void changeProjection(const Area& mapArea, const Rectangle& plotSize) override;
   void plot(DiGLPainter* gl, PlotOrder zorder) override;
-  bool empty() const;
+  bool hasData() override;
 
   ///put area into list of area objects
   void makeAreaObjects(std::string name, std::string areastring, int id);

src/diController.cc
@@ -258,9 +258,9 @@ void Controller::getPlotTimes(std::map<std::string, plottimes_t>& times)
 }
 
 // set plottime
-void Controller::setPlotTime(const miTime& t)
+void Controller::changeTime(const miTime& t)
 {
-  plotm->setPlotTime(t);
+  plotm->changeTime(t);
 }
 
 // toggle area conservatism
@@ -269,10 +269,9 @@ void Controller::keepCurrentArea(bool b)
   plotm->setKeepCurrentArea(b);
 }
 
-// update plot-classes with new data
-bool Controller::updatePlots()
+bool Controller::hasData()
 {
-  return plotm->updatePlots();
+  return plotm->hasData();
 }
 
 // reload obsevations

src/diController.h
@@ -160,9 +160,11 @@ public:
   void getCapabilitiesTime(plottimes_t& okTimes, const PlotCommand_cpv& pinfos, bool allTimes = true);
 
   /// set plottime
-  void setPlotTime(const miutil::miTime&);
-  /// update plots
-  bool updatePlots();
+  void changeTime(const miutil::miTime&);
+
+  /// check if any plot has data
+  bool hasData();
+
   /// toggle area conservatism
   void keepCurrentArea(bool);
 

src/diDrawingManager.cc
@@ -445,13 +445,9 @@ plottimes_t DrawingManager::getTimes() const
  * Prepares the manager for display of, and interaction with, items that
  * correspond to the given \a time.
 */
-bool DrawingManager::changeTime(const miutil::miTime& time)
+void DrawingManager::changeTime(const miutil::miTime& time)
 {
-  bool found = false;
-
-  // Check the requested time against the available times.
-  plottimes_t times = getTimes();
-  found = (times.find(time) != times.end());
+  mapTime_ = time;
 
   QDateTime dateTime;
   if (!time.undef()) {
@@ -492,16 +488,20 @@ bool DrawingManager::changeTime(const miutil::miTime& time)
 
     itemGroup->setTime(dateTime, allVisible);
   }
-
-  return found;
 }
 
-bool DrawingManager::changeProjection(const Area& /*mapArea*/, const Rectangle& plotSize)
+bool DrawingManager::hasData()
+{
+  // Check the requested time against the available times.
+  plottimes_t times = getTimes();
+  return (times.find(mapTime_) != times.end());
+}
+
+void DrawingManager::changeProjection(const Area& /*mapArea*/, const Rectangle& plotSize)
 {
   // Record the new plot rectangle and area.
   // Update the edit rectangle so that objects are positioned consistently.
   setEditRect(plotSize);
-  return true;
 }
 
 void DrawingManager::setCanvas(DiCanvas* canvas)

commit 2016ec35bc46ff0532821b6fe6e2c33ba767139b

bugfix PlotModule: call defineMapArea more often
 - model/sat-area was sometimes not working
 - call from changeTime, i.e. usually after loading data
 - problem probably introduced in commit c307b72e

src/diPlotModule.cc
@@ -183,7 +183,6 @@ void PlotModule::preparePlots(const PlotCommand_cpv& vpi)
 
   if (!mapplots_->getBackColour().empty())
     staticPlot_->setBgColour(mapplots_->getBackColour());
-  defineMapArea();
   staticPlot_->setVerticalLevel(fieldplots_->getVerticalLevel());  // vertical level for observations "as field"
   // changeProjection()?
   // changeTime()?
@@ -355,6 +354,7 @@ void PlotModule::changeTime(const miutil::miTime& mapTime)
       m->changeTime(mapTime);
   }
 
+  defineMapArea();
   setAnnotations();
 }
 