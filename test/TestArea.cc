metno/diana

commit 18096ceeed9ab750b2e03d140def4d2691812181

add GridArea::operator==

src/diField/diArea.cc
@@ -1,7 +1,7 @@
 /*
   Diana - A Free Meteorological Visualisation Tool
 
-  Copyright (C) 2013-2018 met.no
+  Copyright (C) 2013-2019 met.no
 
   Contact information:
   Norwegian Meteorological Institute
@@ -75,7 +75,7 @@ bool Area::operator==(const Area &rhs) const
 
 bool Area::operator!=(const Area &rhs) const
 {
-  return ((proj != rhs.proj) || (rect != rhs.rect));
+  return !(this->operator==(rhs));
 }
 
 std::ostream& operator<<(std::ostream& output, const Area& a)
@@ -170,6 +170,11 @@ GridArea GridArea::scaled(int factor) const
   s.nx /= factor;
   s.ny /= factor;
   return s;
+}
+
+bool GridArea::operator==(const GridArea& rhs) const
+{
+  return Area::operator==(rhs) && nx == rhs.nx && ny == rhs.ny && resolutionX == rhs.resolutionX && resolutionY == rhs.resolutionY;
 }
 
 std::ostream& operator<<(std::ostream& output, const GridArea& ga)

src/diField/diArea.h
@@ -2,7 +2,7 @@
 /*
   Diana - A Free Meteorological Visualisation Tool
 
-  Copyright (C) 2013 met.no
+  Copyright (C) 2013-2019 met.no
 
   Contact information:
   Norwegian Meteorological Institute
@@ -107,6 +107,8 @@ public:
 
   float fromGridY(float ygrid) const
     { return ygrid*resolutionY + R().y1; }
+
+  bool operator==(const GridArea& rhs) const;
 
 public:
   int nx;

src/diField/diGridConverter.cc
@@ -1,7 +1,7 @@
 /*
   Diana - A Free Meteorological Visualisation Tool
 
-  Copyright (C) 2006-2015 met.no
+  Copyright (C) 2006-2019 met.no
 
   Contact information:
   Norwegian Meteorological Institute
@@ -328,7 +328,7 @@ bool GridConverter::getVectorRotationElements(const Area& data_area,
   const int n = anglebuffer->size();
   for (int i=0; i < n; ++i) {
     const Points& pi = (*anglebuffer)[i];
-    if (pi.area == data_area && pi.map_proj == map_proj && pi.npos == nvec) {
+    if ((Area)pi.area == data_area && pi.map_proj == map_proj && pi.npos == nvec) {
       *cosx = pi.x;
       *sinx = pi.y;
       return true;

commit bafb08425120ea68b9ae518187b0da85e4a025ad

bugfix GridArea::operator!=
 * need to implement for GridArea since base class Area has operator!=
 * regression in commit 18096ceeed

src/diField/diArea.cc
@@ -73,11 +73,6 @@ bool Area::operator==(const Area &rhs) const
   return ((proj == rhs.proj) && (rect == rhs.rect));
 }
 
-bool Area::operator!=(const Area &rhs) const
-{
-  return !(this->operator==(rhs));
-}
-
 std::ostream& operator<<(std::ostream& output, const Area& a)
 {
   return output << "name="<<a.name<<" "<<a.proj<<"  "<<a.rect;

src/diField/diArea.h
@@ -60,7 +60,7 @@ public:
   Area& operator=(const Area &rhs);
 
   bool operator==(const Area &rhs) const;
-  bool operator!=(const Area &rhs) const;
+  bool operator!=(const Area& rhs) const { return !(*this == rhs); }
 
   friend std::ostream& operator<<(std::ostream& output, const Area& a);
 
@@ -107,6 +107,7 @@ public:
     { return ygrid*resolutionY + R().y1; }
 
   bool operator==(const GridArea& rhs) const;
+  bool operator!=(const GridArea& rhs) const { return !(*this == rhs); }
 
 public:
   int nx;

test/CMakeLists.txt
@@ -94,6 +94,7 @@ METNO_HEADERS(dianaUnitTests_QTHEADERS   "cc" "h" ${dianaUnitTests_QTSOURCES})
 
 SET (dianaUnitTests_SOURCES
   ${dianaUnitTests_QTSOURCES}
+  TestArea.cc
   TestCommandParser.cc
   TestDianaLevels.cc
   TestFieldPlot.cc

test/TestArea.cc
@@ -1,0 +1,61 @@
+/*
+  Diana - A Free Meteorological Visualisation Tool
+
+  Copyright (C) 2020 met.no
+
+  Contact information:
+  Norwegian Meteorological Institute
+  Box 43 Blindern
+  0313 OSLO
+  NORWAY
+  email: diana@met.no
+
+  This file is part of Diana
+
+  Diana is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+
+  Diana is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with Diana; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+#include <diField/diField.h>
+
+#include <gtest/gtest.h>
+
+TEST(Area, EqOperator)
+{
+  const Area a1(Projection("+proj=ob_tran +o_proj=longlat +lon_0=0 +o_lat_p=90 +R=6.371e+06 +no_defs +x_0=3.13723 +y_0=1.5708"),
+                Rectangle(0, 0, 6.27913, 3.14166));
+  ASSERT_TRUE(a1 == a1);
+  ASSERT_FALSE(a1 != a1);
+
+  const Area a2(Projection("+proj=utm +zone=33 +ellps=WGS84 +datum=WGS84 +units=m +no_defs"), Rectangle(0, 0, 10000, 110000));
+  ASSERT_TRUE(a1 != a2);
+  ASSERT_FALSE(a1 == a2);
+}
+
+TEST(GridArea, EqOperator)
+{
+  const GridArea ga1(Area(Projection("+proj=longlat +a=6367470 +e=0 +no_defs"), Rectangle(-1.0472, 0.5236, 1.5708, 1.5708)), 751, 301, 0.00349069, 0.00349069);
+  ASSERT_TRUE(ga1 == ga1);
+  ASSERT_FALSE(ga1 != ga1);
+
+  // same Area as ga1, different grid size
+  const GridArea ga2(ga1, 1501, 601, 0.00174534, 0.00174534);
+  ASSERT_TRUE(ga1 != ga2);
+  ASSERT_FALSE(ga1 == ga2);
+
+// same P, nx, ny and resolution as ga1, but shifted rect
+  const GridArea ga3(Area(Projection("+proj=longlat +a=6367470 +e=0 +no_defs"), Rectangle(0.0472, -0.4764, 0.5708, 0.5708)), 751, 301, 0.00349069, 0.00349069);
+  ASSERT_FALSE(ga1 == ga3);
+  ASSERT_TRUE(ga1 != ga3);
+}