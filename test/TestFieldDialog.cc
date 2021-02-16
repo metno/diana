metno/diana

commit e8b371ba11e430de78da3c44ff82b9261749a5c6

separate reftimelist update and retrieval to avoid CachedGridIO::clear()

test/TestFieldDialog.cc
@@ -50,6 +50,7 @@ public:
 
   FieldModelGroupInfo_v getFieldModelGroups() override { return fieldModelGroups; }
   attributes_t getFieldGlobalAttributes(const std::string& /*model*/, const std::string& /*refTime*/) override { return attributes_t(); }
+  void updateFieldReferenceTimes(const std::string&) override { }
   std::set<std::string> getFieldReferenceTimes(const std::string& m) override { return fieldReferenceTimes[m]; }
   std::string getBestFieldReferenceTime(const std::string& m, int ro, int rh) override { return ::getBestReferenceTime(getFieldReferenceTimes(m), ro, rh); }
   void getSetupFieldOptions(std::map<std::string, miutil::KeyValue_v>& fieldoptions) override { fieldoptions = setupFieldOptions; }

src/diField/diFieldManager.cc
@@ -440,7 +440,7 @@ std::map<std::string,std::string> FieldManager::getGlobalAttributes(const std::s
 {
   METLIBS_LOG_SCOPE(LOGVAL(modelName)<<LOGVAL(refTime));
 
-  if (GridCollectionPtr pgc = getGridCollection(modelName, refTime, false))
+  if (GridCollectionPtr pgc = getGridCollection(modelName, refTime))
     return pgc->getGlobalAttributes(refTime);
   else
     return std::map<std::string,std::string>();
@@ -449,15 +449,31 @@ std::map<std::string,std::string> FieldManager::getGlobalAttributes(const std::s
 std::map<std::string, FieldPlotInfo> FieldManager::getFieldPlotInfo(const std::string& modelName, const std::string& refTime)
 {
   METLIBS_LOG_SCOPE(LOGVAL(modelName)<<LOGVAL(refTime));
-  if (GridCollectionPtr pgc = getGridCollection(modelName, refTime, false))
+  if (GridCollectionPtr pgc = getGridCollection(modelName, refTime))
     return pgc->getFieldPlotInfo(refTime);
   else
     return std::map<std::string, FieldPlotInfo>();
 }
 
-FieldManager::GridCollectionPtr FieldManager::getGridCollection(const std::string& modelName, const std::string& refTime, bool updateSources)
+bool FieldManager::updateGridCollection(const std::string& modelName)
 {
-  METLIBS_LOG_TIME();
+  METLIBS_LOG_TIME(LOGVAL(modelName));
+
+  GridSources_t::iterator p = gridSources.find(modelName);
+  if (p == gridSources.end()) {
+    METLIBS_LOG_WARN("Undefined model '" << modelName << "'");
+    return false;
+  }
+
+  GridCollectionPtr pgc = p->second;
+  if (pgc->updateSources())
+    pgc->makeInventory("");
+  return true;
+}
+
+FieldManager::GridCollectionPtr FieldManager::getGridCollection(const std::string& modelName, const std::string& refTime)
+{
+  METLIBS_LOG_TIME(LOGVAL(modelName) << LOGVAL(refTime));
 
   GridSources_t::iterator p = gridSources.find(modelName);
   if (p == gridSources.end()) {
@@ -466,10 +482,8 @@ FieldManager::GridCollectionPtr FieldManager::getGridCollection(const std::strin
   }
 
   GridCollectionPtr pgc = p->second;
-  if (updateSources || !pgc->inventoryOk(refTime)) {
-    if (!updateSources || pgc->updateSources())
-      pgc->makeInventory(refTime);
-  }
+  if (!pgc->inventoryOk(refTime))
+    pgc->makeInventory(refTime);
   return pgc;
 }
 
@@ -510,7 +524,7 @@ gridinventory::Grid FieldManager::getGrid(const std::string& modelName)
 {
   std::string reftime = getBestReferenceTime(modelName, 0, -1);
   gridinventory::Grid grid;
-  GridCollectionPtr pgc = getGridCollection(modelName, reftime, false);
+  GridCollectionPtr pgc = getGridCollection(modelName, reftime);
 
   if (pgc) {
     grid = pgc->getGrids();
@@ -519,12 +533,12 @@ gridinventory::Grid FieldManager::getGrid(const std::string& modelName)
   return grid;
 }
 
-plottimes_t FieldManager::getFieldTime(const FieldRequest& frq, bool updateSources)
+plottimes_t FieldManager::getFieldTime(const FieldRequest& frq)
 {
   METLIBS_LOG_SCOPE(LOGVAL(frq.modelName) << LOGVAL(frq.refTime) << LOGVAL(frq.paramName) << LOGVAL(frq.zaxis) << LOGVAL(frq.plevel));
   plottimes_t times;
 
-  if (GridCollectionPtr pgc = getGridCollection(frq.modelName, frq.refTime, updateSources)) {
+  if (GridCollectionPtr pgc = getGridCollection(frq.modelName, frq.refTime)) {
     // if fieldrequest.paramName is a standard_name, find variable_name (times will be empty if translation to variable name fails)
     std::string paramName = frq.paramName;
     if (!frq.standard_name || pgc->standardname2variablename(frq.refTime, frq.paramName, paramName))
@@ -545,7 +559,7 @@ plottimes_t FieldManager::getFieldTime(const FieldRequest& frq, bool updateSourc
   return times;
 }
 
-plottimes_t FieldManager::getFieldTime(const std::vector<FieldRequest>& fieldrequests, bool updateSources)
+plottimes_t FieldManager::getFieldTime(const std::vector<FieldRequest>& fieldrequests)
 {
   METLIBS_LOG_SCOPE();
 
@@ -555,7 +569,7 @@ plottimes_t FieldManager::getFieldTime(const std::vector<FieldRequest>& fieldreq
   for (const FieldRequest& frq : fieldrequests) {
     allTimeSteps &= frq.allTimeSteps;
 
-    const plottimes_t frq_times = getFieldTime(frq, updateSources);
+    const plottimes_t frq_times = getFieldTime(frq);
     if (allTimeSteps) {
       diutil::insert_all(all_times, frq_times);
     } else if (all_times.empty()) {
@@ -572,7 +586,7 @@ plottimes_t FieldManager::getFieldTime(const std::vector<FieldRequest>& fieldreq
 
 std::set<std::string> FieldManager::getReferenceTimes(const std::string& modelName)
 {
-  if (GridCollectionPtr pgc = getGridCollection(modelName, "", true))
+  if (GridCollectionPtr pgc = getGridCollection(modelName, ""))
     return pgc->getReferenceTimes();
   else
     return set<std::string>();
@@ -589,7 +603,7 @@ Field_p FieldManager::makeField(const FieldRequest& frq)
   METLIBS_LOG_TIME(LOGVAL(frq.modelName) << LOGVAL(frq.paramName) << LOGVAL(frq.zaxis) << LOGVAL(frq.refTime) << LOGVAL(frq.ptime) << LOGVAL(frq.plevel)
                                          << LOGVAL(frq.elevel) << LOGVAL(frq.unit));
 
-  GridCollectionPtr pgc = getGridCollection(frq.modelName, frq.refTime, false);
+  GridCollectionPtr pgc = getGridCollection(frq.modelName, frq.refTime);
   if (!pgc) {
     METLIBS_LOG_WARN("could not find grid collection for model=" << frq.modelName << " reftime=" << frq.refTime);
     return nullptr;
@@ -657,7 +671,7 @@ bool FieldManager::writeField(const FieldRequest& fieldrequest, Field_cp field)
       << LOGVAL(fieldrequest.ptime) << LOGVAL(fieldrequest.plevel)
       << LOGVAL(fieldrequest.elevel) << LOGVAL(fieldrequest.unit));
 
-  GridCollectionPtr pgc = getGridCollection(fieldrequest.modelName, fieldrequest.refTime, false);
+  GridCollectionPtr pgc = getGridCollection(fieldrequest.modelName, fieldrequest.refTime);
 
   if (not pgc) {
     METLIBS_LOG_WARN(LOGVAL(fieldrequest.modelName) << LOGVAL(fieldrequest.refTime) << "' not found");

src/diField/diFieldManager.h
@@ -92,8 +92,10 @@ public:
   gridinventory::Grid getGrid(const std::string& modelName);
 
   /// return available times for the requested models and fields
-  plottimes_t getFieldTime(const std::vector<FieldRequest>& fieldrequests, bool updateSources);
-  plottimes_t getFieldTime(const FieldRequest& fieldrequest, bool updateSources);
+  plottimes_t getFieldTime(const std::vector<FieldRequest>& fieldrequests);
+  plottimes_t getFieldTime(const FieldRequest& fieldrequest);
+
+  bool updateGridCollection(const std::string& modelName);
 
   /// return the reference time, time of analysis if a forecast
   std::set<std::string> getReferenceTimes(const std::string& modelName);
@@ -134,7 +136,7 @@ private:
   typedef std::map<std::string, std::string> gridio_sections_t;
   gridio_sections_t gridio_sections;
 
-  GridCollectionPtr getGridCollection(const std::string& modelName, const std::string& refTime, bool updateSources);
+  GridCollectionPtr getGridCollection(const std::string& modelName, const std::string& refTime);
 };
 
 #endif

src/diFieldDialogData.h
@@ -56,6 +56,9 @@ public:
   /// return the reference time given by refOffset and refhour or the last reference time for the given model
   virtual std::string getBestFieldReferenceTime(const std::string& model, int refOffset, int refHour) = 0;
 
+  /// update reference times for the given model
+  virtual void updateFieldReferenceTimes(const std::string& model) = 0;
+
   /// return all reference times for the given model
   virtual std::set<std::string> getFieldReferenceTimes(const std::string& model) = 0;
 

src/diFieldEdit.cc
@@ -444,8 +444,7 @@ bool FieldEdit::readEditfield(const std::string& filename)
   cmd->addOptions(miutil::KeyValue_v() << miutil::kv("units", fieldUnit));
   std::vector<FieldPlotCommand_cp> vpin(1, cmd);
 
-  bool dummy = false;
-  plottimes_t times = fieldPlotManager->getFieldTime(vpin, dummy);
+  plottimes_t times = fieldPlotManager->getFieldTime(vpin);
   miTime time;
   if (!times.empty()) {
     time = *times.begin();

src/diFieldPlot.cc
@@ -190,7 +190,7 @@ const miutil::miTime& FieldPlot::getAnalysisTime() const
 
 plottimes_t FieldPlot::getFieldTimes() const
 {
-  return fieldplotm_->getFieldTime(std::vector<FieldPlotCommand_cp>(1, cmd_), false);
+  return fieldplotm_->getFieldTime(std::vector<FieldPlotCommand_cp>(1, cmd_));
 }
 
 miutil::miTime FieldPlot::getReferenceTime() const

src/diFieldPlotCluster.cc
@@ -75,7 +75,7 @@ plottimes_t FieldPlotCluster::getTimes()
   std::vector<FieldPlotCommand_cp> commands;
   for (FieldPlot* fp : diutil::static_content_cast<FieldPlot*>(plots_))
     commands.push_back(fp->command());
-  return fieldplotm_->getFieldTime(commands, false);
+  return fieldplotm_->getFieldTime(commands);
 }
 
 plottimes_t FieldPlotCluster::fieldAnalysisTimes() const

src/diFieldPlotManager.cc
@@ -379,7 +379,7 @@ vector<std::string> FieldPlotManager::splitComStr(const std::string& s, bool spl
   return tmp;
 }
 
-plottimes_t FieldPlotManager::getFieldTime(const std::vector<FieldPlotCommand_cp>& pinfos, bool updateSources)
+plottimes_t FieldPlotManager::getFieldTime(const std::vector<FieldPlotCommand_cp>& pinfos)
 {
   METLIBS_LOG_SCOPE();
 
@@ -394,7 +394,7 @@ plottimes_t FieldPlotManager::getFieldTime(const std::vector<FieldPlotCommand_cp
   if (requests.empty())
     return plottimes_t();
 
-  return getFieldTime(requests, updateSources);
+  return getFieldTime(requests);
 }
 
 miTime FieldPlotManager::getFieldReferenceTime(FieldPlotCommand_cp cmd)
@@ -435,7 +435,8 @@ void FieldPlotManager::getCapabilitiesTime(plottimes_t& normalTimes, int& timedi
   }
 
   //getting times
-  normalTimes = getFieldTime(std::vector<FieldPlotCommand_cp>(1, cmd), true);
+  fieldManager->updateGridCollection(cmd->field.model);
+  normalTimes = getFieldTime(std::vector<FieldPlotCommand_cp>(1, cmd));
 
   METLIBS_LOG_DEBUG("no. of times"<<normalTimes.size());
 }
@@ -461,7 +462,7 @@ vector<std::string> FieldPlotManager::getFieldLevels(FieldPlotCommand_cp cmd)
   return levels;
 }
 
-plottimes_t FieldPlotManager::getFieldTime(std::vector<FieldRequest>& request, bool updateSources)
+plottimes_t FieldPlotManager::getFieldTime(std::vector<FieldRequest>& request)
 {
   METLIBS_LOG_SCOPE();
 
@@ -479,7 +480,7 @@ plottimes_t FieldPlotManager::getFieldTime(std::vector<FieldRequest>& request, b
     flightlevel2pressure(frq);
   }
 
-  return fieldManager->getFieldTime(request, updateSources);
+  return fieldManager->getFieldTime(request);
 }
 
 bool FieldPlotManager::addGridCollection(const std::string& modelname, const std::string& filename, bool writeable)
@@ -498,6 +499,11 @@ bool FieldPlotManager::addGridCollection(const std::string& modelname, const std
 FieldModelGroupInfo_v FieldPlotManager::getFieldModelGroups()
 {
   return fieldManager->getFieldModelGroups();
+}
+
+void FieldPlotManager::updateFieldReferenceTimes(const std::string& model)
+{
+  fieldManager->updateGridCollection(model);
 }
 
 set<std::string> FieldPlotManager::getFieldReferenceTimes(const std::string& model)

commit 23a75b67de4e9dc32aa53e2c44af01d069a6d2b5

bugfix reference times update for quickmenus
 * regression in commit e8b371ba11

src/qtFieldDialog.cc
@@ -1,7 +1,7 @@
 /*
  Diana - A Free Meteorological Visualisation Tool
 
-  Copyright (C) 2006-2020 met.no
+  Copyright (C) 2006-2021 met.no
 
  Contact information:
  Norwegian Meteorological Institute
@@ -774,6 +774,13 @@ bool FieldDialog::levelsExists(bool up, int type)
   return false;
 }
 
+void FieldDialog::maybeUpdateFieldReferenceTimes(std::set<std::string>& updated_models, const std::string& model)
+{
+  if (updated_models.insert(model).second) {
+    m_data->updateFieldReferenceTimes(model);
+  }
+}
+
 void FieldDialog::putOKString(const PlotCommand_cpv& vstr)
 {
   METLIBS_LOG_SCOPE();
@@ -786,10 +793,16 @@ void FieldDialog::putOKString(const PlotCommand_cpv& vstr)
     return;
   }
 
+  std::set<std::string> updated_models;
+
   for (PlotCommand_cp pc : vstr) {
     FieldPlotCommand_cp cmd = std::dynamic_pointer_cast<const FieldPlotCommand>(pc);
     if (!cmd)
       continue;
+
+    maybeUpdateFieldReferenceTimes(updated_models, cmd->field.model);
+    if (cmd->hasMinusField())
+      maybeUpdateFieldReferenceTimes(updated_models, cmd->minus.model);
 
     SelectedField sf;
     sf.external = true;

src/qtFieldDialog.h
@@ -1,7 +1,7 @@
 /*
   Diana - A Free Meteorological Visualisation Tool
 
-  Copyright (C) 2006-2020 met.no
+  Copyright (C) 2006-2021 met.no
 
   Contact information:
   Norwegian Meteorological Institute
@@ -201,6 +201,7 @@ private:
   void setLevel();
   void setIdnum();
   bool decodeCommand(FieldPlotCommand_cp cmd, const FieldPlotCommand::FieldSpec& fs, SelectedField& sf);
+  void maybeUpdateFieldReferenceTimes(std::set<std::string>& updated_models, const std::string& model);
 
   void toolTips();
 

test/TestFieldDialog.cc
@@ -1,7 +1,7 @@
 /*
   Diana - A Free Meteorological Visualisation Tool
 
-  Copyright (C) 2018-2020 met.no
+  Copyright (C) 2018-2021 met.no
 
   Contact information:
   Norwegian Meteorological Institute
@@ -43,6 +43,9 @@
 #include "miLogger/miLogging.h"
 
 namespace {
+const std::string MODEL1 = "supermodel";
+const std::string PARAM1 = "param_a";
+
 class TestFieldDialogData : public FieldDialogData
 {
 public:
@@ -50,7 +53,7 @@ public:
 
   FieldModelGroupInfo_v getFieldModelGroups() override { return fieldModelGroups; }
   attributes_t getFieldGlobalAttributes(const std::string& /*model*/, const std::string& /*refTime*/) override { return attributes_t(); }
-  void updateFieldReferenceTimes(const std::string&) override { }
+  void updateFieldReferenceTimes(const std::string& model) override { if (model == MODEL1) fieldReferenceTimeUpdates += 1; }
   std::set<std::string> getFieldReferenceTimes(const std::string& m) override { return fieldReferenceTimes[m]; }
   std::string getBestFieldReferenceTime(const std::string& m, int ro, int rh) override { return ::getBestReferenceTime(getFieldReferenceTimes(m), ro, rh); }
   void getSetupFieldOptions(std::map<std::string, miutil::KeyValue_v>& fieldoptions) override { fieldoptions = setupFieldOptions; }
@@ -65,12 +68,12 @@ public:
 
   //! key: plot name, value: options
   std::map<std::string, miutil::KeyValue_v> setupFieldOptions;
+
+  int fieldReferenceTimeUpdates;
 };
 
-const std::string MODEL1 = "supermodel";
-const std::string PARAM1 = "param_a";
-
 TestFieldDialogData::TestFieldDialogData()
+    : fieldReferenceTimeUpdates(0)
 {
   FieldModelGroupInfo fmg;
   fmg.groupName = "My Favourite Test Group";
@@ -137,6 +140,7 @@ TEST(TestFieldDialog, PutGetOKStringRaw)
 
   const PlotCommand_cpv cmds_put = makeCommands({"FIELD model=" + MODEL1 + " refhour=0 parameter=" + PARAM1 + " plottype=contour"});
   dialog->putOKString(cmds_put);
+  EXPECT_EQ(data->fieldReferenceTimeUpdates, 1);
 
   const PlotCommand_cpv cmds_get = dialog->getOKString();
   ASSERT_EQ(cmds_get.size(), 1);
@@ -159,6 +163,7 @@ TEST(TestFieldDialog, PutGetOKStringMinus)
                                                     + " ) refhour=0 plottype=contour"});
   // clang-format on
   dialog->putOKString(cmds_put);
+  EXPECT_EQ(data->fieldReferenceTimeUpdates, 1); // same model, should update only once
 
   const PlotCommand_cpv cmds_get = dialog->getOKString();
   ASSERT_EQ(cmds_get.size(), 1);