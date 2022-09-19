metno/diana

commit 4f8c6ab8801f26189f5d3fba2078b89d4b0be843

support for proj 6 api
 - remove all #include "proj_api.h" from header files
 - add unit test that woul fail due to differences in ob_tran between proj 4
   and proj 8 (partially disabled)
 - disable an existing unit test that would fail for the same reason
 - add "Transformation" class for transformations between two projections
   - cache explicitly in GridReprojection, FilledMap, SatPlot, ShapeObject
   - cache generically as initialization is slow in proj 8
 - drop StaticPlot::ProjToMap and StaticPlot::MapToProj
 - cleanup init of Projection objects for WKT, EPSG, ...
 - compatibility for deg/rad areas: "rectangle=" is converted from rad to deg,
   "rect=" is not converted (unit test in TestArea.cc)

src/diSatPlot.cc
@@ -1,7 +1,7 @@
 /*
   Diana - A Free Meteorological Visualisation Tool
 
-  Copyright (C) 2006-2021 met.no
+  Copyright (C) 2006-2022 met.no
 
   Contact information:
   Norwegian Meteorological Institute
@@ -63,6 +63,15 @@ void SatPlot::changeTime(const miutil::miTime& mapTime)
   setStatus(hasData() ? P_OK_DATA : P_OK_EMPTY);
 }
 
+void SatPlot::changeProjection(const Area& mapArea, const Rectangle& plotSize, const diutil::PointI& physSize)
+{
+  SatPlotBase::changeProjection(mapArea, plotSize, physSize);
+  if (mapArea.P() != mapP_) {
+    mapP_ = mapArea.P();
+    mapT_ = mapP_.transformationFrom(mapArea.P());
+  }
+}
+
 const GridArea& SatPlot::getSatArea()
 {
   return satdata->area;
@@ -84,12 +93,14 @@ void SatPlot::getCalibChannels(std::vector<std::string>& channels) const
 
 void SatPlot::values(float x, float y, std::vector<SatValues>& satval) const
 {
-  if (!isEnabled() || !hasData())
+  if (!isEnabled() || !hasData() || !mapT_)
     return;
 
-  //x, y in map coordinates
-  //Convert to satellite proj coordiantes
-  getStaticPlot()->MapToProj(satdata->area.P(), 1, &x, &y);
+  // x, y in map coordinates
+
+  // convert to satellite proj coordinates
+  mapT_->forward(1, &x, &y);
+
   // convert to satellite pixel
   int xpos = satdata->area.toGridX(x);
   int ypos = satdata->area.toGridY(y);

src/diSatPlot.h
@@ -1,7 +1,7 @@
 /*
   Diana - A Free Meteorological Visualisation Tool
 
-  Copyright (C) 2006-2021 met.no
+  Copyright (C) 2006-2022 met.no
 
   Contact information:
   Norwegian Meteorological Institute
@@ -47,6 +47,7 @@ public:
   ~SatPlot();
 
   void changeTime(const miutil::miTime& mapTime) override;
+  void changeProjection(const Area& mapArea, const Rectangle& plotSize, const diutil::PointI& physSize) override;
   void plot(DiGLPainter* gl, PlotOrder zorder) override;
   void getAnnotation(std::string&, Colour&) const override;
   void getDataAnnotations(std::vector<std::string>& anno) const override;
@@ -66,6 +67,8 @@ private:
 private:
   SatManager* satm_;
   std::unique_ptr<Sat> satdata;
+  Projection mapP_;
+  Transformation_cp mapT_;
 };
 
 #endif

CMakeLists.txt
@@ -1,6 +1,6 @@
 # Diana - A Free Meteorological Visualisation Tool
 #
-# Copyright (C) 2017-2021 met.no
+# Copyright (C) 2017-2022 met.no
 #
 # Contact information:
 # Norwegian Meteorological Institute
@@ -134,8 +134,30 @@ LIST(APPEND PKGS_CORE ${libcurl_PACKAGE})
 METNO_FIND_PACKAGE(proj
   PKGCONFIG_NAME "proj"
   LIBRARY_NAME "proj"
-  INCLUDE_HDR "proj_api.h"
+  INCLUDE_HDR "proj.h" "proj_api.h"
 )
+MESSAGE (STATUS "proj version='${proj_PACKAGE_VERSION}'")
+IF (proj_PACKAGE_VERSION STREQUAL "")
+  # unknown version
+  IF ("${proj_HEADER}" STREQUAL "proj.h")
+    SET(HAVE_PROJ_H 1)
+  ENDIF()
+ELSEIF (proj_PACKAGE_VERSION VERSION_LESS "4.6.0")
+  # <4.6
+  MESSAGE(SEND_ERROR "proj version '${proj_PACKAGE_VERSION}' is too old")
+ELSEIF (${proj_PACKAGE_VERSION} VERSION_LESS "5.0.0")
+  # >=4.6,<5.0
+  # proj_api.h
+ELSEIF (${proj_PACKAGE_VERSION} VERSION_LESS "6.2.0")
+  # >=5.0,<6.2
+  MESSAGE(SEND_ERROR "proj version '${proj_PACKAGE_VERSION}' not supported")
+ELSEIF (${proj_PACKAGE_VERSION} VERSION_LESS "8.0.0")
+  # >=6.2,<8.0
+  # ok, proj_api.h
+ELSE ()
+  # >=8.0
+  SET(HAVE_PROJ_H 1)
+ENDIF()
 LIST(APPEND PKGS_CORE ${proj_PACKAGE})
 
 METNO_FIND_PACKAGE(shp

cmake/MetnoFindPackage.cmake
@@ -18,6 +18,7 @@ FUNCTION(METNO_FIND_PACKAGE ffp)
   )
   set(ARGS1
     # arguments with one value
+    PACKAGE
     VERSION_MIN
     CMAKE_NAME
     PKGCONFIG_NAME
@@ -34,6 +35,7 @@ FUNCTION(METNO_FIND_PACKAGE ffp)
   IF (_ffp_CMAKE_NAME)
     FIND_PACKAGE(${_ffp_CMAKE_NAME} ${_ffp_VERSION_MIN} QUIET)
     IF(${_ffp_CMAKE_NAME}_FOUND)
+      SET(${ffp}_PACKAGE_VERSION "${${_ffp_CMAKE_NAME}_VERSION}" PARENT_SCOPE)
       IF (NOT(DEFINED _ffp_CMAKE_TARGETS))
         SET(_ffp_CMAKE_TARGET ${_ffp_CMAKE_NAME})
       ENDIF()
@@ -70,6 +72,7 @@ FUNCTION(METNO_FIND_PACKAGE ffp)
     UNSET(${_ffp_pc}_FOUND CACHE)
     PKG_CHECK_MODULES(${_ffp_pc} IMPORTED_TARGET QUIET "${_ffp_pc_version}")
     IF(${_ffp_pc}_FOUND)
+      SET(${ffp}_PACKAGE_VERSION "${${_ffp_pc}_VERSION}" PARENT_SCOPE)
       SET(${ffp}_PACKAGE "PkgConfig::${_ffp_pc}" PARENT_SCOPE)
       MESSAGE(STATUS "Found ${ffp} via pkg-config '${_ffp_pc_version}'")
       RETURN()
@@ -91,8 +94,8 @@ FUNCTION(METNO_FIND_PACKAGE ffp)
   IF (NOT _ffp_inc_dir)
     MESSAGE(FATAL_ERROR "Cannot find ${ffp}, include header '${_ffp_INCLUDE_HDR}' not found")
   ENDIF()
+  UNSET(_ffp_lib CACHE)
   IF (_ffp_LIBRARY_NAME)
-    UNSET(_ffp_lib CACHE)
     FIND_LIBRARY(_ffp_lib
       NAMES ${_ffp_LIBRARY_NAME}
       HINTS "${${ffp}_LIB_DIR}" "${${ffp}_DIR}/lib"
@@ -103,5 +106,6 @@ FUNCTION(METNO_FIND_PACKAGE ffp)
   ENDIF()
   METNO_ADD_IMPORTED_LIBRARY(${ffp}_LIB_INC "${_ffp_lib}" "${_ffp_inc_dir}" "")
   SET(${ffp}_PACKAGE "${ffp}_LIB_INC" PARENT_SCOPE)
+  SET(${ffp}_PACKAGE_VERSION "" PARENT_SCOPE)
   MESSAGE(STATUS "Found ${ffp} via find_package/find_path, lib='${_ffp_lib}', include='${_ffp_inc_dir}'")
 ENDFUNCTION()

src/diContouring.cc
@@ -1,7 +1,7 @@
 /*
   Diana - A Free Meteorological Visualisation Tool
 
-  Copyright (C) 2006-2021 met.no
+  Copyright (C) 2006-2022 met.no
 
   Contact information:
   Norwegian Meteorological Institute
@@ -60,6 +60,7 @@ using namespace std; using namespace miutil;
 const int maxLines=1000000;
 
 namespace {
+const float RAD_TO_DEG = 180 / M_PI;
 
 /// temporary field contouring info
 struct LinePart {

src/diEditManager.cc
@@ -1,7 +1,7 @@
 /*
   Diana - A Free Meteorological Visualisation Tool
 
-  Copyright (C) 2006-2021 met.no
+  Copyright (C) 2006-2022 met.no
 
   Contact information:
   Norwegian Meteorological Institute
@@ -2773,7 +2773,8 @@ void EditManager::plot(DiGLPainter* gl, PlotOrder zorder)
           }
         }
       }
-      if (plotm->getStaticPlot()->MapToProj(fedits[0]->editfield->area.P(), npos, x.get(), y.get())) {
+      const auto tf = fedits[0]->editfield->area.P().transformationFrom(plotm->getStaticPlot()->getMapProjection());
+      if (tf->forward(npos, x.get(), y.get())) {
         float s= 0.;
         for (int j=0; j<npos; j+=2) {
           float dx= x[j] - x[j+1];

src/diField/FimexIO.cc
@@ -1,7 +1,7 @@
 /*
   Diana - A Free Meteorological Visualisation Tool
 
-  Copyright (C) 2010-2020 met.no
+  Copyright (C) 2010-2022 met.no
 
   Contact information:
   Norwegian Meteorological Institute
@@ -33,6 +33,7 @@
 
 #include "diField.h"
 #include "util/misc_util.h"
+#include "util/string_util.h"
 
 #include <puCtools/stat.h>
 #include <puTools/miStringFunctions.h>
@@ -234,6 +235,7 @@ FimexIO::FimexIO(const std::string & modelname, const std::string & sourcename,
     std::string value = options[i].substr(pos+1);
     if (key == "proj4string") {
       projDef = value;
+      diutil::remove_quote(projDef);
     } else if (key == "singletimestep") {
       singleTimeStep = true;
     } else if (key == "writeable") {
@@ -384,7 +386,7 @@ void FimexIO::inventoryExtractGridProjection(MetNoFimex::Projection_cp projectio
   grid.y_0 = 0;
 
   const float axis_scale = 1.0; // X/Y-axes are scaled by this
-  const std::string xyUnit = projection->isDegree() ? "radian" : METER;
+  const std::string xyUnit = projection->isDegree() ? "degree" : METER;
   DataPtr xdata = feltReader->getScaledDataInUnit(xAxis->getName(), xyUnit);
   DataPtr ydata = feltReader->getScaledDataInUnit(yAxis->getName(), xyUnit);
   if (xdata) {
@@ -419,13 +421,12 @@ void FimexIO::inventoryExtractGridProjection(MetNoFimex::Projection_cp projectio
       METLIBS_LOG_DEBUG(" y_resolution:" << grid.y_resolution << " y_0:" << grid.y_0);
     }
   }
-  
-  grid.projection = projection->getProj4String();
 
-  // if defined, use projDefinition from setup
-  if (not projDef.empty()) {
+  if (!projDef.empty()) {
     grid.projection = projDef;
-    METLIBS_LOG_DEBUG(" using projection from setup: "<<grid.projection);
+    METLIBS_LOG_DEBUG("using projection from setup: '" << grid.projection << "'");
+  } else {
+    grid.projection = projection->getProj4String();
   }
 }
 

src/diField/diArea.cc
@@ -1,7 +1,7 @@
 /*
   Diana - A Free Meteorological Visualisation Tool
 
-  Copyright (C) 2013-2020 met.no
+  Copyright (C) 2013-2022 met.no
 
   Contact information:
   Norwegian Meteorological Institute
@@ -27,13 +27,12 @@
   Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
-#include "diana_config.h"
-
 #include "diArea.h"
+#include "util/geo_util.h"
+#include "util/string_util.h"
 
 #include <puTools/miString.h>
 #include <puDatatypes/miCoordinates.h>
-#include <sstream>
 
 #define MILOGGER_CATEGORY "diField.Area"
 #include "miLogger/miLogging.h"
@@ -88,15 +87,20 @@ void Area::memberCopy(const Area& rhs)
 
 bool Area::setAreaFromString(const std::string& areaString)
 {
+  // FIXME: this is VERY similar to PlotModule::prepareArea, the difference
+  // is that this version does not look up name in MapAreaSetup
+
   if (areaString.empty())
     return false;
 
   const char key_name[] = "name";
   const char key_proj[] = "proj4string";
   const char key_rectangle[] = "rectangle";
+  const char key_rect[] = "rect";
 
   std::string rectangleStr;
   std::string projStr;
+  bool angles_deg = false;
 
   // split on blank, preserve ""
   const std::vector<std::string> tokens= miutil::split_protected(areaString, '"','"'," ",true);
@@ -110,30 +114,40 @@ bool Area::setAreaFromString(const std::string& areaString)
         name = stokens[1];
       } else if (key==key_proj){
         projStr = stokens[1];
+        diutil::remove_quote(projStr);
       } else if (key==key_rectangle){
         rectangleStr = stokens[1];
+        angles_deg = false;
+      } else if (key == key_rect) {
+        rectangleStr = stokens[1];
+        angles_deg = true;
       }
     }
   }
 
-  if (projStr.empty() || projStr == "\"\"") {
+  if (projStr.empty()) {
     //Undefined projections (used in model/sat area)
     return true;
   }
 
-  if (proj.setProj4Definition(projStr) && rect.setRectangle(rectangleStr)) {
-    return true;
+  if (!proj.setFromString(projStr))
+    return false;
+
+  if (!rect.setRectangle(rectangleStr))
+    return false;
+
+  if (!angles_deg && proj.isDegree()) {
+    // backward compatibility: when using proj4, rectangles were in radians; keep it like that for now
+    diutil::convertRectToDegrees(rect);
   }
-
-  return false;
+  return true;
 }
-
 
 std::string Area::getAreaString() const
 {
   std::ostringstream ost;
-  ost << " proj4string=\"" << proj.getProj4Definition() << "\""
-      << " rectangle=" << rect.toString();
+  ost << "proj4string=\"" << proj.getProj4Definition() << "\""
+      << " rect=" << rect.toString();
   return ost.str();
 }
 

commit 2722e68380da41be1e3ec466df8ac687e19a62d1

bugfix sat projection for values
 * using wrong projection
 * regression in commit 4f8c6ab880

src/diSatPlot.cc
@@ -58,7 +58,10 @@ SatPlot::~SatPlot()
 
 void SatPlot::changeTime(const miutil::miTime& mapTime)
 {
+  const auto satP = getSatArea();
   satm_->setData(satdata.get(), mapTime);
+  if (satP != getSatArea())
+    updateMapSatTransformation();
   setPlotName(satdata->plotname);
   setStatus(hasData() ? P_OK_DATA : P_OK_EMPTY);
 }
@@ -68,8 +71,13 @@ void SatPlot::changeProjection(const Area& mapArea, const Rectangle& plotSize, c
   SatPlotBase::changeProjection(mapArea, plotSize, physSize);
   if (mapArea.P() != mapP_) {
     mapP_ = mapArea.P();
-    mapT_ = mapP_.transformationFrom(mapArea.P());
+    updateMapSatTransformation();
   }
+}
+
+void SatPlot::updateMapSatTransformation()
+{
+  mapT_ = getSatArea().P().transformationFrom(mapP_);
 }
 
 const GridArea& SatPlot::getSatArea()
@@ -99,7 +107,8 @@ void SatPlot::values(float x, float y, std::vector<SatValues>& satval) const
   // x, y in map coordinates
 
   // convert to satellite proj coordinates
-  mapT_->forward(1, &x, &y);
+  if (!mapT_->forward(1, &x, &y))
+    return;
 
   // convert to satellite pixel
   int xpos = satdata->area.toGridX(x);

src/diSatPlot.h
@@ -63,6 +63,7 @@ private:
   SatPlot& operator=(const SatPlot& rhs) = delete;
 
   bool hasData() const;
+  void updateMapSatTransformation();
 
 private:
   SatManager* satm_;