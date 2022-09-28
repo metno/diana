metno/diana

commit 1939ad3f8be841c71d9cfa6865af8d76eeb052ab

de-duplicate timeFromString and stringFromTime
 * implement using miTime ctor
 * remove most usages of unix_t0
 * return undef for bad time

src/diField/VcrossUtil.cc
@@ -264,7 +264,7 @@ int vc_select_cs(const std::string& ucs, Inventory_cp inv)
 const char SECONDS_SINCE_1970[] = "seconds since 1970-01-01 00:00:00";
 const char DAYS_SINCE_1900[] = "days since 1900-01-01 00:00:00";
 
-static const miutil::miTime time0("1970-01-01 00:00:00"), day0("1900-01-01 00:00:00");
+static const miutil::miTime time0 = miutil::unix_t0, day0("1900-01-01 00:00:00");
 
 Time from_miTime(const miutil::miTime& mitime)
 {

src/diDisplayObjects.cc
@@ -1,7 +1,7 @@
 /*
   Diana - A Free Meteorological Visualisation Tool
 
-  Copyright (C) 2006-2015 met.no
+  Copyright (C) 2006-2018 met.no
 
   Contact information:
   Norwegian Meteorological Institute
@@ -36,6 +36,8 @@
 #include "diWeatherFront.h"
 #include "diWeatherSymbol.h"
 #include "util/misc_util.h"
+#include "util/string_util.h"
+#include "util/time_util.h"
 
 #include <puTools/miStringFunctions.h>
 
@@ -90,12 +92,12 @@ bool DisplayObjects::define(const PlotCommand_cp& pc)
         int l= value.length();
         int f= value.rfind('.') + 1;
         std::string tstr= value.substr(f,l-f);
-        itsTime= timeFromString(tstr);
+        itsTime = miutil::timeFromString(tstr);
         autoFile= false;
       } else if (key=="name") {
         objectname = value;
       } else if (key=="time") {
-        itsTime = timeFromString(value);
+        itsTime = miutil::timeFromString(value);
         autoFile= false;
       } else if (key == "timediff") {
         timeDiff = kv.toInt();
@@ -165,7 +167,9 @@ bool DisplayObjects::prepareObjects()
 void DisplayObjects::getObjAnnotation(string &str, Colour &col)
 {
   if (approved) {
-    str = objectname + " " + itsTime.format("%D %H:%M", "", true);
+    str = objectname;
+    if (!itsTime.undef())
+      diutil::appendText(str, itsTime.format("%D %H:%M", "", true));
     col = Colour("black");
   } else {
     str.erase();
@@ -202,8 +206,8 @@ std::string DisplayObjects::getName() const
   std::string name;
   if (approved) {
     name = objectname;
-    if (!autoFile)
-      name += " " + itsTime.isoTime();
+    if (!autoFile && !itsTime.undef())
+      diutil::appendText(name, itsTime.isoTime());
   }
   return name;
 }

src/diEditObjects.cc
@@ -31,9 +31,10 @@
 #include "diana_config.h"
 
 #include "diEditObjects.h"
+#include "diWeatherArea.h"
 #include "diWeatherFront.h"
 #include "diWeatherSymbol.h"
-#include "diWeatherArea.h"
+#include "util/string_util.h"
 
 #include <cmath>
 
@@ -1401,11 +1402,13 @@ void EditObjects::setScaleToField(float s){
  *  Methods for reading and writing comments                *
  ************************************************************/
 
-void EditObjects::putCommentStartLines(const std::string name, const std::string prefix, const std::string lines)
+void EditObjects::putCommentStartLines(const std::string& name, const std::string& prefix, const std::string& lines)
 {
   //return the startline of the comments file to read
-  startlines = prefix + std::string(" ") + name +
-  std::string(" ") + itsTime.isoTime()+ std::string("\n");
+  startlines = prefix + std::string(" ") + name;
+  if (!itsTime.undef())
+    diutil::appendText(startlines, itsTime.isoTime());
+  startlines += "\n";
   itsComments+=
     "*************************************************\n";
   itsComments+=startlines;

src/diEditObjects.h
@@ -1,7 +1,7 @@
 /*
   Diana - A Free Meteorological Visualisation Tool
 
-  Copyright (C) 2006-2015 met.no
+  Copyright (C) 2006-2018 met.no
 
   Contact information:
   Norwegian Meteorological Institute
@@ -9,7 +9,7 @@
   0313 OSLO
   NORWAY
   email: diana@met.no
-  
+
   This file is part of Diana
 
   Diana is free software; you can redistribute it and/or modify
@@ -21,7 +21,7 @@
   but WITHOUT ANY WARRANTY; without even the implied warranty of
   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
   GNU General Public License for more details.
-  
+
   You should have received a copy of the GNU General Public License
   along with Diana; if not, write to the Free Software
   Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
@@ -219,7 +219,7 @@ public:
   /// check all fronts for join points...
   void checkJoinPoints();
   /// put prefix, name and time at start of comments
-  void putCommentStartLines(const std::string name, const std::string prefix, const std::string lines);
+  void putCommentStartLines(const std::string& name, const std::string& prefix, const std::string& lines);
   ///returns true if comment differ from startlines
   bool hasComments();
   /// returns the comments

src/diObjectManager.cc
@@ -43,6 +43,7 @@
 #include "miSetupParser.h"
 
 #include "util/charsets.h"
+#include "util/time_util.h"
 
 #include <puTools/miStringFunctions.h>
 
@@ -56,11 +57,6 @@
 
 using namespace std;
 using namespace::miutil;
-
-namespace {
-//zero time = 00:00:00 UTC Jan 1 1970
-const miutil::miTime ztime = miTime(1970,1,1,0,0,0);
-} // namespace
 
 ObjectManager::ObjectManager(PlotModule* pl)
   : plotm(pl), mapmode(normal_mode)
@@ -262,7 +258,7 @@ bool ObjectManager::prepareObjects(const miTime& t, const Area& area)
     return false;
 
   //if autoFile or wrong time, set correct time
-  if (objects.isAutoFile() || objects.getTime() ==ztime)
+  if (objects.isAutoFile() || objects.getTime().undef())
     objects.setTime(t);
 
   if (objects.isAutoFile() || objects.filename.empty()){
@@ -320,8 +316,8 @@ vector<ObjFileInfo> ObjectManager::listFiles(ObjectList & ol)
   const diutil::string_v matches = diutil::glob(fileString);
   for (diutil::string_v::const_iterator it = matches.begin(); it != matches.end(); ++it) {
     const std::string& name = *it;
-    miTime time = timeFilterFileName(name, ol.filter);
-    if(!time.undef() && time!=ztime){
+    const miTime time = timeFilterFileName(name, ol.filter);
+    if (!time.undef()) {
       ObjFileInfo info;
       info.name = name;
       info.time = time;
@@ -369,12 +365,7 @@ miTime ObjectManager::timeFileName(const std::string& fileName)
   //get time from a file with name *.yyyymmddhh
   const vector <std::string> parts= miutil::split(fileName, 0, ".");
   int nparts= parts.size();
-  //if (parts.size() != 2) {
-  //if (parts.size() < 2) {
-  //return ztime ;
-  //}
   if (parts[nparts-1].length() < 10) {
-
     size_t pos1 = fileName.find_last_of("_");
     size_t pos2 = fileName.find_last_of(".");
     if ( pos1 != string::npos && pos2 != string::npos && pos2 > pos1 + 10 ) {
@@ -385,33 +376,14 @@ miTime ObjectManager::timeFileName(const std::string& fileName)
     }
     return miTime();
   }
-  return timeFromString(parts[nparts-1]);
+  return miutil::timeFromString(parts[nparts - 1]);
 }
-
-// static
-miTime ObjectManager::timeFromString(const std::string& timeString)
-{
-  //get time from a string with yyyymmddhhmm
-  if (timeString.size() < 10)
-    return miTime();
-  int year= atoi(timeString.substr(0,4).c_str());
-  int mon=  atoi(timeString.substr(4,2).c_str());
-  int day=  atoi(timeString.substr(6,2).c_str());
-  int hour= atoi(timeString.substr(8,2).c_str());
-  int min= 0;
-  if (timeString.length() >= 12)
-    min= atoi(timeString.substr(10,2).c_str());
-  if (year<0 || mon <0 || day<0 || hour<0 || min < 0)
-    return ztime;
-  return miTime(year,mon,day,hour,min,0);
-}
-
 
 bool ObjectManager::getFileName(DisplayObjects& wObjects)
 {
   METLIBS_LOG_SCOPE();
 
-  if (wObjects.getObjectName().empty() || wObjects.getTime()==ztime)
+  if (wObjects.getObjectName().empty() || wObjects.getTime().undef())
     return false;
 
 
@@ -424,23 +396,23 @@ bool ObjectManager::getFileName(DisplayObjects& wObjects)
     po->second.updated= true;
   }
 
-  int n= po->second.files.size();
-
-  int fileno=-1;
-  int d, diff=wObjects.getTimeDiff() + 1;
-
-  miTime t = wObjects.getTime();
-  for (int i=0; i<n; i++) {
-    d= abs(miTime::minDiff(t,po->second.files[i].time));
-    if (d<diff) {
-      diff= d;
-      fileno= i;
+  const ObjFileInfo* best = 0;
+  const miutil::miTime& t = wObjects.getTime();
+  if (!t.undef()) {
+    int diff = 0;
+    for (const auto& f : po->second.files) {
+      const int d = abs(miTime::minDiff(t, f.time));
+      if (!best || d < diff) {
+        diff = d;
+        best = &f;
+      }
     }
   }
-  if (fileno<0) return false;
+  if (!best)
+    return false;
 
-  wObjects.filename= po->second.files[fileno].name;
-  wObjects.setTime(po->second.files[fileno].time);
+  wObjects.filename = best->name;
+  wObjects.setTime(best->time);
 
   return true;
 }
@@ -1228,32 +1200,10 @@ void ObjectManager::undofrontClear()
   editobjects.undofrontClear();
 }
 
-
 map <std::string,bool> ObjectManager::decodeTypeString(std::string token)
 {
   return WeatherObjects::decodeTypeString(token);
 }
-
-
-std::string ObjectManager::stringFromTime(const miTime& t,bool addMinutes)
-{
-  int yyyy= t.year();
-  int mm  = t.month();
-  int dd  = t.day();
-  int hh  = t.hour();
-  int mn  = t.min();
-
-  ostringstream ostr;
-  ostr << setw(4) << setfill('0') << yyyy
-       << setw(2) << setfill('0') << mm
-       << setw(2) << setfill('0') << dd
-       << setw(2) << setfill('0') << hh;
-  if (addMinutes)
-       ostr << setw(2) << setfill('0') << mn;
-
-  return ostr.str();
-}
-
 
 bool ObjectManager::_isafile(const std::string& name)
 {
@@ -1264,7 +1214,6 @@ bool ObjectManager::_isafile(const std::string& name)
   } else
     return false;
 }
-
 
 bool ObjectManager::checkFileName(const std::string& fileName)
 {

src/diObjectManager.h
@@ -94,9 +94,6 @@ private:
 
   bool getFileName(DisplayObjects &wObjects);
   miutil::miTime timeFilterFileName(const std::string& fileName, const miutil::TimeFilter& filter);
-  static miutil::miTime timeFromString(const std::string& timeString);
-  //get time string yyyymmddhh or yyyymmddhhmm from time
-  std::string stringFromTime(const miutil::miTime& t,bool addMinutes);
 
   bool checkFileName(const std::string &fileName);
 

src/diSatManager.cc
@@ -36,6 +36,7 @@
 #include "diStaticPlot.h"
 #include "diUtilities.h"
 #include "miSetupParser.h"
+#include "util/time_util.h"
 #include "util/was_enabled.h"
 
 #include <puTools/miStringFunctions.h>
@@ -59,7 +60,6 @@ using namespace miutil;
 
 namespace {
 const std::vector<SatFileInfo> emptyfile;
-const miutil::miTime ztime(1970, 1, 1, 0, 0, 0); //zero time = 00:00:00 UTC Jan 1 1970
 
 int _filestat(const std::string fname, pu_struct_stat& filestat)
 {
@@ -1009,7 +1009,6 @@ void SatManager::listFiles(subProdInfo &subp)
       subp.file.insert(p, ft);
     }
   }
-
 
   if (subp.formattype == "mitiff") {
     //update Prod[satellite][file].colours

src/diSpectrumManager.cc
@@ -1,7 +1,7 @@
 /*
   Diana - A Free Meteorological Visualisation Tool
 
-  Copyright (C) 2006-2017 met.no
+  Copyright (C) 2006-2018 met.no
 
   Contact information:
   Norwegian Meteorological Institute
@@ -31,6 +31,7 @@
 
 #include "diSpectrumManager.h"
 
+#include "diField/VcrossUtil.h"
 #include "diSpectrumData.h"
 #include "diSpectrumFile.h"
 #include "diSpectrumOptions.h"
@@ -42,11 +43,8 @@
 #include "vcross_v2/VcrossCollector.h"
 #include "vcross_v2/VcrossSetup.h"
 
-#include <diField/VcrossUtil.h>
-
 #include <QSize>
 
-#include <puCtools/stat.h>
 #include <puTools/miStringFunctions.h>
 
 #define MILOGGER_CATEGORY "diana.SpectrumManager"
@@ -63,9 +61,6 @@ SpectrumManager::SpectrumManager()
   , dataChange(true)
 {
   METLIBS_LOG_SCOPE();
-
-  //zero time = 00:00:00 UTC Jan 1 1970
-  ztime = miTime(1970,1,1,0,0,0);
 
   plotTime= miTime::nowTime();
 }

commit 69f6dcdce96e46b304a16130c2d0363f6fcceec4

bugfix time conversion
 * undefined initialization order
 * regression in commit 1939ad3f8b (from 2018)

src/diField/VcrossUtil.cc
@@ -1,7 +1,7 @@
 /*
   Diana - A Free Meteorological Visualisation Tool
 
-  Copyright (C) 2015-2020 met.no
+  Copyright (C) 2015-2022 met.no
 
   Contact information:
   Norwegian Meteorological Institute
@@ -285,29 +285,39 @@ int vc_select_cs(const std::string& ucs, Inventory_cp inv)
 const char SECONDS_SINCE_1970[] = "seconds since 1970-01-01 00:00:00";
 const char DAYS_SINCE_1900[] = "days since 1900-01-01 00:00:00";
 
-static const miutil::miTime time0 = miutil::unix_t0, day0("1900-01-01 00:00:00");
+namespace {
+const miutil::miTime& time0()
+{
+  return miutil::unix_t0;
+}
+
+const miutil::miTime& day0()
+{
+  static const miutil::miTime day0("1900-01-01 00:00:00");
+  return day0;
+}
+} // namespace
 
 Time from_miTime(const miutil::miTime& mitime)
 {
   if (mitime.undef())
     return Time();
-  else if (mitime >= time0)
-    return Time(SECONDS_SINCE_1970, miutil::miTime::secDiff(mitime, time0));
+  else if (mitime >= time0())
+    return Time(SECONDS_SINCE_1970, miutil::miTime::secDiff(mitime, time0()));
   else
-    return Time(DAYS_SINCE_1900, miutil::miTime::hourDiff(mitime, day0)/24);
+    return Time(DAYS_SINCE_1900, miutil::miTime::hourDiff(mitime, day0()) / 24);
 }
 
 // ------------------------------------------------------------------------
 
 miutil::miTime to_miTime(const std::string& unit, Time::timevalue_t value)
 {
-  if (!unit.empty()) {
-    if (diutil::startswith(unit, "days")) {
-      if (MetNoFimex::UnitsConverter_p uconv = unitConverter(unit, DAYS_SINCE_1900))
-        return miutil::addHour(day0, 24 * uconv->convert(value));
-    }
+  if (diutil::startswith(unit, "days")) {
+    if (MetNoFimex::UnitsConverter_p uconv = unitConverter(unit, DAYS_SINCE_1900))
+      return miutil::addHour(day0(), 24 * uconv->convert(value));
+  } else if (!unit.empty()) {
     if (MetNoFimex::UnitsConverter_p uconv = unitConverter(unit, SECONDS_SINCE_1970))
-      return miutil::addSec(time0, uconv->convert(value));
+      return miutil::addSec(time0(), uconv->convert(value));
   }
   return miutil::miTime();
 }