metno/diana

commit 7f6bd4c340a412c67807c0730685d0e7a0b5779a

SatManager::setPalette: drop unused parameter

src/diSatManager.cc
@@ -230,7 +230,7 @@ void SatManager::setData(Sat* satdata, const miutil::miTime& satptime)
 
   if (satdata->palette && satdata->formatType != "geotiff") {
     // at the moment no controllable rgb or alpha operations here
-    setPalette(satdata, fInfo);
+    setPalette(satdata);
   } else {
     // GEOTIFF ALWAYS RGBA
     setRGB(satdata);
@@ -367,7 +367,7 @@ bool SatManager::readSatFile(Sat* satdata, const miutil::miTime& t)
 }
 
 /***********************************************************************/
-void SatManager::setPalette(Sat* satdata, SatFileInfo& /*fInfo*/)
+void SatManager::setPalette(Sat* satdata)
 {
   //  PURPOSE:   uses palette to put data from image into satdata.image
   METLIBS_LOG_TIME();

src/diSatManager.h
@@ -83,7 +83,7 @@ private:
 
   void setRGB(Sat* satdata);
   void calcRGBstrech(unsigned char *image, const int& size, const float& cut);
-  void setPalette(Sat* satdata, SatFileInfo &);
+  void setPalette(Sat* satdata);
   void listFiles(subProdInfo &subp);
   void readHeader(SatFileInfo&, const std::vector<std::string>&);
 

commit fba93fdcd41976b4fa88e13c10fe406ee4dd4212

bugfix mosaic files
 * regression in commit 7f6bd4c340
 * `addMosaicfiles` specified filename outside `Sat` struct,
   and the `filename` parameter was ignored in `doReadSatFile`

src/diSatManager.cc
@@ -306,7 +306,7 @@ bool SatManager::parseChannels(Sat* satdata, const SatFileInfo &fInfo)
 
 /***********************************************************************/
 
-bool SatManager::doReadSatFile(const std::string& filename, Sat* satdata)
+bool SatManager::doReadSatFile(Sat* satdata)
 {
   //read the file with name satdata->actualfile, channels given in
   //satdata->index. Result in satdata->rawimage
@@ -315,7 +315,7 @@ bool SatManager::doReadSatFile(const std::string& filename, Sat* satdata)
     //stat() is not able to get the file attributes,
     //so the file obviously does not exist or
     //more capabilities is required
-    METLIBS_LOG_ERROR("filename:" << satdata->actualfile << " does not exist or is not readable");
+    METLIBS_LOG_ERROR("filename: '" << satdata->actualfile << "' does not exist or is not readable");
     return false;
   }
 
@@ -355,7 +355,7 @@ bool SatManager::doReadSatFile(const std::string& filename, Sat* satdata)
 
 bool SatManager::readSatFile(Sat* satdata, const miutil::miTime& t)
 {
-  if (!doReadSatFile(satdata->actualfile, satdata))
+  if (!doReadSatFile(satdata))
     return false;
 
   if (!satdata->palette) {
@@ -622,8 +622,9 @@ void SatManager::addMosaicfiles(Sat* satdata, const std::vector<SatFileInfo>& mo
       sd.index[j] = satdata->index[j];
     sd.no = satdata->no;
     sd.formatType = satdata->formatType;
+    sd.actualfile = mfi.name;
 
-    if (!doReadSatFile(mfi.name, &sd))
+    if (!doReadSatFile(&sd))
       continue;
 
     if (sd.area.nx!=satdata->area.nx || sd.area.ny!=satdata->area.ny

src/diSatManager.h
@@ -89,7 +89,7 @@ private:
 
   bool parseChannels(Sat* satdata, const SatFileInfo& info);
   bool readSatFile(Sat* satdata, const miutil::miTime& t);
-  bool doReadSatFile(const std::string& filename, Sat* satdata);
+  bool doReadSatFile(Sat* satdata);
 
   void init_rgbindex(Sat& sd);
   void init_rgbindex_Meteosat(Sat& sd);